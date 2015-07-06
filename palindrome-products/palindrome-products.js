1152df5ad971083145d82aef9f5c32811d839373
rchavarria <txingo@gmail.com>
2015-07-06 12:32:08 +0200

    Add configuration files that will allow users to run tests easier
    A package.json file to install several tools. A gulpfile.js file
    to run tests automatically (transpiling ES6 code to ES5). And a
    SETUP.md file describing how to install dependencies and run tests
    for each assignment.

---
+++ palindrome-products/palindrome-products.js
@@ -0,0 +1,23 @@
+'use strict';
+exports.generate = generate;
+
+// module imports (remove when es6 features are shipped with Node.js)
+require('./traceur-runtime');
+
+/**
+ * Generate palindrome-products table and calculate largest/smallest.
+ *
+ * @param {Number} [minFactor]
+ * Minimum Factor
+ *
+ * @param {Number} [maxFactor]
+ * Maximum Factor
+ *
+ * @return {{largest: number, smallest: number}}
+ */
+function generate({ minFactor = 1, maxFactor = Number.MAX_VALUE }) {
+//
+// YOUR CODE GOES HERE
+//
+}
+
