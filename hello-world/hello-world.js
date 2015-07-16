df291309e179a036faa392e162dd2b0987784eab
rchavarria <txingo@gmail.com>
2015-07-16 08:51:45 +0200

    Add new assignment: hello-world

---
+++ hello-world/hello-world.js
@@ -0,0 +1,15 @@
+//
+// This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+class HelloWorld {
+  hello(name) {
+    //
+    // YOUR CODE GOES HERE
+    //
+  }
+}
+
+module.exports = HelloWorld;
+
