8ebdc11075b3b1195ea8303b4a92174e202dc5d3
rchavarria <txingo@gmail.com>
2015-07-23 23:25:46 +0200

    Add new exercise called Bob
    This exercise has been copied from the JavaScript track of
    exercism.io
    
    It has been refactored to use some ES6 features

---
+++ bob/bob.js
@@ -0,0 +1,15 @@
+//
+// This is only a SKELETON file for the 'Bob' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+class Bob {
+  hey(message) {
+    //
+    // YOUR CODE GOES HERE
+    //
+  }
+}
+
+module.exports = Bob;
+
