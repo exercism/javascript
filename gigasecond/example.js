61bb39a04c599640b90290116f79c335e9f9afe0
Rubén Chavarría <txingo@gmail.com>
2015-09-13 22:38:40 +0200

    Add new exercise: Gigasecond

---
+++ gigasecond/example.js
@@ -0,0 +1,14 @@
+const GIGASECOND_IN_MILIS = 1e9 * 1e3;
+
+export default class Gigasecond {
+
+  constructor(dateOfBirth) {
+    this.dateOfBirth = dateOfBirth;
+  }
+
+  date() {
+    let birthTime = this.dateOfBirth.getTime();
+    return new Date(birthTime + GIGASECOND_IN_MILIS);
+  }
+
+}
