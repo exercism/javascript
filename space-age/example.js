d223924f96946b5d377d0f25aa5d2285fbbb3397
Rubén Chavarría <txingo@gmail.com>
2015-10-18 22:45:01 +0200

    New exercise: Space age

---
+++ space-age/example.js
@@ -0,0 +1,59 @@
+const EARTH_TO_OTHER_PLANETS = {
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  earth: 1,
+  mars: 1.8808158,
+  jupiter: 11.862615,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132
+};
+
+class SpaceAge {
+
+  constructor(seconds) {
+    this.seconds = seconds;
+    this.earthYears = seconds / 31557600;
+  }
+
+  yearsOnPlanet(planet) {
+    var years = this.earthYears / EARTH_TO_OTHER_PLANETS[planet];
+    return parseFloat(years.toFixed(2));
+  };
+
+  onMercury() {
+    return this.yearsOnPlanet('mercury');
+  };
+
+  onVenus() {
+    return this.yearsOnPlanet('venus');
+  };
+
+  onEarth() {
+    return this.yearsOnPlanet('earth');
+  };
+
+  onMars() {
+    return this.yearsOnPlanet('mars');
+  };
+
+  onJupiter() {
+    return this.yearsOnPlanet('jupiter');
+  };
+
+  onSaturn() {
+    return this.yearsOnPlanet('saturn');
+  };
+
+  onUranus() {
+    return this.yearsOnPlanet('uranus');
+  };
+
+  onNeptune() {
+    return this.yearsOnPlanet('neptune');
+  };
+
+}
+
+export default SpaceAge;
+
