e1ea389103e07b495cb02025beac4dece7f508b1
Matthew Morgan <matt@dulcetsoftware.com>
2015-11-13 17:17:39 -0500

    new test to ensure calling .date() multiple times doesn't mutate value

--- gigasecond/gigasecond.spec.js
+++ gigasecond/gigasecond.spec.js
@@ -3,21 +3,27 @@ import Gigasecond from './gigasecond';
 describe('Gigasecond', () => {
 
   it('tells a gigasecond anniversary since midnight', () => {
-    let gs = new Gigasecond(new Date(Date.UTC(2015, 8, 14)));
-    let expectedDate = new Date(Date.UTC(2047, 4, 23, 1, 46, 40));
+    const gs = new Gigasecond(new Date(Date.UTC(2015, 8, 14)));
+    const expectedDate = new Date(Date.UTC(2047, 4, 23, 1, 46, 40));
     expect(gs.date()).toEqual(expectedDate);
   });
-  
+
   xit('tells the anniversary is next day when you are born at night', () => {
-    let gs = new Gigasecond(new Date(Date.UTC(2015, 8, 14, 23, 59, 59)));
-    let expectedDate = new Date(Date.UTC(2047, 4, 24, 1, 46, 39));
+    const gs = new Gigasecond(new Date(Date.UTC(2015, 8, 14, 23, 59, 59)));
+    const expectedDate = new Date(Date.UTC(2047, 4, 24, 1, 46, 39));
     expect(gs.date()).toEqual(expectedDate);
   });
 
   xit('even works before 1970 (beginning of Unix epoch)', () => {
-    let gs = new Gigasecond(new Date(Date.UTC(1959, 6, 19, 5, 13, 45)));
-    let expectedDate = new Date(Date.UTC(1991, 2, 27, 7, 0, 25));
+    const gs = new Gigasecond(new Date(Date.UTC(1959, 6, 19, 5, 13, 45)));
+    const expectedDate = new Date(Date.UTC(1991, 2, 27, 7, 0, 25));
     expect(gs.date()).toEqual(expectedDate);
   });
 
+  xit('make sure calling "date" doesn\'t mutate value', () => {
+    const gs = new Gigasecond(new Date(Date.UTC(1959, 6, 19, 5, 13, 45)));
+    const expectedDate = new Date(Date.UTC(1991, 2, 27, 7, 0, 25));
+    gs.date();
+    expect(gs.date()).toEqual(expectedDate);
+  });
 });
