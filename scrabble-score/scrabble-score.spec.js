f6dc0c8d45bf214f70f6be258f5bfe75917d4cac
Matthew Morgan <matt@dulcetsoftware.com>
2015-11-16 19:34:54 -0500

    new exercise scrabble-score added

---
+++ scrabble-score/scrabble-score.spec.js
@@ -0,0 +1,16 @@
+import score from './scrabble-score';
+
+describe('Scrabble', function() {
+  it('scores an empty word as zero',() => expect(score('')).toEqual(0));
+
+  xit('scores a null as zero',() => expect(score(null)).toEqual(0));
+
+  xit('scores a very short word',() => expect(score('a')).toEqual(1));
+
+  xit('scores the word by the number of letters', () => expect(score('street')).toEqual(6));
+
+  xit('scores more complicated words with more',() => expect(score('quirky')).toEqual(22));
+
+  xit('scores case insensitive words',() => expect(score('MULTIBILLIONAIRE')).toEqual(20));
+
+});
