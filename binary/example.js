4e27ee4569aa69d9058ff0109c8ac34411517580
Matthew Morgan <matt@dulcetsoftware.com>
2015-11-18 05:05:09 -0500

    new exercise binary

---
+++ binary/example.js
@@ -0,0 +1,13 @@
+//classy solution, eh?
+
+class Binary{
+  constructor(binary){
+    this.binary = binary.match(/^[01]*$/) ? parseInt(binary, 2) : 0;
+  }
+  toDecimal() {
+    const out = Number(this.binary.toString(10));
+    return isNaN(out) ? 0 : out;
+  }
+}
+
+export default Binary;
