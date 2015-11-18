703f5286813f3e7f9708b0598ea015dfc05fe2e5
Matthew Morgan <matt@dulcetsoftware.com>
2015-11-18 09:00:18 -0500

    new exercise prime-factors

---
+++ prime-factors/example.js
@@ -0,0 +1,19 @@
+class PrimeFactors {
+  for (num) {
+    const factors=[];
+    let currentFactor = 2;
+
+    while (num !== 1) {
+      if (num % currentFactor === 0) {
+        factors.push(currentFactor);
+        num /= currentFactor;
+        currentFactor = 2;
+      } else {
+        currentFactor++;
+      }
+    }
+    return factors;
+  }
+};
+
+export default PrimeFactors;
