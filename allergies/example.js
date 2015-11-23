c399c8ce78606816c4ade58caeeacec2de13f898
Rubén Chavarría <txingo@gmail.com>
2015-11-23 23:19:06 +0100

    Add new exercise: Allergies

---
+++ allergies/example.js
@@ -0,0 +1,29 @@
+const possibleAllergies = [
+  'eggs',
+  'peanuts',
+  'shellfish',
+  'strawberries',
+  'tomatoes',
+  'chocolate',
+  'pollen',
+  'cats'
+]; 
+
+class Allergies {
+  constructor(allergenIndex) {
+    this.allergenIndex = allergenIndex;
+  }
+
+  list() {
+    return possibleAllergies.filter((allergy, i) => {
+      return this.allergenIndex & Math.pow(2, i);
+    });
+  }
+
+  allergicTo(food) {
+    return this.list().some(allergy => allergy === food);
+  }
+}
+
+export default Allergies;
+
