1fdf61c11966ad0b8cab6f1eafbc00bb619c931e
Matthew Morgan <matt@dulcetsoftware.com>
2015-11-30 14:43:06 -0500

    Add new exercise octal

---
+++ octal/example.js
@@ -0,0 +1,6 @@
+export default function (octal) {
+  octal = octal.match(/[^0-7]/) ? octal = '0' : octal;
+  return {
+    toDecimal: () => octal.split('').reduce((prev, curr) => prev * 8 + parseInt(curr), 0)
+  };
+};
