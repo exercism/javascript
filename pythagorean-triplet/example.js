15b8b780dfe87179a3c012fbe49850f41b28dafa
Matthew Morgan <matt@dulcetsoftware.com>
2015-12-01 21:46:19 -0500

    Add new exercise pythagorean-triplet

---
+++ pythagorean-triplet/example.js
@@ -0,0 +1,53 @@
+export default class Triplet {
+
+  constructor(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+
+  isPythagorean() {
+    return this.a * this.a + this.b * this.b === this.c * this.c;
+  }
+
+  sum() {
+    return this.a + this.b + this.c;
+  }
+
+  product() {
+    return this.a * this.b * this.c;
+  }
+
+  static where(conditions) {
+    return new Triplets(conditions).toArray();
+  }
+}
+
+class Triplets {
+
+  constructor(conditions) {
+    this.min = conditions.minFactor || 1;
+    this.max = conditions.maxFactor;
+    this.sum = conditions.sum;
+  }
+
+  toArray() {
+    let triplet;
+    const triplets = [];
+    for (let a = this.min; a < this.max - 1; a++) {
+      for (let b = a + 1; b < this.max; b++) {
+        for (let c = b + 1; c <= this.max; c++) {
+          triplet = new Triplet(a, b, c);
+          if (this.isDesired(triplet)) {
+            triplets.push(triplet);
+          }
+        }
+      }
+    }
+    return triplets;
+  }
+
+  isDesired(triplet) {
+    return triplet.isPythagorean() && (!this.sum || triplet.sum() === this.sum);
+  }
+}
---
+++ pythagorean-triplet/pythagorean-triplet.spec.js
@@ -0,0 +1,45 @@
+import Triplet from './pythagorean-triplet';
+
+describe('Triplet', () => {
+
+  it('calculates the sum', () => {
+    expect(new Triplet(3, 4, 5).sum()).toBe(12);
+  });
+
+  xit('calculates the product', () => {
+    expect(new Triplet(3, 4, 5).product()).toBe(60);
+  });
+
+  xit('can recognize a pythagorean triplet', () => {
+    expect(new Triplet(3, 4, 5).isPythagorean()).toBe(true);
+  });
+
+  xit('can recognize a non pythagorean triplet', () => {
+    expect(new Triplet(5, 6, 7).isPythagorean()).toBe(false);
+  });
+
+  xit('can make triplets up to 10', () => {
+    const triplets = Triplet.where({ maxFactor: 10 });
+    const products = triplets.sort().map(triplet => {
+      return triplet.product();
+    });
+    expect(products).toEqual([60, 480]);
+  });
+
+  xit('can make triplets 11 through 20', () => {
+    const triplets = Triplet.where({ minFactor: 11, maxFactor: 20 });
+    const products = triplets.sort().map(triplet => {
+      return triplet.product();
+    });
+    expect(products).toEqual([3840]);
+  });
+
+  xit('can filter on sum', () => {
+    const triplets = Triplet.where({ sum: 180, maxFactor: 100 });
+    const products = triplets.sort().map(triplet => {
+      return triplet.product();
+    });
+    expect(products).toEqual([118080, 168480, 202500]);
+  });
+
+});
