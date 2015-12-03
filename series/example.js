4a8a9a1618d1f80a4a636f8d15d74c9199c05f78
Matthew Morgan <matt@dulcetsoftware.com>
2015-12-03 17:15:25 -0500

    Add new exercise series

---
+++ series/example.js
@@ -0,0 +1,30 @@
+export default class Series {
+
+  constructor(numberString) {
+    this.numberString = numberString;
+    this.digits = this.getDigits();
+  }
+
+  getDigits() {
+    return [...this.numberString].map(digit => parseInt(digit, 10));
+  }
+
+  slices(sliceSize) {
+    const result = [];
+    let slice = [];
+
+    if (sliceSize > this.digits.length) {
+      throw new Error('Slice size is too big.');
+    }
+
+    for (let i = 0; i < this.digits.length - sliceSize + 1; i++) {
+      for (let j = 0; j < sliceSize; j++) {
+        slice.push(this.digits[i + j]);
+      }
+      result.push(slice);
+      slice = [];
+    }
+
+    return result;
+  }
+}
