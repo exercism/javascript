4ebc5f79fcf753f822de72434ec0b9147f09e369
Rubén Chavarría <txingo@gmail.com>
2015-12-05 17:54:13 +0100

    Add new exercise: Kindergarten garden

---
+++ kindergarten-garden/example.js
@@ -0,0 +1,49 @@
+const defaultChildren = [
+  'Alice',
+  'Bob',
+  'Charlie',
+  'David',
+  'Eve',
+  'Fred',
+  'Ginny',
+  'Harriet',
+  'Ileana',
+  'Joseph',
+  'Kincaid',
+  'Larry'
+];
+
+const plants = {
+  G: 'grass',
+  V: 'violets',
+  R: 'radishes',
+  C: 'clover'
+};
+
+function getPlants(pots, index) {
+  let plants = [];
+  const position = 2*index;
+  plants.push(pots[0][position]);
+  plants.push(pots[0][position+1]);
+  plants.push(pots[1][position]);
+  plants.push(pots[1][position+1]);
+  return plants;
+}
+
+function parse(diagram) {
+  return diagram.split('\n').map((row) => [...row].map((sign) => plants[sign]));
+}
+
+class Garden {
+  constructor(diagram, students) {
+    this.students = students || defaultChildren;
+    this.students.sort();
+
+    this.students.forEach((student, index) => {
+      this[student.toLowerCase()] = getPlants(parse(diagram), index);
+    });
+  }
+}
+
+export default Garden;
+
