9a080e748692ed31047a1f5801591a2ce2263874
Rubén Chavarría <txingo@gmail.com>
2015-12-07 14:12:44 +0100

    Add new exercise: Binary search tree

---
+++ binary-search-tree/binary-search-tree.spec.js
@@ -0,0 +1,89 @@
+import BinarySearchTree from './binary-search-tree';
+
+function recordAllData(bst) {
+  let out = [];
+
+  bst.each((data) => out.push(data));
+
+  return out;
+}
+
+describe('BinarySearchTree', () => {
+
+  it('data is retained', () => {
+    expect(new BinarySearchTree(4).data).toEqual(4);
+  });
+
+  xit('inserting less', () => {
+    let four = new BinarySearchTree(4);
+    four.insert(2);
+
+    expect(four.data).toEqual(4);
+    expect(four.left.data).toEqual(2);
+  });
+
+  xit('inserting same', () => {
+    let four = new BinarySearchTree(4);
+    four.insert(4);
+
+    expect(four.data).toEqual(4);
+    expect(four.left.data).toEqual(4);
+  });
+
+  xit('inserting right', () => {
+    let four = new BinarySearchTree(4);
+    four.insert(5);
+
+    expect(four.data).toEqual(4);
+    expect(four.right.data).toEqual(5);
+  });
+
+  xit('complex tree', () => {
+    let four = new BinarySearchTree(4);
+    four.insert(2);
+    four.insert(6);
+    four.insert(1);
+    four.insert(3);
+    four.insert(7);
+    four.insert(5);
+
+    expect(four.data).toEqual(4);
+    expect(four.left.data).toEqual(2);
+    expect(four.left.left.data).toEqual(1);
+    expect(four.left.right.data).toEqual(3);
+    expect(four.right.data).toEqual(6);
+    expect(four.right.left.data).toEqual(5);
+    expect(four.right.right.data).toEqual(7);
+  });
+
+  xit('iterating one element', () => {
+    expect(recordAllData(new BinarySearchTree(4))).toEqual([ 4 ]);
+  });
+
+  xit('iterating over smaller element', () => {
+    let four = new BinarySearchTree(4);
+    four.insert(2);
+
+    expect(recordAllData(four)).toEqual([ 2, 4 ]);
+  });
+
+  xit('iterating over larger element', () => {
+    let four = new BinarySearchTree(4);
+    four.insert(5);
+
+    expect(recordAllData(four)).toEqual([ 4, 5 ]);
+  });
+
+  xit('iterating over complex tree', () => {
+    let four = new BinarySearchTree(4);
+    four.insert(2);
+    four.insert(1);
+    four.insert(3);
+    four.insert(6);
+    four.insert(7);
+    four.insert(5);
+
+    expect(recordAllData(four)).toEqual([ 1, 2, 3, 4, 5, 6, 7 ]);
+  });
+
+});
---
+++ binary-search-tree/example.js
@@ -0,0 +1,50 @@
+class BinarySearchTree {
+  constructor(data) {
+    this.data  = data;
+    this.left  = undefined;
+    this.right = undefined;
+  }
+
+  insert(value) {
+    if (value <= this.data) {
+      this.insertLeft(value);
+    } else {
+      this.insertRight(value);
+    }
+
+    return this;
+  }
+
+  insertLeft(value) {
+    if (!this.left) {
+      this.left = new BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+
+    return this;
+  }
+
+  insertRight(value) {
+    if (!this.right) {
+      this.right = new BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+
+    return this;
+  }
+
+  each(fn) {
+    if (this.left) {
+      this.left.each(fn);
+    }
+    fn.call(this, this.data);
+    if (this.right) {
+      this.right.each(fn);
+    }
+  }
+}
+
+export default BinarySearchTree;
+
