53bd6196cb7c39c918b527ff349b53a590b7192e
Rubén Chavarría <txingo@gmail.com>
2016-02-16 16:24:43 +0100

    Disable all tests but not the first one

--- exercises/acronym/acronym.spec.js
+++ exercises/acronym/acronym.spec.js
@@ -5,23 +5,23 @@ describe('Acronyms are produced from', ()=>{
     expect(Acronyms.parse('Portable Network Graphics')).toEqual('PNG');
   });
 
-  it('other title cased phrases', () => {
+  xit('other title cased phrases', () => {
     expect(Acronyms.parse('Ruby on Rails')).toEqual('ROR');
   });
 
-  it('inconsistently cased phrases', ()=>{
+  xit('inconsistently cased phrases', ()=>{
     expect(Acronyms.parse('HyperText Markup Language')).toEqual('HTML');
   });
 
-  it('phrases with punctuation', () => {
+  xit('phrases with punctuation', () => {
     expect(Acronyms.parse('First In, First Out')).toEqual('FIFO');
   });
 
-  it('other phrases with punctuation', () => {
+  xit('other phrases with punctuation', () => {
     expect(Acronyms.parse('PHP: Hypertext Preprocessor')).toEqual('PHP');
   });
 
-  it('phrases with punctuation and sentence casing', () => {
+  xit('phrases with punctuation and sentence casing', () => {
     expect(Acronyms.parse('Complementary metal-oxide semiconductor')).toEqual('CMOS');
   });
 });
