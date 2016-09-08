3bab0a130208a36e52083895477485ab9e08d127
Roshan Jossey <roshan.j@multunus.com>
2016-09-08 20:02:58 +0530

    Add isogram problem to exercises

---
+++ exercises/isogram/example.js
@@ -0,0 +1,11 @@
+export default class Isogram {
+
+  constructor(string) {
+    this.string = string.replace(/ |-/g, '');
+  }
+
+  isIsogram() {
+    const uniqueLetters = this.string.toLowerCase().split('').filter((element, index, self) => self.indexOf(element) === index);
+    return uniqueLetters.length === this.string.length;
+  }
+}
---
+++ exercises/isogram/isogram.spec.js
@@ -0,0 +1,64 @@
+import Isogram from './isogram.js'
+
+describe('Isogram Test Suite', function () {
+  it('duplicates', function () {
+    var word = new Isogram('duplicates');
+
+    expect(word.isIsogram()).toEqual(true);
+  });
+
+  xit('eleven', function () {
+    var word = new Isogram('eleven');
+
+    expect(word.isIsogram()).toEqual(false);
+  });
+
+  xit('subdermatoglyphic', function () {
+    var word = new Isogram('subdermatoglyphic');
+
+    expect(word.isIsogram()).toEqual(true);
+  });
+
+  xit('Alphabet', function () {
+    var word = new Isogram('Alphabet');
+
+    expect(word.isIsogram()).toEqual(false);
+  });
+
+  xit('thumbscrew-japingly', function () {
+    var word = new Isogram('thumbscrew-japingly');
+
+    expect(word.isIsogram()).toEqual(true);
+  });
+
+  xit('Hjelmqvist-Gryb-Zock-Pfund-Wax', function () {
+    var word = new Isogram('Hjelmqvist-Gryb-Zock-Pfund-Wax');
+
+    expect(word.isIsogram()).toEqual(true);
+  });
+
+  xit('Heizölrückstoßabdämpfung', function () {
+    var word = new Isogram('Heizölrückstoßabdämpfung');
+
+    expect(word.isIsogram()).toEqual(true);
+  });
+
+  xit('the quick brown fox', function () {
+    var word = new Isogram('the quick brown fox');
+
+    expect(word.isIsogram()).toEqual(false);
+  });
+
+  xit('Emily Jung Schwartzkopf', function () {
+    var word = new Isogram('Emily Jung Schwartzkopf');
+
+    expect(word.isIsogram()).toEqual(true);
+  });
+
+  xit('éléphant', function () {
+    var word = new Isogram('éléphant');
+
+    expect(word.isIsogram()).toEqual(false);
+  });
+
+});
