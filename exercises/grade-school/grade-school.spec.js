1b020dd09892ddccaeb1377d72711778864da030
mgmatola <matola@sbcglobal.net>
2016-10-11 21:07:11 -0400

    Add test to grade-school

--- exercises/grade-school/grade-school.spec.js
+++ exercises/grade-school/grade-school.spec.js
@@ -70,4 +70,11 @@ describe('School', () => {
     expect(school.roster()).toEqual(expectedDb);
   });
 
+  xit('roster cannot be modified outside of module using grade()', () => {
+    school.add('Aimee', 2);
+    school.grade(2).push('Oops.');
+    const expectedDb = { 2 : [ 'Aimee' ] };
+    expect(school.roster()).toEqual(expectedDb);
+  });
+
 });
