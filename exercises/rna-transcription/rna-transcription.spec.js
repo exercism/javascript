158b712f09f592f0e20109ec799fca9ddf1e4ddd
Tejas Bubane <tejasbubane@gmail.com>
2016-12-02 21:36:49 +0530

    Rna-Transcription: Handle invalid nucleotides in strand
    As per [x-common canonical
    data](https://github.com/exercism/x-common/blob/master/exercises/rna-transcription/canonical-data.json#L40-L54)
    but instead raising an error if invalid DNA is passed.
    
    Fixes #248

--- exercises/rna-transcription/rna-transcription.spec.js
+++ exercises/rna-transcription/rna-transcription.spec.js
@@ -24,4 +24,22 @@ describe('Transcriptor', () => {
         .toEqual('UGCACCAGAAUU');
   });
 
+  xit('correctly handles invalid input', () => {
+    expect(() => transcriptor.toRna('U')).toThrow(
+      new Error('Invalid input DNA.')
+    );
+  });
+
+  xit('correctly handles completely invalid input', () => {
+    expect(() => transcriptor.toRna('XXX')).toThrow(
+      new Error('Invalid input DNA.')
+    );
+  });
+
+  xit('correctly handles partially invalid input', () => {
+    expect(() => transcriptor.toRna('ACGTXXXCTTAA')).toThrow(
+      new Error('Invalid input DNA.')
+    );
+  });
+
 });
