bc052157858002db8a1b5972df7f0e911bcc1dbc
Dmitry Noranovich <javaeeeee@gmail.com>
2016-12-08 08:25:14 -0500

    Added all-your-base to the ECMAscript track

---
+++ exercises/all-your-base/example.js
@@ -0,0 +1,55 @@
+const isValidBase = function (base) {
+  return !base || base < 2 || Math.floor(base) !== base;
+};
+
+const isInputValid = function (array, base) {
+  if (!array || !array.length) {
+    return false;
+  }
+  const val = base - 1;
+  for (let i = 0, n = array.length; i < n; i++) {
+    const tmp = array[i];
+    if (tmp > val || tmp < 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const convertFromDecimalToBase = function (num, outputBase) {
+  let tmp = num;
+  const result = [];
+  while (tmp) {
+    result.unshift(tmp % outputBase);
+    tmp = Math.floor(tmp / outputBase);
+  }
+  return result;
+};
+
+export default class Converter {
+  convert(array, inputBase, outputBase) {
+    if (isValidBase(inputBase)) {
+      throw new Error('Wrong input base');
+    }
+    if (isValidBase(outputBase)) {
+      throw new Error('Wrong output base');
+    }
+    const regexp = new RegExp('^0.', 'g');
+    const str = array.join('');
+    if (str.match(regexp)
+      || !isInputValid(array, inputBase)) {
+      throw new Error('Input has wrong format');
+    }
+    if (str === '0') {
+      return [0];
+    }
+    if (str === '1') {
+      return [1];
+    }
+    const decimalValue = array
+      .reduce((accumulator, value) => {
+        return accumulator * inputBase + value;
+      }, 0);
+    return convertFromDecimalToBase(decimalValue, outputBase);
+  }
+}
