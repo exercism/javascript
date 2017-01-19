cef16e96195c6fb76d66c059307ee9e358c18062
Rubén Chavarría <txingo@gmail.com>
2017-01-19 12:24:15 +0100

    Synchronize Luhn exercise with canonical specs
    Canonical specs for Luhn exercise were changed, and tracks implementing that exercise should synchronize wih them.
    
    Closes #260

--- exercises/luhn/example.js
+++ exercises/luhn/example.js
@@ -1,57 +1,34 @@
+function isValid(number) {
+  number = number.replace(/\s/g, '');
+  const digits = [...number];
+
+  const sum = digits
+    // convert to integers
+    .map(d => parseInt(d, 10))
+    // double even positions (odd indexes)
+    .map((d, i) => {
+      if (i % 2 !== 0) {
+        return d * 2;
+      }
+      return d;
+    })
+    // limit to digits less than 10
+    .map(d => {
+      if (d > 9) {
+        return d - 9;
+      }
+      return d;
+    })
+    // sum all digits
+    .reduce((d, acc) => d + acc, 0);
+
+  return sum > 0 && sum % 10 === 0;
+}
+
 export default class Luhn {
 
   constructor(number) {
-    this.checkDigit = number % 10;
-    this.addends = Luhn.calculateAddends(number);
-    this.checksum = Luhn.calculateChecksum(this.addends);
-    this.valid = Luhn.determineIfValid(this.checksum);
+    this.valid = isValid(number);
   }
 
-  static calculateAddends(number) {
-    const numberAsString = '' + number + '',
-      numbers = [...numberAsString],
-      addends = [];
-
-    for (let i = 0; i < numbers.length; i++) {
-      const index = numbers.length - 1 - i;
-      let currentAddend = parseInt(numbers[index], 10);
-
-      if ((i + 1) % 2 === 0) {
-        currentAddend = currentAddend * 2;
-        if (currentAddend > 10) {
-          currentAddend = currentAddend - 9;
-        }
-      }
-      addends.push(currentAddend);
-    }
-    return addends.reverse();
-  }
-
-  static calculateChecksum(numbers) {
-    let sum = 0;
-    for (let i = 0; i < numbers.length; i++) {
-      sum += numbers[i];
-    }
-    return sum;
-  }
-
-  static determineIfValid(sum) {
-    return sum % 10 === 0;
-  }
-
-  static create(number) {
-    let finalNumber = number * 10,
-      luhnNumber = new Luhn(finalNumber),
-      index = 0;
-
-    while (!luhnNumber.valid) {
-      finalNumber = number * 10 + index;
-      luhnNumber = new Luhn(finalNumber);
-      if (luhnNumber.valid) {
-        break;
-      }
-      index += 1;
-    }
-    return finalNumber;
-  }
 }
