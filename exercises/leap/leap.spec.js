f6f401e4e376f7c7a1b8ba6ffd331359f4874af1
Tejas Bubane <tejasbubane@gmail.com>
2017-04-26 21:17:54 +0530

    Remove extra specs from leap
    Keep only what are absolute necessary, maintain consistency with x-common.

--- exercises/leap/leap.spec.js
+++ exercises/leap/leap.spec.js
@@ -2,49 +2,24 @@ import Year from './leap';
 
 describe('A leap year', () => {
 
-  it('is not very common', () => {
+  it('year not divisible by 4: common year', () => {
     let year = new Year(2015);
     expect(year.isLeap()).toBeFalsy();
   });
 
-  xit('is introduced every 4 years to adjust about a day', () => {
+  xit('year divisible by 4, not divisible by 100: leap year', () => {
     let year = new Year(2016);
     expect(year.isLeap()).toBeTruthy();
   });
 
-  xit('is skipped every 100 years to remove an extra day', () => {
-    let year = new Year(1900);
+  xit('year divisible by 100, not divisible by 400: common year', () => {
+    let year = new Year(2100);
     expect(year.isLeap()).toBeFalsy();
   });
 
-  xit('is reintroduced every 400 years to adjust another day', () => {
+  xit('year divisible by 400: leap year', () => {
     let year = new Year(2000);
     expect(year.isLeap()).toBeTruthy();
-  });
-
-  // Feel free to enable the following tests to check some more examples
-  xdescribe('Additional example of a leap year that', () => {
-
-    it('is not a leap year', () => {
-      let year = new Year(1978);
-      expect(year.isLeap()).toBeFalsy();
-    });
-
-    it('is a common leap year', () => {
-      let year = new Year(1992);
-      expect(year.isLeap()).toBeTruthy();
-    });
-
-    it('is skipped every 100 years', () => {
-      let year = new Year(2100);
-      expect(year.isLeap()).toBeFalsy();
-    });
-
-    it('is reintroduced every 400 years', () => {
-      let year = new Year(2400);
-      expect(year.isLeap()).toBeTruthy();
-    });
-
   });
 
 });
