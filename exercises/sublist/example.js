6c4de9d3d81719419240fe9b7ba0255d4674189e
Matthew Morgan <mail.matt.morgan@gmail.com>
2017-07-17 13:14:24 -0400

    Add exercise sublist

---
+++ exercises/sublist/example.js
@@ -0,0 +1,30 @@
+class List {
+
+  constructor(list = []) {
+    this.list = list;
+  }
+
+  compare(other) {
+    return {
+      '-1': isSublist(other.list, this.list)
+              ? 'SUBLIST'
+              : 'UNEQUAL',
+      '0':  isSublist(other.list, this.list)
+              ? 'EQUAL'
+              : 'UNEQUAL',
+      '1':  isSublist(this.list, other.list)
+              ? 'SUPERLIST'
+              : 'UNEQUAL'
+    }[lengthDiff(this, other)];
+  }
+}
+
+function lengthDiff(one, two){
+  return String(Math.sign(one.list.length - two.list.length));
+}
+
+function isSublist(one, two){
+  return one.join().match(two.join());
+}
+
+export default List;
