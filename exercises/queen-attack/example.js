b09c7641624d4a78c829f79bc86a4d41ba02d2e4
= <alexashley15@gmail.com>
2017-08-24 19:31:42 -0500

    queen-attack: force test failure when exception is not thrown

--- exercises/queen-attack/example.js
+++ exercises/queen-attack/example.js
@@ -1,10 +1,34 @@
-const W = 8,
-  H = 8,
-  STARTING = { black: [7, 3], white: [0, 3] };
+const W = 8;
+const H = 8;
+const STARTING = { black: [7, 3], white: [0, 3] };
+
+function samePosition({ white, black }) {
+  return white[0] === black[0] && white[1] === black[1];
+}
+
+function buildRow(cell, colCount) {
+  return Array(...Array(colCount)).map(() => cell);
+}
+
+function concatRows(row, rowCount) {
+  return [...Array.prototype.concat.apply(buildRow(row, rowCount)).join('')];
+}
+
+function constructBoard() {
+  let row = buildRow('_ ', W).join('');
+  row = `${row.substring(0, row.length - 1)}\n`;
+  return concatRows(row, H);
+}
+
+function placePieces(self) {
+  const board = self.board;
+  board[(self.black[0] * W * 2) + (self.black[1] * 2)] = 'B';
+  board[(self.white[0] * W * 2) + (self.white[1] * 2)] = 'W';
+}
 
 const QueenAttack = (params = STARTING) => {
   const self = this instanceof QueenAttack ? this : Object.getPrototypeOf(QueenAttack);
-  if (params && params.white === params.black) {
+  if (samePosition(params)) {
     throw new Error('Queens cannot share the same space');
   }
 
@@ -24,24 +48,5 @@ const QueenAttack = (params = STARTING) => {
 
   return self;
 };
-
-function constructBoard() {
-  let row = buildRow('_ ', W).join('');
-  row = `${row.substring(0, row.length - 1)}\n`;
-  return concatRows(row, H);
-}
-
-function placePieces(self) {
-  self.board[self.black[0] * W * 2 + self.black[1] * 2] = 'B';
-  self.board[self.white[0] * W * 2 + self.white[1] * 2] = 'W';
-}
-
-function buildRow(cell, colCount) {
-  return Array(...Array(colCount)).map(() => cell);
-}
-
-function concatRows(row, rowCount) {
-  return [...Array.prototype.concat.apply(buildRow(row, rowCount)).join('')];
-}
 
 export default QueenAttack;
