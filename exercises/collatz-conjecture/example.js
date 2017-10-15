4db1a1fa75e0b70b7ec3bc4b6ed6c27eb7fcf5e2
al-pakhomov <lx.pakhomov@gmail.com>
2017-10-15 19:42:53 +0300

    Implement exercise collatz-conjecture

---
+++ exercises/collatz-conjecture/example.js
@@ -0,0 +1,14 @@
+export default (n) => {
+  if (n <= 0) {
+    throw new Error('Only positive numbers are allowed');
+  }
+
+  const iterate = (number, step) => {
+    if (number === 1) {
+      return step;
+    }
+
+    return number % 2 === 0 ? iterate(number / 2, step + 1) : iterate((3 * number) + 1, step + 1);
+  };
+  return iterate(n, 0);
+};
