6be7814e3e6e22bcb8d4de856fafb5f7b6658ee3
Matt H <4052774+MattH-be@users.noreply.github.com>
2017-10-28 00:41:57 +0200

    Implement exercise complex-numbers
    Grouped all tests under a single describe
    
    Ordered the tests in a more logical way.
    All tests except the first one are skipped.
    
    Removed mention of enabling tests by changing xdescribe to describe.
    
    Implement exercise isbn-verifier (#366)
    
    [Housekeeping] Upgrade jest (#368)
    
    * Use `package-lock.json` instead of `yarn.lock`
    
    * Upgrade jest and babel-jest to latest versions
    
    Jest to 21.2.1
    babel-jest to 21.2.0
    
    Change Exercise is now ECMAScript ready (#362)
    
    Transpose Exercise is ECMAScript ready (#364)
    
    Upgrade jest
    
    Protein Translation is ECMAScript ready (#365) (#365)
    
    Update  setup links in all READMEs (#371)
    
    Update all setup links in exercise READMEs from http://exercism.io/languages/ecmascript to http://exercism.io/languages/ecmascript/installation
    
    Closes #370
    
    Updated setup link

---
+++ exercises/complex-numbers/complex-numbers.spec.js
@@ -0,0 +1,216 @@
+import ComplexNumber from './complex-numbers.js';
+
+describe('Complex numbers', () => {
+  test('Real part of a purely real number', () => {
+    const expected = 1;
+    const actual = new ComplexNumber(1, 0).real;
+
+    expect(actual).toEqual(expected);
+  });
+
+  xtest('Real part of a purely imaginary number', () => {
+    const expected = 0;
+    const actual = new ComplexNumber(0, 1).real;
+
+    expect(actual).toEqual(expected);
+  });
+
+  xtest('Real part of a number with real and imaginary part', () => {
+    const expected = 1;
+    const actual = new ComplexNumber(1, 2).real;
+
+    expect(actual).toEqual(expected);
+  });
+
+  xtest('Imaginary part of a purely real number', () => {
+    const expected = 0;
+    const actual = new ComplexNumber(1, 0).imag;
+
+    expect(actual).toEqual(expected);
+  });
+
+  xtest('Imaginary part of a purely imaginary number', () => {
+    const expected = 1;
+    const actual = new ComplexNumber(0, 1).imag;
+
+    expect(actual).toEqual(expected);
+  });
+
+  xtest('Imaginary part of a number with real and imaginary part', () => {
+    const expected = 2;
+    const actual = new ComplexNumber(1, 2).imag;
+
+    expect(actual).toEqual(expected);
+  });
+  
+  xtest('Add purely real numbers', () => {
+    const expected = new ComplexNumber(3, 0);
+    const actual = new ComplexNumber(1, 0).add(new ComplexNumber(2, 0));
+
+    expect(actual).toEqual(expected);
+  });
+
+  xtest('Add purely imaginary numbers', () => {
+    const expected = new ComplexNumber(0, 3);
+    const actual = new ComplexNumber(0, 1).add(new ComplexNumber(0, 2));
+
+    expect(actual).toEqual(expected);
+  });
+
+  xtest('Add numbers with real and imaginary part', () => {
+    const expected = new ComplexNumber(4, 6);
+    const actual = new ComplexNumber(1, 2).add(new ComplexNumber(3, 4));
+
+    expect(actual).toEqual(expected);
+  });
+
+  xtest('Subtract purely real numbers', () => {
+    const expected = new ComplexNumber(-1, 0);
+    const actual = new ComplexNumber(1, 0).sub(new ComplexNumber(2, 0));
+
+    expect(actual).toEqual(expected);
+  });
+
+  xtest('Subtract purely imaginary numbers', () => {
+    const expected = new ComplexNumber(0, -1);
+    const actual = new ComplexNumber(0, 1).sub(new ComplexNumber(0, 2));
+
+    expect(actual).toEqual(expected);
+  });
+
+  xtest('Subtract numbers with real and imaginary part', () => {
+    const expected = new ComplexNumber(-2, -2);
+    const actual = new ComplexNumber(1, 2).sub(new ComplexNumber(3, 4));
+
+    expect(actual).toEqual(expected);
+  });
+
+  xtest('Multiply purely real numbers', () => {
+    const expected = new ComplexNumber(2, 0);
+    const actual = new ComplexNumber(1, 0).mul(new ComplexNumber(2, 0));
+
+    expect(actual).toEqual(expected);
+  });
+
+  xtest('Multiply imaginary unit', () => {
+    const expected = new ComplexNumber(-1, 0);
+    const actual = new ComplexNumber(0, 1).mul(new ComplexNumber(0, 1));
+
+    expect(actual).toEqual(expected);
+  });
+
+  xtest('Multiply purely imaginary numbers', () => {
+    const expected = new ComplexNumber(-2, 0);
+    const actual = new ComplexNumber(0, 1).mul(new ComplexNumber(0, 2));
+
+    expect(actual).toEqual(expected);
+  });
+
+  xtest('Multiply numbers with real and imaginary part', () => {
+    const expected = new ComplexNumber(-5, 10);
+    const actual = new ComplexNumber(1, 2).mul(new ComplexNumber(3, 4));
+
+    expect(actual).toEqual(expected);
+  });
+
+  xtest('Divide purely real numbers', () => {
+    const expected = new ComplexNumber(0.5, 0);
+    const actual = new ComplexNumber(1, 0).div(new ComplexNumber(2, 0));
+
+    expect(actual).toEqual(expected);
+  });
+
+  xtest('Divide purely imaginary numbers', () => {
+    const expected = new ComplexNumber(0.5, 0);
+    const actual = new ComplexNumber(0, 1).div(new ComplexNumber(0, 2));
+
+    expect(actual).toEqual(expected);
+  });
+
+  xtest('Divide numbers with real and imaginary part', () => {
+    const expected = new ComplexNumber(0.44, 0.08);
+    const actual = new ComplexNumber(1, 2).div(new ComplexNumber(3, 4));
+
+    expect(actual).toEqual(expected);
+  });
+
+  xtest('Absolute value of a positive purely real number', () => {
+    const expected = 5;
+    const actual = new ComplexNumber(5, 0).abs;
+
+    expect(actual).toEqual(expected);
+  });
+
+  xtest('Absolute value of a negative purely real number', () => {
+    const expected = 5;
+    const actual = new ComplexNumber(-5, 0).abs;
+
+    expect(actual).toEqual(expected);
+  });
+
+  xtest('Absolute value of a purely imaginary number with positive imaginary part', () => {
+    const expected = 5;
+    const actual = new ComplexNumber(0, 5).abs;
+
+    expect(actual).toEqual(expected);
+  });
+
+  xtest('Absolute value of a purely imaginary number with negative imaginary part', () => {
+    const expected = 5;
+    const actual = new ComplexNumber(0, -5).abs;
+
+    expect(actual).toEqual(expected);
+  });
+
+  xtest('Absolute value of a number with real and imaginary part', () => {
+    const expected = 5;
+    const actual = new ComplexNumber(3, 4).abs;
+
+    expect(actual).toEqual(expected);
+  });
+
+  xtest('Conjugate a purely real number', () => {
+    const expected = new ComplexNumber(5, 0);
+    const actual = new ComplexNumber(5, 0).conj;
+
+    expect(actual).toEqual(expected);
+  });
+
+  xtest('Conjugate a purely imaginary number', () => {
+    const expected = new ComplexNumber(0, -5);
+    const actual = new ComplexNumber(0, 5).conj;
+
+    expect(actual).toEqual(expected);
+  });
+
+  xtest('Conjugate a number with real and imaginary part', () => {
+    const expected = new ComplexNumber(1, -1);
+    const actual = new ComplexNumber(1, 1).conj;
+
+    expect(actual).toEqual(expected);
+  });
+
+  xtest('Euler\'s identity/formula', () => {
+    const expected = new ComplexNumber(-1, 0);
+    const actual = new ComplexNumber(0, Math.PI).exp;
+
+    expect(actual.real).toBeCloseTo(expected.real);
+    expect(actual.imag).toBeCloseTo(expected.imag);
+  });
+
+  xtest('Exponential of 0', () => {
+    const expected = new ComplexNumber(1, 0);
+    const actual = new ComplexNumber(0, 0).exp;
+
+    expect(actual.real).toBeCloseTo(expected.real);
+    expect(actual.imag).toBeCloseTo(expected.imag);
+  });
+
+  xtest('Exponential of a purely real number', () => {
+    const expected = new ComplexNumber(Math.E, 0);
+    const actual = new ComplexNumber(1, 0).exp;
+
+    expect(actual.real).toBeCloseTo(expected.real);
+    expect(actual.imag).toBeCloseTo(expected.imag);
+  });
+});
---
+++ exercises/complex-numbers/example.js
@@ -0,0 +1,43 @@
+export default class ComplexNumber {
+
+  constructor(real, imag) {
+    this.real = real;
+    this.imag = imag;
+  }
+
+  add(other) {
+    return new ComplexNumber(this.real + other.real, this.imag + other.imag);
+  }
+
+  sub(other) {
+    return new ComplexNumber(this.real - other.real, this.imag - other.imag);
+  }
+
+  mul(other) {
+    return new ComplexNumber(
+      (this.real * other.real) - (this.imag * other.imag),
+      (this.imag * other.real) + (this.real * other.imag));
+  }
+
+  div(other) {
+    return new ComplexNumber(
+      ((this.real * other.real) + (this.imag * other.imag))
+      / ((other.real * other.real) + (other.imag * other.imag)),
+      ((this.imag * other.real) - (this.real * other.imag))
+      / ((other.real * other.real) + (other.imag * other.imag)));
+  }
+
+  get abs() {
+    return Math.sqrt((this.real * this.real) + (this.imag * this.imag));
+  }
+
+  get conj() {
+    return new ComplexNumber(this.real, this.imag !== 0 ? -this.imag : 0);
+  }
+
+  get exp() {
+    return new ComplexNumber(
+      Math.exp(this.real) * Math.cos(this.imag),
+      Math.exp(this.real) * Math.sin(this.imag));
+  }
+}
