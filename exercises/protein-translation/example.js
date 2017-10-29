816f3a7bb11d63dafb47f620d66e1978ce91c1c2
Robin Cussol <me@robincussol.com>
2017-10-29 14:55:12 +0100

    Protein Translation is ECMAScript ready (#365)

---
+++ exercises/protein-translation/example.js
@@ -0,0 +1,45 @@
+const ACID_PROTEIN_MAP = {
+  AUG: 'Methionine',
+  UUU: 'Phenylalanine',
+  UUC: 'Phenylalanine',
+  UUA: 'Leucine',
+  UUG: 'Leucine',
+  UCU: 'Serine',
+  UCC: 'Serine',
+  UCA: 'Serine',
+  UCG: 'Serine',
+  UAU: 'Tyrosine',
+  UAC: 'Tyrosine',
+  UGU: 'Cysteine',
+  UGC: 'Cysteine',
+  UGG: 'Tryptophan',
+  UAA: 'STOP',
+  UAG: 'STOP',
+  UGA: 'STOP',
+};
+
+const getProtein = codon => ACID_PROTEIN_MAP[codon] || 'INVALID';
+
+export default function translate(rnaStrand) {
+  const proteins = [];
+
+  if (rnaStrand) {
+    for (let i = 0; i < rnaStrand.length; i += 3) {
+      const protein = getProtein(rnaStrand.substring(i, i + 3));
+
+      if (protein) {
+        if (protein === 'STOP') {
+          break;
+        }
+
+        if (protein === 'INVALID') {
+          throw new Error('Invalid codon');
+        }
+
+        proteins.push(protein);
+      }
+    }
+  }
+
+  return proteins;
+}
---
+++ exercises/protein-translation/protein-translation.spec.js
@@ -0,0 +1,59 @@
+import translate from './protein-translation';
+
+describe('ProteinTranslation', () => {
+  test('Empty RNA has no proteins', () => {
+    expect(translate()).toEqual([]);
+  });
+
+  xtest('Methionine codon translates into protein', () => {
+    expect(translate('AUG')).toEqual(['Methionine']);
+  });
+
+  xtest('Phenylalanine codons translate into protein', () => {
+    expect(translate('UUUUUC')).toEqual(['Phenylalanine', 'Phenylalanine']);
+  });
+
+  xtest('Leucine codons translate into protein', () => {
+    expect(translate('UUAUUG')).toEqual(['Leucine', 'Leucine']);
+  });
+
+  xtest('Serine codons translate into protein', () => {
+    expect(translate('UCUUCCUCAUCG')).toEqual(['Serine', 'Serine', 'Serine', 'Serine']);
+  });
+
+  xtest('Tyrosine codons translate into protein', () => {
+    expect(translate('UAUUAC')).toEqual(['Tyrosine', 'Tyrosine']);
+  });
+
+  xtest('Cysteine codons translate into protein', () => {
+    expect(translate('UGUUGC')).toEqual(['Cysteine', 'Cysteine']);
+  });
+
+  xtest('Tryptophan codon translates into protein', () => {
+    expect(translate('UGG')).toEqual(['Tryptophan']);
+  });
+
+  xtest('Sequence starts with stop codon 1', () => {
+    expect(translate('UAAUUUUUA')).toEqual([]);
+  });
+
+  xtest('Sequence starts with stop codon 2', () => {
+    expect(translate('UAGAUGUAU')).toEqual([]);
+  });
+
+  xtest('Sequence starts with stop codon 3', () => {
+    expect(translate('UGAUGU')).toEqual([]);
+  });
+
+  xtest('Small RNA strand', () => {
+    expect(translate('AUGUUUUCU')).toEqual(['Methionine', 'Phenylalanine', 'Serine']);
+  });
+
+  xtest('Stop codon ends translation', () => {
+    expect(translate('AUGUUUUCUUAAAUG')).toEqual(['Methionine', 'Phenylalanine', 'Serine']);
+  });
+
+  xtest('Invalid codon throws error', () => {
+    expect(() => translate('LOL')).toThrow(new Error('Invalid codon'));
+  });
+});
