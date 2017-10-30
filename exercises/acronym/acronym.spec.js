af7b50b42de37df9e1a37cdb122c40af76d2c72b
Gustavo Ribeiro <gu_ribeiro@live.com>
2017-10-30 07:39:57 -0200

    Fix document skipping of tests

--- exercises/acronym/acronym.spec.js
+++ exercises/acronym/acronym.spec.js
@@ -5,23 +5,23 @@ describe('Acronyms are produced from', () => {
     expect(Acronyms.parse('Portable Network Graphics')).toEqual('PNG');
   });
 
-  xtest('other title cased phrases', () => {
+  test('other title cased phrases', () => {
     expect(Acronyms.parse('Ruby on Rails')).toEqual('ROR');
   });
 
-  xtest('inconsistently cased phrases', () => {
+  test('inconsistently cased phrases', () => {
     expect(Acronyms.parse('HyperText Markup Language')).toEqual('HTML');
   });
 
-  xtest('phrases with punctuation', () => {
+  test('phrases with punctuation', () => {
     expect(Acronyms.parse('First In, First Out')).toEqual('FIFO');
   });
 
-  xtest('other phrases with punctuation', () => {
+  test('other phrases with punctuation', () => {
     expect(Acronyms.parse('PHP: Hypertext Preprocessor')).toEqual('PHP');
   });
 
-  xtest('phrases with punctuation and sentence casing', () => {
+  test('phrases with punctuation and sentence casing', () => {
     expect(Acronyms.parse('Complementary metal-oxide semiconductor')).toEqual('CMOS');
   });
 });
