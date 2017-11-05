02427ec1051294ffad46a882dfce89391a3112b3
Robin Cussol <me@robincussol.com>
2017-11-05 10:47:37 +0100

    Transpose Exercise Fixed

---
+++ exercises/transpose/example.js
@@ -0,0 +1,12 @@
+function trimTrailingUndefined(array) {
+  const trailingUndefinedCount = [...array].reverse().findIndex(x => x !== undefined);
+  return array.slice(0, array.length - trailingUndefinedCount);
+}
+
+export default function transpose(input) {
+  const maxCol = Math.max(0, ...(input.map(row => row.length)));
+  return [...Array(maxCol).keys()].map(col =>
+    trimTrailingUndefined(input.map((_v, row) => input[row][col]))
+      .map(charOrUndefined => charOrUndefined || ' ')
+      .join(''));
+}
