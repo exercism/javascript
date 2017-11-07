918b738892e31e2cb6429ed68e07c6389a51e9e2
Matt H <4052774+MattH-be@users.noreply.github.com>
2017-11-08 00:11:49 +0100

    Implement exercise rotational-cipher
    Added missing test
    Use xtest for all tests except first one
    
    Fixed the input of a test. Be careful when replacing test with xtest

---
+++ exercises/rotational-cipher/example.js
@@ -0,0 +1,16 @@
+class RotationalCipher {
+
+  static rotate(text, shift) {
+    return [...text].map((c) => {
+      const isUpper = c.match(/[A-Z]/);
+      const isAlpha = c.match(/[a-z]/i);
+      const charShift = (isUpper ? 'A' : 'a').charCodeAt(0);
+
+      return isAlpha ?
+        String.fromCharCode((((c.charCodeAt(0) - charShift) + shift) % 26) + charShift) : c;
+    }).join('');
+  }
+
+}
+
+export default RotationalCipher;
---
+++ exercises/rotational-cipher/rotational-cipher.spec.js
@@ -0,0 +1,73 @@
+import RotationalCipher from './rotational-cipher';
+
+describe('Rotational cipher', () => {
+  test('rotate a by 0, same output as input', () => {
+    const expected = 'a';
+    const actual = RotationalCipher.rotate('a', 0);
+
+    expect(actual).toEqual(expected);
+  });
+
+  xtest('rotate a by 1', () => {
+    const expected = 'b';
+    const actual = RotationalCipher.rotate('a', 1);
+
+    expect(actual).toEqual(expected);
+  });
+
+  xtest('rotate a by 26, same output as input', () => {
+    const expected = 'a';
+    const actual = RotationalCipher.rotate('a', 26);
+
+    expect(actual).toEqual(expected);
+  });
+
+  xtest('rotate m by 13', () => {
+    const expected = 'z';
+    const actual = RotationalCipher.rotate('m', 13);
+
+    expect(actual).toEqual(expected);
+  });
+
+  xtest('rotate n by 13 with wrap around alphabet', () => {
+    const expected = 'a';
+    const actual = RotationalCipher.rotate('n', 13);
+
+    expect(actual).toEqual(expected);
+  });
+
+  xtest('rotate capital letters', () => {
+    const expected = 'TRL';
+    const actual = RotationalCipher.rotate('OMG', 5);
+
+    expect(actual).toEqual(expected);
+  });
+
+  xtest('rotate spaces', () => {
+    const expected = 'T R L';
+    const actual = RotationalCipher.rotate('O M G', 5);
+
+    expect(actual).toEqual(expected);
+  });
+
+  xtest('rotate numbers', () => {
+    const expected = 'Xiwxmrk 1 2 3 xiwxmrk';
+    const actual = RotationalCipher.rotate('Testing 1 2 3 testing', 4);
+
+    expect(actual).toEqual(expected);
+  });
+
+  xtest('rotate punctuation', () => {
+    const expected = 'Gzo\'n zvo, Bmviyhv!';
+    const actual = RotationalCipher.rotate('Let\'s eat, Grandma!', 21);
+
+    expect(actual).toEqual(expected);
+  });
+
+  xtest('rotate all letters', () => {
+    const expected = 'Gur dhvpx oebja sbk whzcf bire gur ynml qbt.';
+    const actual = RotationalCipher.rotate('The quick brown fox jumps over the lazy dog.', 13);
+
+    expect(actual).toEqual(expected);
+  });
+});
