93b6028fb170dd8224bf43e5050689ca2832ff6f
Matthew Morgan <mail.matt.morgan@gmail.com>
2018-07-20 06:59:29 -0400

    Port exercise Zipper (#425)

---
+++ exercises/zipper/example.js
@@ -0,0 +1,92 @@
+function fromTrail(tree, last) {
+  if (last[0] === 'left') {
+    return {
+      value: last[1],
+      left: tree,
+      right: last[2]
+    };
+  }
+  return {
+    value: last[1],
+    left: last[2],
+    right: tree
+  };
+}
+
+function rebuildTree(tree, trail) {
+  if (trail.length === 0) return tree;
+
+  const last = trail[0];
+  return rebuildTree(fromTrail(tree, last), trail.slice(1));
+}
+
+class Zipper {
+  constructor(tree, trail){
+    this.tree = tree;
+    this.trail = trail;
+  }
+  static fromTree(tree) {
+    return new Zipper(tree, []);
+  }
+  toTree() {
+    return rebuildTree(this.tree, this.trail);
+  }
+  value() {
+    return this.tree.value;
+  }
+  left() {
+    if (!this.tree.left) return null;
+
+    return new Zipper(
+      this.tree.left,
+      [['left', this.tree.value, this.tree.right]].concat(this.trail)
+    );
+  }
+  right() {
+    if (!this.tree.right) return null;
+
+    return new Zipper(
+      this.tree.right,
+      [['right', this.tree.value, this.tree.left]].concat(this.trail)
+    );
+  }
+  up() {
+    if (this.trail.length === 0) return null;
+
+    const last = this.trail[0];
+    return new Zipper(fromTrail(this.tree, last), this.trail.slice(1));
+  }
+  setValue(value) {
+    return new Zipper(
+      {
+        value: value,
+        left: this.tree.left,
+        right: this.tree.right
+      },
+      this.trail
+    );
+  }
+  setLeft(left) {
+    return new Zipper(
+      {
+        value: this.tree.value,
+        left: left,
+        right: this.tree.right
+      },
+      this.trail
+    );
+  }
+  setRight(right) {
+    return new Zipper(
+      {
+        value: this.tree.value,
+        left: this.tree.left,
+        right: right
+      },
+      this.trail
+    );
+  }
+}
+
+
+export default Zipper;
---
+++ exercises/zipper/zipper.spec.js
@@ -0,0 +1,77 @@
+import Zipper  from './zipper';
+
+// Tests adapted from `problem-specifications/zipper/canonical-data.json` @ v1.0.0
+
+function bt(value, left, right) {
+  return {
+    value: value,
+    left: left,
+    right: right
+  };
+}
+
+function leaf(value) {
+  return bt(value, null, null);
+}
+
+describe('Zipper', () => {
+  const t1 = bt(1, bt(2, null, leaf(3)), leaf(4));
+  const t2 = bt(1, bt(5, null, leaf(3)), leaf(4));
+  const t3 = bt(1, bt(2, leaf(5), leaf(3)), leaf(4));
+  const t4 = bt(1, leaf(2), leaf(4));
+  const t5 = bt(1, bt(2, null, leaf(3)), bt(6, leaf(7), leaf(8)));
+  const t6 = bt(1, bt(2, null, leaf(5)), leaf(4));
+  let zipper;
+
+  beforeEach(() => {
+    zipper = Zipper.fromTree(t1);
+  });
+
+  test('data is retained', () => {
+    expect(zipper.toTree(t1)).toEqual(t1);
+  });
+
+  xtest('left, right and value', () => {
+    expect(zipper.left().right().value()).toEqual(3);
+  });
+
+  xtest('dead end', () => {
+    expect(zipper.left().left()).toBe(null);
+  });
+
+  xtest('tree from deep focus', () => {
+    expect(zipper.left().right().toTree()).toEqual(t1);
+  });
+
+  xtest('traversing up from top', () => {
+    expect(zipper.up()).toEqual(null);
+  });
+
+  xtest('left, right and up', () => {
+    expect(zipper.left().up().right().up().left().right().value()).toEqual(3);
+  });
+
+  xtest('setValue', () => {
+    expect(zipper.left().setValue(5).toTree()).toEqual(t2);
+  });
+
+  xtest('setValue after traversing up', () => {
+    expect(zipper.left().right().up().setValue(5).toTree()).toEqual(t2);
+  });
+
+  xtest('setLeft with leaf', () => {
+    expect(zipper.left().setLeft(leaf(5)).toTree()).toEqual(t3);
+  });
+
+  xtest('setRight with null', () => {
+    expect(zipper.left().setRight(null).toTree()).toEqual(t4);
+  });
+
+  xtest('setRight with subtree', () => {
+    expect(zipper.setRight(bt(6, leaf(7), leaf(8))).toTree()).toEqual(t5);
+  });
+
+  xtest('setValue on deep focus', () => {
+    expect(zipper.left().right().setValue(5).toTree()).toEqual(t6);
+  });
+});
