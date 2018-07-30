0f5ca4feb1069c9b11312e41f137167d676983b1
Anthony Scotti <amscotti@users.noreply.github.com>
2018-07-30 09:49:29 -0400

    Update scrabble-score exercise spec (#431)

--- exercises/scrabble-score/scrabble-score.spec.js
+++ exercises/scrabble-score/scrabble-score.spec.js
@@ -1,15 +1,49 @@
 import score from './scrabble-score';
 
 describe('Scrabble', () => {
-  test('scores an empty word as zero', () => expect(score('')).toEqual(0));
 
-  xtest('scores a null as zero', () => expect(score(null)).toEqual(0));
+  test('lowercase letter', () => { 
+    expect(score('a')).toEqual(1)
+  });
 
-  xtest('scores a very short word', () => expect(score('a')).toEqual(1));
+  xtest('uppercase letter', () => { 
+    expect(score('A')).toEqual(1)
+  });
 
-  xtest('scores the word by the number of letters', () => expect(score('street')).toEqual(6));
+  xtest('valuable letter', () => { 
+    expect(score('f')).toEqual(4)
+  });
 
-  xtest('scores more complicated words with more', () => expect(score('quirky')).toEqual(22));
+  xtest('short word', () => { 
+    expect(score('at')).toEqual(2)
+  });
 
-  xtest('scores case insensitive words', () => expect(score('OXYPHENBUTAZONE')).toEqual(41));
+  xtest('short, valuable word', () => { 
+    expect(score('zoo')).toEqual(12)
+  });
+
+  xtest('medium word', () => { 
+    expect(score('street')).toEqual(6)
+  });
+
+  xtest('medium, valuable word', () => { 
+    expect(score('quirky')).toEqual(22)
+  });
+
+  xtest('long, mixed-case word', () => { 
+    expect(score('OxyphenButazone')).toEqual(41)
+  });
+
+  xtest('english-like word', () => { 
+    expect(score('pinata')).toEqual(8)
+  });
+
+  xtest('empty input', () => { 
+    expect(score('')).toEqual(0)
+  });
+
+  xtest('entire alphabet available', () => { 
+    expect(score('abcdefghijklmnopqrstuvwxyz')).toEqual(87)
+  });
+
 });
