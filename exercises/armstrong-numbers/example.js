ead4a7b10548f7b5e5b3e848af8c5b8586d5f671
xarxziux <hairy.one@virgin.net>
2018-08-09 11:15:45 +0100

    Fix unit tests for Armstrong Number exercise in accordance with issue #436:
    - Change default import `ArmstrongNumbers` to named import `{ validate }`.
    - Change calls to imported function from `ArmstrongNumbers.validate(input)` to the simpler `validate(input)`.
    - Remove default export statement from example.js.
    - Change export statement in example.js to a named export.

--- exercises/armstrong-numbers/example.js
+++ exercises/armstrong-numbers/example.js
@@ -1,11 +1,7 @@
-const validate = input => {
+export const validate = (input) => {
   const digits = [...String(input)];
   const sum = digits.reduce((total, current) => (
     total + Math.pow(current, digits.length)
   ), 0);
   return sum === input;
-}
-
-export default {
-  validate,
-}
+};
