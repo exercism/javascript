c73c20e0dc3d07b6abb768e7a537d7c6e0d22c10
Anthony Scotti <anthony.m.scotti@gmail.com>
2018-08-11 17:46:20 -0400

    Update matrix exercise

--- exercises/matrix/matrix.spec.js
+++ exercises/matrix/matrix.spec.js
@@ -1,16 +1,37 @@
 import Matrix from './matrix';
 
 describe('Matrix', () => {
-  test('can extract a row', () => {
-    expect(new Matrix('1 2\n10 20').rows[0]).toEqual([1, 2]);
+
+  test('extract row from one number matrix', () => {
+    expect(new Matrix('1').rows[0]).toEqual([1]);
   });
 
-  xtest('can extract the other row', () => {
-    expect(new Matrix('9 8 7\n19 18 17').rows[1]).toEqual([19, 18, 17]);
+  xtest('can extract row', () => {
+    expect(new Matrix('1 2\n3 4').rows[1]).toEqual([3, 4]);
   });
 
-  xtest('can extract a column', () => {
-    expect(new Matrix('89 1903 3\n18 3 1\n9 4 800').columns[1])
-      .toEqual([1903, 3, 4]);
+  xtest('extract row where numbers have different widths', () => {
+    expect(new Matrix('1 2\n10 20').rows[1]).toEqual([10, 20]);
   });
+
+  xtest('can extract row from non-square matrix', () => {
+    expect(new Matrix('1 2 3\n4 5 6\n7 8 9\n8 7 6').rows[2]).toEqual([7, 8, 9]);
+  });
+
+  xtest('extract column from one number matrix', () => {
+    expect(new Matrix('1').columns[0]).toEqual([1]);
+  });
+
+  xtest('can extract column', () => {
+    expect(new Matrix('1 2 3\n4 5 6\n7 8 9').columns[2]).toEqual([3, 6, 9]);
+  });
+
+  xtest('can extract column from non-square matrix', () => {
+    expect(new Matrix('1 2 3\n4 5 6\n7 8 9\n8 7 6').columns[2]).toEqual([3, 6, 9, 6]);
+  });
+
+  xtest('extract column where numbers have different widths', () => {
+    expect(new Matrix('89 1903 3\n18 3 1\n9 4 800').columns[1]).toEqual([1903, 3, 4]);
+  });
+
 });
