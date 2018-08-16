beda1019ef1018a1bfd85a3a38fdb614de82de9e
xarxziux <hairy.one@virgin.net>
2018-08-16 20:43:06 +0100

    Fix unit tests and example for Simple Cipher exercise Change the example for the Simple Cipher exercise to accept a named class export instead of a default class export as per issues #274 and #436, and expand unit tests as per issue #445: - Refactor example.js to export a named class. - Change default import `Cipher` to named import `{ Cipher }`. - Add unit tests to include checks for non-alphabet characters.

--- exercises/simple-cipher/example.js
+++ exercises/simple-cipher/example.js
@@ -17,20 +17,23 @@ function xCode(key, inText, sign) {
 
 const mod = (n, m) => (n % m + m) % m;
 
-export default function (key) {
-  if (typeof key === 'undefined') {
-    key = generateKey();
-  } else if (key.length === 0 || key.match(/[^a-z]/, 'g')) {
-    throw new Error('Bad key');
+export class Cipher {
+
+  constructor (key) {
+    if (typeof key === 'undefined') {
+      this.key = generateKey();
+    } else if (key.length === 0 || key.match(/[^a-z]/, 'g')) {
+      throw new Error('Bad key');
+    } else {
+      this.key = key;
+    }
   }
 
-  return {
-    key,
-    encode(plainText) {
-      return xCode(this.key, plainText, 1);
-    },
-    decode(encodedText) {
-      return xCode(this.key, encodedText, -1);
-    },
-  };
+  encode(plainText) {
+    return xCode(this.key, plainText, 1);
+  }
+
+  decode(encodedText) {
+    return xCode(this.key, encodedText, -1);
+  }
 }
