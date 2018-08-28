d3526f740e81fe6056a4586edc42079ee0475f0e
xarxziux <hairy.one@virgin.net>
2018-08-28 09:29:04 +0100

    Fix unit tests for Gigasecond exercise Change the unit tests for the Gigasecond exercise to accept a named function export instead of a default class export as per issues #274 and #436: - Change default class import `Gigasecond` to named function import `{ gigasecond }`. - Change `new Gigasecond(...` calls to `gigasecond(...`. - Change `gs.date()` calls to `gs` calls. - Remove mutation test which is no longer relevant. - Redesign example.js file to export a single named function.

--- exercises/gigasecond/example.js
+++ exercises/gigasecond/example.js
@@ -1,14 +1,6 @@
 const GIGASECOND_IN_MILIS = 1e9 * 1e3;
 
-export default class Gigasecond {
-
-  constructor(dateOfBirth) {
-    this.dateOfBirth = dateOfBirth;
-  }
-
-  date() {
-    const birthTime = this.dateOfBirth.getTime();
-    return new Date(birthTime + GIGASECOND_IN_MILIS);
-  }
-
+export const gigasecond = dateOfBirth => {
+  const birthTime = dateOfBirth.getTime();
+  return new Date(birthTime + GIGASECOND_IN_MILIS);
 }
