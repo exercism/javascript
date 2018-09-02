7ac93a13702aa7240cba72c77218d9201f0da49c
xarxziux <hairy.one@virgin.net>
2018-09-02 21:14:46 +0100

    Fix unit tests for Gigasecond exercise (#464)
    Change the unit tests for the Gigasecond exercise to accept a named function export instead of a default class export as per issues #274 and #436:
    - Change default class import `Gigasecond` to named function import `{ gigasecond }`.
    - Change `new Gigasecond(...` calls to `gigasecond(...`.
    - Change `gs.date()` calls to `gs` calls.
    - Remove mutation test which is no longer relevant.
    - Redesign example.js file to export a single named function.

--- exercises/gigasecond/gigasecond.spec.js
+++ exercises/gigasecond/gigasecond.spec.js
@@ -1,28 +1,21 @@
-import Gigasecond from './gigasecond';
+import { gigasecond } from './gigasecond';
 
 describe('Gigasecond', () => {
   test('tells a gigasecond anniversary since midnight', () => {
-    const gs = new Gigasecond(new Date(Date.UTC(2015, 8, 14)));
+    const gs = gigasecond(new Date(Date.UTC(2015, 8, 14)));
     const expectedDate = new Date(Date.UTC(2047, 4, 23, 1, 46, 40));
-    expect(gs.date()).toEqual(expectedDate);
+    expect(gs).toEqual(expectedDate);
   });
 
   xtest('tells the anniversary is next day when you are born at night', () => {
-    const gs = new Gigasecond(new Date(Date.UTC(2015, 8, 14, 23, 59, 59)));
+    const gs = gigasecond(new Date(Date.UTC(2015, 8, 14, 23, 59, 59)));
     const expectedDate = new Date(Date.UTC(2047, 4, 24, 1, 46, 39));
-    expect(gs.date()).toEqual(expectedDate);
+    expect(gs).toEqual(expectedDate);
   });
 
   xtest('even works before 1970 (beginning of Unix epoch)', () => {
-    const gs = new Gigasecond(new Date(Date.UTC(1959, 6, 19, 5, 13, 45)));
+    const gs = gigasecond(new Date(Date.UTC(1959, 6, 19, 5, 13, 45)));
     const expectedDate = new Date(Date.UTC(1991, 2, 27, 7, 0, 25));
-    expect(gs.date()).toEqual(expectedDate);
-  });
-
-  xtest('make sure calling "date" doesn\'t mutate value', () => {
-    const gs = new Gigasecond(new Date(Date.UTC(1959, 6, 19, 5, 13, 45)));
-    const expectedDate = new Date(Date.UTC(1991, 2, 27, 7, 0, 25));
-    gs.date();
-    expect(gs.date()).toEqual(expectedDate);
+    expect(gs).toEqual(expectedDate);
   });
 });
