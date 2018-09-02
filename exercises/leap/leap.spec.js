74bec089f4e6084686ebc2b3378553d8f55419ca
xarxziux <hairy.one@virgin.net>
2018-09-02 21:09:55 +0100

    Fix unit tests for Leap exercise (#446)
    Change the unit tests for the Leap exercise to accept a named function import instead of a default named import as per issues #274 and #436:
    - Change default import `Year` to named import `{ isLeap }`.
    - Remove unnecessary `const year = new Year(...` calls.
    - Change `year.isLeap()` calls to `isLeap(XXXX)`.
    - Rewrite example.js file to export a single named function.

--- exercises/leap/leap.spec.js
+++ exercises/leap/leap.spec.js
@@ -1,23 +1,19 @@
-import Year from './leap';
+import { isLeap } from './leap';
 
 describe('A leap year', () => {
   test('year not divisible by 4: common year', () => {
-    const year = new Year(2015);
-    expect(year.isLeap()).toBeFalsy();
+    expect(isLeap(2015)).toBeFalsy();
   });
 
   xtest('year divisible by 4, not divisible by 100: leap year', () => {
-    const year = new Year(2016);
-    expect(year.isLeap()).toBeTruthy();
+    expect(isLeap(2016)).toBeTruthy();
   });
 
   xtest('year divisible by 100, not divisible by 400: common year', () => {
-    const year = new Year(2100);
-    expect(year.isLeap()).toBeFalsy();
+    expect(isLeap(2100)).toBeFalsy();
   });
 
   xtest('year divisible by 400: leap year', () => {
-    const year = new Year(2000);
-    expect(year.isLeap()).toBeTruthy();
+    expect(isLeap(2000)).toBeTruthy();
   });
 });
