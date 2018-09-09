4fb44c77661fcf2730156538fe1cb785f5fdc6b2
Lyle Scott <Lyle.C.Scott@icloud.com>
2018-09-09 13:33:59 -0700

    add test for area/exchange codes in example.js

--- exercises/phone-number/example.js
+++ exercises/phone-number/example.js
@@ -15,11 +15,11 @@ export default class PhoneNumber {
   _cleanedNumber() {
     const num = this.rawNumber.replace(/\D/g, '');
 
-    if (num.length === 10) {
+    if (num.length === 10 && num[0] >= 2 && num[3] >= 2) {
       return num;
     }
 
-    if (num.length === 11 && num[0] === '1') {
+    if (num.length === 11 && num[0] === '1' && num[1] >= 2 && num[4] >= 2) {
       return num.substr(1);
     }
 
