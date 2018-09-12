9390e5f628a553caeab3f2386f94edfcded403c1
xarxziux <hairy.one@virgin.net>
2018-09-12 10:47:10 +0100

    Fix unit tests and example solution for Secret Handshake exercise (#470)
    * Fix unit test and example solution for Secret Handshake exercise
    Change the unit tests for the Secret Handshake exercise to accept a named 
    function export instead of a default class export as per issues #274 and #436:
    - Change default class import `SecretHandshake` to named function import `{ secretHandshake }`.
    - Remove unneeded `new SecretHandshake(` calls.
    - Change `handshake.commands(` calls to `secretHandshake(` calls.
    - Redesign example.js file to export a single named function.
    
    * Refactor Secret Handshake example
    Replace for loop algorithm as requested in pull request #470 .

--- exercises/secret-handshake/example.js
+++ exercises/secret-handshake/example.js
@@ -1,30 +1,16 @@
-export default function (handshake) {
-  const HANDSHAKE_COMMANDS = ['wink', 'double blink', 'close your eyes', 'jump', 'REVERSE'];
+const handshakeCommands = ['wink', 'double blink', 'close your eyes', 'jump'];
 
+export const secretHandshake = (handshake) => {
   if (typeof handshake !== 'number') {
     throw new Error('Handshake must be a number');
   }
 
-  this.commands = () => this.shakeWith;
+  const shakeWith = handshakeCommands.filter((_, i) => (
+    handshake & (Math.pow(2, i))
+  ));
 
-  this.calculateHandshake = (handshake) => {
-    const shakeWith = [];
+  if (handshake & (Math.pow(2, 4)))
+    shakeWith.reverse();
 
-    for (let i = 0; i < HANDSHAKE_COMMANDS.length; i++) {
-      const currentCommand = HANDSHAKE_COMMANDS[i];
-      const handshakeHasCommand = handshake & Math.pow(2, i);
-
-      if (handshakeHasCommand) {
-        if (currentCommand === 'REVERSE') {
-          shakeWith.reverse();
-        } else {
-          shakeWith.push(HANDSHAKE_COMMANDS[i]);
-        }
-      }
-    }
-
-    return shakeWith;
-  };
-
-  this.shakeWith = this.calculateHandshake(handshake);
-}
+  return shakeWith;
+};
