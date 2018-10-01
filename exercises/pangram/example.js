3c3c88fb6c9587a3b8112c6bfae8896cb89edb8c
Tarun <tarunvelli@gmail.com>
2018-10-01 20:45:27 +0530

    Fix linting for pangram exercise

--- exercises/pangram/example.js
+++ exercises/pangram/example.js
@@ -1,10 +1,10 @@
-const notAlpha = /[^a-z]+/gi,
-  alphaLength = 26;
-let cleaned,
-  sortedSet;
+const notAlpha = /[^a-z]+/gi;
+const alphaLength = 26;
+let cleaned;
+let sortedSet;
 
-export const isPangram = candidate => {
+export const isPangram = (candidate) => {
   cleaned = (candidate.replace(notAlpha, '')).toLowerCase();
   sortedSet = new Set([...cleaned].sort());
   return sortedSet.size === alphaLength;
-}
+};
