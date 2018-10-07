05c141f22ac003dc0bd49ed3637097ad41c09e63
Tarun <tarunvelli@gmail.com>
2018-10-07 20:07:35 +0530

    remove unnecessary variable initialization

--- exercises/pangram/example.js
+++ exercises/pangram/example.js
@@ -1,10 +1,8 @@
 const notAlpha = /[^a-z]+/gi;
 const alphaLength = 26;
-let cleaned;
-let sortedSet;
 
 export const isPangram = (candidate) => {
-  cleaned = (candidate.replace(notAlpha, '')).toLowerCase();
-  sortedSet = new Set([...cleaned].sort());
+  const cleaned = (candidate.replace(notAlpha, '')).toLowerCase();
+  const sortedSet = new Set([...cleaned].sort());
   return sortedSet.size === alphaLength;
 };
