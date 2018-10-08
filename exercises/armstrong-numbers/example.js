df7c876c0b3793f5169741e1c578db43fa880d10
Gabe Hargrave <gkhargrave@gmail.com>
2018-10-08 09:52:30 -0700

    (#480) Armstrong numbers linting fixes (#482)
    * Install and use `babel-eslint` parser
    
    * Fix linting errors in 'armstrong-numbers'
    
    * Fix license information
    
    * Update NPM Lockfile and all package.jsons

--- exercises/armstrong-numbers/example.js
+++ exercises/armstrong-numbers/example.js
@@ -1,7 +1,7 @@
 export const validate = (input) => {
   const digits = [...String(input)];
   const sum = digits.reduce((total, current) => (
-    total + Math.pow(current, digits.length)
+    total + (current ** digits.length)
   ), 0);
   return sum === input;
 };
