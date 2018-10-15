d2616eda8cb7d907e1123b38f01c64112c67f97f
Eric Ti Yu Chiang <eric.ti.yu.chiang@gmail.com>
2018-10-15 14:44:08 -0400

    Fix eslint errors in hamming exercise

--- exercises/hamming/example.js
+++ exercises/hamming/example.js
@@ -1,6 +1,5 @@
 class Hamming {
-
-  compute(strand1, strand2) {
+  static compute(strand1, strand2) {
     const len1 = strand1.length;
     const len2 = strand2.length;
     if (len1 !== len2) {
@@ -8,17 +7,15 @@ class Hamming {
     }
 
     let distance = 0;
-    let idx = -1;
     const end = len1; // there could be len2, they're equal
-    while (++idx < end) {
+    for (let idx = 0; idx < end; idx += 1) {
       if (strand1[idx] !== strand2[idx]) {
-        distance++;
+        distance += 1;
       }
     }
 
     return distance;
   }
-
 }
 
 export default Hamming;
