20cbe3740039f1698ae2f4eb6b0a771215812b71
Eric Ti Yu Chiang <eric.ti.yu.chiang@gmail.com>
2018-10-15 15:51:19 -0400

    export `compute` function instead of `Hamming` class
    This addresses the `Expected 'this' to be used by class method
    'compute'` lint error.

--- exercises/hamming/example.js
+++ exercises/hamming/example.js
@@ -1,21 +1,17 @@
-class Hamming {
-  static compute(strand1, strand2) {
-    const len1 = strand1.length;
-    const len2 = strand2.length;
-    if (len1 !== len2) {
-      throw new Error('left and right strands must be of equal length');
-    }
-
-    let distance = 0;
-    const end = len1; // there could be len2, they're equal
-    for (let idx = 0; idx < end; idx += 1) {
-      if (strand1[idx] !== strand2[idx]) {
-        distance += 1;
-      }
-    }
-
-    return distance;
+export const compute = (strand1, strand2) => {
+  const len1 = strand1.length;
+  const len2 = strand2.length;
+  if (len1 !== len2) {
+    throw new Error('left and right strands must be of equal length');
   }
-}
 
-export default Hamming;
+  let distance = 0;
+  const end = len1; // there could be len2, they're equal
+  for (let idx = 0; idx < end; idx += 1) {
+    if (strand1[idx] !== strand2[idx]) {
+      distance += 1;
+    }
+  }
+
+  return distance;
+};
