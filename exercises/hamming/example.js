ce51acd4b5ed8e321a784f08f58489dd073479c4
Eric Ti-Yu Chiang <eric.ti.yu.chiang@gmail.com>
2018-10-16 15:21:02 -0400

    Fix eslint errors in hamming exercise (#505)
    * Fix eslint errors in hamming exercise
    
    * export `compute` function instead of `Hamming` class
    
    This addresses the `Expected 'this' to be used by class method
    'compute'` lint error.
    
    * Use `array.reduce` instead of for loop
    
    Per feedback at
    https://github.com/exercism/javascript/pull/505#pullrequestreview-165118429
    prefer `array.reduce` over for loop.
    
    * Use `filter` instead of `reduce`
    
    Based on feedback from
    https://github.com/exercism/javascript/pull/505#pullrequestreview-165309766
    using `filter` here is semantically appropriate.

--- exercises/hamming/example.js
+++ exercises/hamming/example.js
@@ -1,24 +1,9 @@
-class Hamming {
-
-  compute(strand1, strand2) {
-    const len1 = strand1.length;
-    const len2 = strand2.length;
-    if (len1 !== len2) {
-      throw new Error('left and right strands must be of equal length');
-    }
-
-    let distance = 0;
-    let idx = -1;
-    const end = len1; // there could be len2, they're equal
-    while (++idx < end) {
-      if (strand1[idx] !== strand2[idx]) {
-        distance++;
-      }
-    }
-
-    return distance;
+export const compute = (strand1, strand2) => {
+  const len1 = strand1.length;
+  const len2 = strand2.length;
+  if (len1 !== len2) {
+    throw new Error('left and right strands must be of equal length');
   }
 
-}
-
-export default Hamming;
+  return [...strand1].filter((element, index) => element !== strand2[index]).length;
+};
--- exercises/hamming/hamming.spec.js
+++ exercises/hamming/hamming.spec.js
@@ -1,68 +1,66 @@
-import Hamming from './hamming';
+import { compute } from './hamming';
 
 describe('Hamming', () => {
-  const hamming = new Hamming();
-
   test('no difference between empty strands', () => {
-    expect(hamming.compute('', '')).toEqual(0);
+    expect(compute('', '')).toEqual(0);
   });
 
   xtest('no difference between identical strands', () => {
-    expect(hamming.compute('A', 'A')).toEqual(0);
+    expect(compute('A', 'A')).toEqual(0);
   });
 
   xtest('long identical strands', () => {
-    expect(hamming.compute('GGACTGA', 'GGACTGA')).toEqual(0);
+    expect(compute('GGACTGA', 'GGACTGA')).toEqual(0);
   });
 
   xtest('complete distance in single nucleotide strands', () => {
-    expect(hamming.compute('A', 'G')).toEqual(1);
+    expect(compute('A', 'G')).toEqual(1);
   });
 
   xtest('complete distance in small strands', () => {
-    expect(hamming.compute('AG', 'CT')).toEqual(2);
+    expect(compute('AG', 'CT')).toEqual(2);
   });
 
   xtest('small distance in small strands', () => {
-    expect(hamming.compute('AT', 'CT')).toEqual(1);
+    expect(compute('AT', 'CT')).toEqual(1);
   });
 
   xtest('small distance', () => {
-    expect(hamming.compute('GGACG', 'GGTCG')).toEqual(1);
+    expect(compute('GGACG', 'GGTCG')).toEqual(1);
   });
 
   xtest('small distance in long strands', () => {
-    expect(hamming.compute('ACCAGGG', 'ACTATGG')).toEqual(2);
+    expect(compute('ACCAGGG', 'ACTATGG')).toEqual(2);
   });
 
   xtest('non-unique character in first strand', () => {
-    expect(hamming.compute('AAG', 'AAA')).toEqual(1);
+    expect(compute('AAG', 'AAA')).toEqual(1);
   });
 
   xtest('non-unique character in second strand', () => {
-    expect(hamming.compute('AAA', 'AAG')).toEqual(1);
+    expect(compute('AAA', 'AAG')).toEqual(1);
   });
 
   xtest('same nucleotides in different positions', () => {
-    expect(hamming.compute('TAG', 'GAT')).toEqual(2);
+    expect(compute('TAG', 'GAT')).toEqual(2);
   });
 
   xtest('large distance', () => {
-    expect(hamming.compute('GATACA', 'GCATAA')).toEqual(4);
+    expect(compute('GATACA', 'GCATAA')).toEqual(4);
   });
 
   xtest('large distance in off-by-one strand', () => {
-    expect(hamming.compute('GGACGGATTCTG', 'AGGACGGATTCT')).toEqual(9);
+    expect(compute('GGACGGATTCTG', 'AGGACGGATTCT')).toEqual(9);
   });
 
   xtest('disallow first strand longer', () => {
-    expect(() => hamming.compute('AATG', 'AAA')).toThrow(
+    expect(() => compute('AATG', 'AAA')).toThrow(
       new Error('left and right strands must be of equal length'),
     );
   });
 
   xtest('disallow second strand longer', () => {
-    expect(() => hamming.compute('ATA', 'AGTG')).toThrow(
+    expect(() => compute('ATA', 'AGTG')).toThrow(
       new Error('left and right strands must be of equal length'),
     );
   });
