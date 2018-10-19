bd6aeafff3f941a2565031f9bd3c87f2f422091f
Natasha Rocha <nattycdb@gmail.com>
2018-10-19 17:34:35 -0300

    Lint crypto-square exercise

--- exercises/crypto-square/example.js
+++ exercises/crypto-square/example.js
@@ -21,27 +21,27 @@ export default class Square {
   }
 
   ciphertext() {
-    const textSegments = this.plaintextSegments(),
-      columns = [];
-    let i,
-      j,
-      currentSegment,
-      currentLetter;
+    const textSegments = this.plaintextSegments();
+    const columns = [];
+    let i;
+    let j;
+    let currentSegment;
+    let currentLetter;
 
-    for (i = 0; i < this.size(); i++) {
+    for (i = 0; i < this.size(); i += 1) {
       columns.push([]);
     }
 
-    for (i = 0; i < textSegments.length; i++) {
+    for (i = 0; i < textSegments.length; i += 1) {
       currentSegment = textSegments[i];
 
-      for (j = 0; j < currentSegment.length; j++) {
+      for (j = 0; j < currentSegment.length; j += 1) {
         currentLetter = currentSegment[j];
         columns[j].push(currentLetter);
       }
     }
 
-    for (i = 0; i < columns.length; i++) {
+    for (i = 0; i < columns.length; i += 1) {
       columns[i] = columns[i].join('');
     }
 
