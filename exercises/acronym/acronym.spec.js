7d530b6a5a930193f993087855976fcb8c5f6880
Eric Ti Yu Chiang <eric.ti.yu.chiang@gmail.com>
2018-10-22 11:25:21 -0400

    Acronym: change default export to named export
    per #436, change `Acronym.parse` to `parse` because named export is
    preferred over default export.

--- exercises/acronym/acronym.spec.js
+++ exercises/acronym/acronym.spec.js
@@ -1,27 +1,27 @@
-import Acronyms from './acronym';
+import { parse } from './acronym';
 
 describe('Acronyms are produced from', () => {
   test('title cased phrases', () => {
-    expect(Acronyms.parse('Portable Network Graphics')).toEqual('PNG');
+    expect(parse('Portable Network Graphics')).toEqual('PNG');
   });
 
   xtest('other title cased phrases', () => {
-    expect(Acronyms.parse('Ruby on Rails')).toEqual('ROR');
+    expect(parse('Ruby on Rails')).toEqual('ROR');
   });
 
   xtest('inconsistently cased phrases', () => {
-    expect(Acronyms.parse('HyperText Markup Language')).toEqual('HTML');
+    expect(parse('HyperText Markup Language')).toEqual('HTML');
   });
 
   xtest('phrases with punctuation', () => {
-    expect(Acronyms.parse('First In, First Out')).toEqual('FIFO');
+    expect(parse('First In, First Out')).toEqual('FIFO');
   });
 
   xtest('other phrases with punctuation', () => {
-    expect(Acronyms.parse('PHP: Hypertext Preprocessor')).toEqual('PHP');
+    expect(parse('PHP: Hypertext Preprocessor')).toEqual('PHP');
   });
 
   xtest('phrases with punctuation and sentence casing', () => {
-    expect(Acronyms.parse('Complementary metal-oxide semiconductor')).toEqual('CMOS');
+    expect(parse('Complementary metal-oxide semiconductor')).toEqual('CMOS');
   });
 });
