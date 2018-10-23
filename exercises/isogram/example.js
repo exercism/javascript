1bd774d4907c56fe383301c2f62159cb25d3a84c
Eric Ti Yu Chiang <eric.ti.yu.chiang@gmail.com>
2018-10-23 11:25:40 -0400

    Isogram: change default export to named export
    per #436, change `Isogram.isIsogram` to `isIsogram` because named export is
    preferred over default export.  Note this also involve a function
    signature change.  `isIsogram` now takes an argument instead.

--- exercises/isogram/example.js
+++ exercises/isogram/example.js
@@ -1,10 +1,5 @@
-export default class Isogram {
-  constructor(string) {
-    this.string = string.replace(/ |-/g, '');
-  }
-
-  isIsogram() {
-    const uniqueLetters = this.string.toLowerCase().split('').filter((element, index, self) => self.indexOf(element) === index);
-    return uniqueLetters.length === this.string.length;
-  }
-}
+export const isIsogram = (string) => {
+  const stringNoSpaceOrHyphen = string.replace(/ |-/g, '');
+  const uniqueLetters = stringNoSpaceOrHyphen.toLowerCase().split('').filter((element, index, self) => self.indexOf(element) === index);
+  return uniqueLetters.length === stringNoSpaceOrHyphen.length;
+};
