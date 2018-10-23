f761db42f4e885e9720e8d066a3b1099180721ba
ramadis <rramiro.o@hotmail.com>
2018-10-23 21:00:29 +0200

    Several fixes: Problem specification + Style

--- exercises/target/example.js
+++ exercises/target/example.js
@@ -1,17 +1,13 @@
 export default function solve(x, y) {
-  // Define as numbers
-  x = Number(x);
-  y = Number(y);
-
   // Check for NaN
-  if (x !== x || y !== y) return null;
+  if (Number.isNaN(x) || Number.isNaN(y)) return null;
 
   // Use euclidean distance
-  const distanceToDart = Math.sqrt(x*x + y*y);
+  const distanceToDart = Math.sqrt(x * x + y * y);
 
   // Define points for each section of the target
   if (distanceToDart > 10) return 0;
-  else if (distanceToDart > 5) return 1;
-  else if (distanceToDart > 1) return 5;
+  if (distanceToDart > 5) return 1;
+  if (distanceToDart > 1) return 5;
   return 10;
 }
--- exercises/target/target.spec.js
+++ exercises/target/target.spec.js
@@ -8,34 +8,34 @@ describe('Solve the target problem', () => {
     expect(solve(x, y)).toEqual(expected);
   });
 
-  test('A dart lands just in the border of the target', () => {
+  xtest('A dart lands just in the border of the target', () => {
     const x = 10;
     const y = 0;
     const expected = 1;
     expect(solve(x, y)).toEqual(expected);
   });
 
-  test('Input is not a number', () => {
+  xtest('Input is not a number', () => {
     const x = 'WRONG';
     const y = 10;
     expect(solve(x, y)).toBeNull();
   });
 
-  test('A dart lands in the middle circle', () => {
+  xtest('A dart lands in the middle circle', () => {
     const x = 3;
     const y = 3.7;
     const expected = 5;
     expect(solve(x, y)).toEqual(expected);
   });
 
-  test('A dart lands right in the border between outside and middle circles', () => {
+  xtest('A dart lands right in the border between outside and middle circles', () => {
     const x = 0;
     const y = 5;
     const expected = 5;
     expect(solve(x, y)).toEqual(expected);
   });
 
-  test('A dart arrives in the inner circle', () => {
+  xtest('A dart lands in the inner circle', () => {
     const x = 0;
     const y = 0;
     const expected = 10;
