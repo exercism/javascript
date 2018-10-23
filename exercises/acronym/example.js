4c3b3f9f79537a1ee05d28e3902d2808d39c535d
Eric Ti-Yu Chiang <eric.ti.yu.chiang@gmail.com>
2018-10-23 01:03:10 -0400

    Acronym: change default export to named export (#531)
    per #436, change `Acronym.parse` to `parse` because named export is
    preferred over default export.

--- exercises/acronym/example.js
+++ exercises/acronym/example.js
@@ -1,6 +1,1 @@
-export default class Acronyms {
-  static parse(phrase) {
-    return phrase.match(/[A-Z]+[a-z]*|[a-z]+/g)
-      .reduce((acronym, word) => `${acronym}${word[0]}`, '').toUpperCase();
-  }
-}
+export const parse = phrase => phrase.match(/[A-Z]+[a-z]*|[a-z]+/g).reduce((acronym, word) => `${acronym}${word[0]}`, '').toUpperCase();
