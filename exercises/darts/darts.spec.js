c261c3f9b2bdf7cbb0fc57de13206f54fad03c9d
Rama <rramiro.o@hotmail.com>
2018-10-28 18:54:57 +0100

    darts: Add new exercise (#498)
    * Add exercise 'target'
    
    * Several fixes: Problem specification + Style
    
    * Some other improvements
    
    * Rename folder. Update package.json
    
    * Fixes for all test to pass
    
    * Fix package.json diff

---
+++ exercises/darts/darts.spec.js
@@ -0,0 +1,44 @@
+import solve from './darts';
+
+describe('Return the correct amount earned by a dart landing in a given point in the target problem', () => {
+  test('A dart lands outside the target', () => {
+    const x = 15.3;
+    const y = 13.2;
+    const expected = 0;
+    expect(solve(x, y)).toEqual(expected);
+  });
+
+  xtest('A dart lands just in the border of the target', () => {
+    const x = 10;
+    const y = 0;
+    const expected = 1;
+    expect(solve(x, y)).toEqual(expected);
+  });
+
+  xtest('Input is not a number', () => {
+    const x = 'WRONG';
+    const y = 10;
+    expect(solve(x, y)).toBeNull();
+  });
+
+  xtest('A dart lands in the middle circle', () => {
+    const x = 3;
+    const y = 3.7;
+    const expected = 5;
+    expect(solve(x, y)).toEqual(expected);
+  });
+
+  xtest('A dart lands right in the border between outside and middle circles', () => {
+    const x = 0;
+    const y = 5;
+    const expected = 5;
+    expect(solve(x, y)).toEqual(expected);
+  });
+
+  xtest('A dart lands in the inner circle', () => {
+    const x = 0;
+    const y = 0;
+    const expected = 10;
+    expect(solve(x, y)).toEqual(expected);
+  });
+});
---
+++ exercises/darts/example.js
@@ -0,0 +1,13 @@
+export default function solve(x, y) {
+  // Check for NaN
+  if (Number.isNaN(Number(x)) || Number.isNaN(Number(y))) return null;
+
+  // Use euclidean distance
+  const distanceToDart = Math.sqrt(x * x + y * y);
+
+  // Define points for each section of the target
+  if (distanceToDart > 10) return 0;
+  if (distanceToDart > 5) return 1;
+  if (distanceToDart > 1) return 5;
+  return 10;
+}
