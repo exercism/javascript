b1361d7ad7280c677019aaf2684f9ba8f8b57335
Eric Ti Yu Chiang <eric.ti.yu.chiang@gmail.com>
2018-11-01 15:19:17 -0400

    Use simple functions instead of classes
    Also, use named import/export instead of default import/export

--- exercises/perfect-numbers/example.js
+++ exercises/perfect-numbers/example.js
@@ -1,12 +1,17 @@
-
-export default class PerfectNumbers {
+/**
+  * Classify a given number as perfect, abundant or deficient.
+  * @param {number} number - number to be classified. Note: if the number is equal or smaller than 0,
+  * then returns an error: Classification is only possible for natural numbers.
+  * @returns {string} - string that contains the number classification (perfect, abundant or deficient).
+  */
+export const classify = (number) => {
 
   /**
    * Calculate all the divisors for a given number.
    * @param {number} number - natural number.
    * @returns {array} array that contains the divisors for a given number NOT including the number itself.
    */
-  getDivisors(number) {
+  function getDivisors(number) {
     let i;
     const divs = new Array();
 
@@ -28,37 +33,28 @@ export default class PerfectNumbers {
     return divs;
   }
 
-  /**
-   * Classify a given number as perfect, abundant or deficient.
-   * @param {number} number - number to be classified. Note: if the number is equal or smaller than 0,
-   * then returns an error: Classification is only possible for natural numbers.
-   * @returns {string} - string that contains the number classification (perfect, abundant or deficient).
-   */
-  classify(number) {
-    let sum,
-      result;
+  let sum,
+    result;
 
-    // Check if the input is valid
-    if (number <= 0) {
-      throw new Error('Classification is only possible for natural numbers.');
-    }
-
-    // Factorize the current number.
-    const divsArray = this.getDivisors(number);
-
-    // Sum the factors.
-    sum = divsArray.reduce((sum, div) => sum += div, 0);
-
-    // Check if the number is perfect.
-    if (sum === number) {
-      result = 'perfect';
-    } else if (sum > number) {
-      result = 'abundant';
-    } else {
-      result = 'deficient';
-    }
-
-    return result;
+  // Check if the input is valid
+  if (number <= 0) {
+    throw new Error('Classification is only possible for natural numbers.');
   }
 
-}
+  // Factorize the current number.
+  const divsArray = getDivisors(number);
+
+  // Sum the factors.
+  sum = divsArray.reduce((sum, div) => sum += div, 0);
+
+  // Check if the number is perfect.
+  if (sum === number) {
+    result = 'perfect';
+  } else if (sum > number) {
+    result = 'abundant';
+  } else {
+    result = 'deficient';
+  }
+
+  return result;
+};
