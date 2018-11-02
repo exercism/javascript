b5b920e3f07fc03a99772ffee45085486b072fac
Eric Ti-Yu Chiang <eric.ti.yu.chiang@gmail.com>
2018-11-02 10:47:51 -0400

    Improve code quality of perfect numbers exercise (#545)
    * Use simple functions instead of classes
    
    Also, use named import/export instead of default import/export
    
    * Test invalid input first
    
    * Test edge case first within deficient number tests
    
    The idea is to verify input domain validation first.
    
    * lint perfect-numbers exercise

--- exercises/perfect-numbers/example.js
+++ exercises/perfect-numbers/example.js
@@ -1,64 +1,49 @@
+/**
+  * Calculate all the divisors for a given number.
+  * @param {number} number - natural number.
+  * @returns {array} that contains the divisors for a given number excluding the number itself.
+  */
+function getDivisors(number) {
+  const divs = [];
 
-export default class PerfectNumbers {
-
-  /**
-   * Calculate all the divisors for a given number.
-   * @param {number} number - natural number.
-   * @returns {array} array that contains the divisors for a given number NOT including the number itself.
-   */
-  getDivisors(number) {
-    let i;
-    const divs = new Array();
-
-    // Accepts only natural numbers greater than 1.
-    if (number <= 1) {
-      return divs;
-    }
-
-    // 1 always divides everyone!
-    divs.push(1);
-
-    // Calculate the divisors up the the half of the number + 1
-    for (i = 2; i <= number / 2; i++) {
-      if (number % i === 0) {
-        divs.push(i);
-      }
-    }
-
+  // Accepts only natural numbers greater than 1.
+  if (number <= 1) {
     return divs;
   }
 
-  /**
-   * Classify a given number as perfect, abundant or deficient.
-   * @param {number} number - number to be classified. Note: if the number is equal or smaller than 0,
-   * then returns an error: Classification is only possible for natural numbers.
-   * @returns {string} - string that contains the number classification (perfect, abundant or deficient).
-   */
-  classify(number) {
-    let sum,
-      result;
+  // 1 always divides everyone!
+  divs.push(1);
 
-    // Check if the input is valid
-    if (number <= 0) {
-      throw new Error('Classification is only possible for natural numbers.');
+  // Calculate the divisors up the the half of the number + 1
+  for (let i = 2; i <= number / 2; i += 1) {
+    if (number % i === 0) {
+      divs.push(i);
     }
-
-    // Factorize the current number.
-    const divsArray = this.getDivisors(number);
-
-    // Sum the factors.
-    sum = divsArray.reduce((sum, div) => sum += div, 0);
-
-    // Check if the number is perfect.
-    if (sum === number) {
-      result = 'perfect';
-    } else if (sum > number) {
-      result = 'abundant';
-    } else {
-      result = 'deficient';
-    }
-
-    return result;
   }
 
+  return divs;
 }
+
+/**
+  * Classify a given number as perfect, abundant or deficient.
+  * @param {number} - number to be classified. Note: if the number is equal or smaller than 0,
+  * then returns an error: Classification is only possible for natural numbers.
+  * @returns {string} - string that contains the classification (perfect, abundant or deficient).
+  */
+export const classify = (number) => {
+  // Check if the input is valid
+  if (number <= 0) {
+    throw new Error('Classification is only possible for natural numbers.');
+  }
+
+  // Factorize the current number.
+  const divsArray = getDivisors(number);
+
+  // Sum the factors.
+  const sum = divsArray.reduce((partialSum, div) => partialSum + div, 0);
+
+  // Check if the number is perfect/abundant/deficient
+  if (sum === number) return 'perfect';
+  if (sum > number) return 'abundant';
+  return 'deficient';
+};
--- exercises/perfect-numbers/perfect-numbers.spec.js
+++ exercises/perfect-numbers/perfect-numbers.spec.js
@@ -1,67 +1,65 @@
-import PerfectNumbers from './perfect-numbers';
+import { classify } from './perfect-numbers';
 
 describe('Exercise - Perfect Numbers', () => {
-  const perfectNumbers = new PerfectNumbers();
+  describe('Invalid Inputs', () => {
+    test('Zero is rejected (not a natural number)', () => {
+      expect(() => classify(0))
+        .toThrow(new Error('Classification is only possible for natural numbers.'));
+    });
+
+    xtest('Negative integer is rejected (not a natural number)', () => {
+      expect(() => classify(-1))
+        .toThrow(new Error('Classification is only possible for natural numbers.'));
+    });
+  });
 
   describe('Perfect Numbers', () => {
-    test('Smallest perfect number is classified correctly', () => {
-      expect(perfectNumbers.classify(6)).toEqual('perfect');
+    xtest('Smallest perfect number is classified correctly', () => {
+      expect(classify(6)).toEqual('perfect');
     });
 
     xtest('Medium perfect number is classified correctly', () => {
-      expect(perfectNumbers.classify(28)).toEqual('perfect');
+      expect(classify(28)).toEqual('perfect');
     });
 
     xtest('Large perfect number is classified correctly', () => {
-      expect(perfectNumbers.classify(33550336)).toEqual('perfect');
+      expect(classify(33550336)).toEqual('perfect');
     });
   });
 
   describe('Abundant Numbers', () => {
     xtest('Smallest abundant number is classified correctly', () => {
-      expect(perfectNumbers.classify(12)).toEqual('abundant');
+      expect(classify(12)).toEqual('abundant');
     });
 
     xtest('Medium abundant number is classified correctly', () => {
-      expect(perfectNumbers.classify(30)).toEqual('abundant');
+      expect(classify(30)).toEqual('abundant');
     });
 
     xtest('Large abundant number is classified correctly', () => {
-      expect(perfectNumbers.classify(33550335)).toEqual('abundant');
+      expect(classify(33550335)).toEqual('abundant');
     });
   });
 
   describe('Deficient Numbers', () => {
+    xtest('Edge case (no factors other than itself) is classified correctly', () => {
+      expect(classify(1)).toEqual('deficient');
+    });
+
     xtest('Smallest prime deficient number is classified correctly', () => {
-      expect(perfectNumbers.classify(2)).toEqual('deficient');
+      expect(classify(2)).toEqual('deficient');
     });
 
     xtest('Smallest non-prime deficient number is classified correctly', () => {
-      expect(perfectNumbers.classify(4)).toEqual('deficient');
+      expect(classify(4)).toEqual('deficient');
     });
 
     xtest('Medium deficient number is classified correctly', () => {
-      expect(perfectNumbers.classify(32)).toEqual('deficient');
+      expect(classify(32)).toEqual('deficient');
     });
 
     xtest('Large deficient number is classified correctly', () => {
-      expect(perfectNumbers.classify(33550337)).toEqual('deficient');
-    });
-
-    xtest('Edge case (no factors other than itself) is classified correctly', () => {
-      expect(perfectNumbers.classify(1)).toEqual('deficient');
-    });
-  });
-
-  describe('Invalid Inputs', () => {
-    xtest('Zero is rejected (not a natural number)', () => {
-      expect(() => perfectNumbers.classify(0))
-        .toThrow('Classification is only possible for natural numbers.');
-    });
-
-    xtest('Negative integer is rejected (not a natural number)', () => {
-      expect(() => perfectNumbers.classify(-1))
-        .toThrow('Classification is only possible for natural numbers.');
+      expect(classify(33550337)).toEqual('deficient');
     });
   });
 });
