e9dffa614195fabf8f73cc6e601afbdc0ad6606f
Maisam Somji <maisam.somji@gmail.com>
2018-11-10 14:48:55 -0500

    lint say exaple (#530)

--- exercises/say/example.js
+++ exercises/say/example.js
@@ -1,79 +1,88 @@
-const smallNumbers = {
-  0: 'zero',
-  1: 'one',
-  2: 'two',
-  3: 'three',
-  4: 'four',
-  5: 'five',
-  6: 'six',
-  7: 'seven',
-  8: 'eight',
-  9: 'nine',
-  10: 'ten',
-  11: 'eleven',
-  12: 'twelve',
-  13: 'thirteen',
-  14: 'fourteen',
-  15: 'fifteen',
-  16: 'sixteen',
-  17: 'seventeen',
-  18: 'eighteen',
-  19: 'nineteen',
-};
-
-const decades = {
-  20: 'twenty',
-  30: 'thirty',
-  40: 'forty',
-  50: 'fifty',
-  60: 'sixty',
-  70: 'seventy',
-  80: 'eighty',
-  90: 'ninety',
-};
-
-const bigNumbers = {
-  1000: 'thousand',
-  1000000: 'million',
-  1000000000: 'billion',
-};
-
-function bigPart(number) {
-  let factor,
-    result = '';
-  for (let bigNumber = 1000000000; bigNumber >= 1000; bigNumber /= 1000) {
-    if (number.current >= bigNumber) {
-      factor = Math.floor(number.current / bigNumber);
-      result += `${threeDigit(factor)} ${bigNumbers[bigNumber]} `;
-      number.current -= factor * bigNumber;
-    }
-  }
-  return result;
-}
-
-const sayDecade = (n) => {
-  for (let decade = 90; decade >= 20; decade -= 10) {
-    if (n >= decade) {
-      return `${decades[decade]}-${smallNumbers[n - decade]}`;
-    }
-  }
-};
-
-const twoDigit = n => n < 20 ? smallNumbers[n] : sayDecade(n);
-
-const threeDigit = n => n < 100 ? twoDigit(n)
-  : `${smallNumbers[Math.floor(n / 100)]} hundred ${twoDigit(n % 100)}`;
 
 export default class Say {
+  constructor() {
+    this.smallNumbers = {
+      0: 'zero',
+      1: 'one',
+      2: 'two',
+      3: 'three',
+      4: 'four',
+      5: 'five',
+      6: 'six',
+      7: 'seven',
+      8: 'eight',
+      9: 'nine',
+      10: 'ten',
+      11: 'eleven',
+      12: 'twelve',
+      13: 'thirteen',
+      14: 'fourteen',
+      15: 'fifteen',
+      16: 'sixteen',
+      17: 'seventeen',
+      18: 'eighteen',
+      19: 'nineteen',
+    };
+
+    this.decades = {
+      20: 'twenty',
+      30: 'thirty',
+      40: 'forty',
+      50: 'fifty',
+      60: 'sixty',
+      70: 'seventy',
+      80: 'eighty',
+      90: 'ninety',
+    };
+
+    this.bigNumbers = {
+      1000: 'thousand',
+      1000000: 'million',
+      1000000000: 'billion',
+    };
+  }
+
   inEnglish(n) {
     let result;
     const number = { current: n };
 
     if (n >= 0 && n < 1000000000000) {
-      result = bigPart(number);
-      result += threeDigit(number.current);
+      result = this.bigPart(number);
+      result += this.threeDigit(number.current);
       return result.replace(/.zero/, '');
     }
     throw new Error('Number must be between 0 and 999,999,999,999.');
+  }
+
+  bigPart(number) {
+    let factor;
+    let result = '';
+    const num = number;
+    for (let bigNumber = 1000000000; bigNumber >= 1000; bigNumber /= 1000) {
+      if (num.current >= bigNumber) {
+        factor = Math.floor(num.current / bigNumber);
+        result += `${this.threeDigit(factor)} ${this.bigNumbers[bigNumber]} `;
+        num.current -= factor * bigNumber;
+      }
+    }
+    return result;
+  }
+
+  twoDigit(n) {
+    return n < 20 ? this.smallNumbers[n] : this.sayDecade(n);
+  }
+
+  threeDigit(n) {
+    return n < 100 ? this.twoDigit(n)
+      : `${this.smallNumbers[Math.floor(n / 100)]} hundred ${this.twoDigit(n % 100)}`;
+  }
+
+  sayDecade(n) {
+    for (let decade = 90; decade >= 20; decade -= 10) {
+      if (n >= decade) {
+        return `${this.decades[decade]}-${this.smallNumbers[n - decade]}`;
+      }
+    }
+    return '';
   }
 }
