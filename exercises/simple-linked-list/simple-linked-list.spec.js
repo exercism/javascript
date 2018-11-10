c68ba80f496fb9ce84538b8d24c368285ac26269
Matthew Morgan <matt@dulcetsoftware.com>
2018-11-10 15:27:14 -0500

    Update simple linked list

--- exercises/simple-linked-list/simple-linked-list.spec.js
+++ exercises/simple-linked-list/simple-linked-list.spec.js
@@ -1,146 +1,122 @@
-import {
-  List,
-  Element,
-  ElementValueRequiredException,
-  ElementNextNotInstanceException,
-} from './simple-linked-list';
+import { List, Element } from './simple-linked-list';
 
-describe('simple-linked-list', () => {
-  test('exports a List constructor', () => {
-    expect(List).toBeDefined();
+describe('Element class', () => {
+  test('has constructor', () => {
+    const element = new Element(1);
+    expect(element.value).toEqual(1);
   });
-
-  xtest('exports a Element constructor', () => {
-    expect(Element).toBeDefined();
+  xtest('value reflects constructor arg', () => {
+    const element = new Element(2);
+    expect(element.value).toEqual(2);
   });
-
-  describe('Element', () => {
-    xtest('is a constructor', () => {
-      const el = new Element(1);
-      expect(el).toBeDefined();
-
-      expect(() => {
-        Element(1);
-      }).toThrow();
-    });
-
-    xtest('requires an argument', () => {
-      const el = new Element(1);
-      expect(el).toBeDefined();
-
-      expect(() => new Element()).toThrow(ElementValueRequiredException);
-    });
-
-    xtest('has as a value', () => {
-      const el = new Element(1);
-      expect(el.value).toBe(1);
-    });
-
-    xtest('reference to next is undefined by default', () => {
-      const el = new Element(1);
-      expect(el.next).toBeUndefined();
-    });
+  xtest('has null for next by default', () => {
+    const element = new Element(1);
+    expect(element.next).toEqual(null);
   });
+});
 
-  xtest('accepts a reference to the next element', () => {
-    const elOne = new Element(1);
-    const elTwo = new Element(2, elOne);
-    expect(elTwo.next).toBe(elOne);
+describe('List class', () => {
+  xtest('has constructor', () => {
+    const list = new List();
+    expect(list).toBeDefined();
   });
-
-  xtest('requires an instance of Element as next element', () => {
-    expect(() => new Element(1, true)).toThrow(ElementNextNotInstanceException);
-    expect(() => new Element(1, 'lorem ipsum')).toThrow(ElementNextNotInstanceException);
-    expect(() => new Element(1, [])).toThrow(ElementNextNotInstanceException);
-    expect(() => new Element(1, {})).toThrow(ElementNextNotInstanceException);
+  xtest('new lists should have length 0', () => {
+    const list = new List();
+    expect(list.length).toEqual(0);
   });
+  xtest('can add a element', () => {
+    const list = new List();
+    const element = new Element(1);
+    expect(() => list.add(element)).not.toThrow();
+  });
+  xtest('adding a element increments length', () => {
+    const list = new List();
+    const element = new Element(1);
+    list.add(element);
+    expect(list.length).toEqual(1);
+  });
+  xtest('adding two elements increments twice', () => {
+    const list = new List();
+    const element1 = new Element(1);
+    const element2 = new Element(3);
+    list.add(element1);
+    list.add(element2);
+    expect(list.length).toEqual(2);
+  });
+  xtest('new Lists have a null head element', () => {
+    const list = new List();
+    expect(list.head).toEqual(null);
+  });
+  xtest('adding an Element to an empty list sets the head Element', () => {
+    const list = new List();
+    const element = new Element(1);
+    list.add(element);
+    expect(list.head.value).toEqual(1);
+  });
+  xtest('adding a second Element updates the head Element', () => {
+    const list = new List();
+    const element1 = new Element(1);
+    const element2 = new Element(3);
+    list.add(element1);
+    list.add(element2);
+    expect(list.head.value).toEqual(3);
+  });
+  xtest('can get the next Element from the head', () => {
+    const list = new List();
+    const element1 = new Element(1);
+    const element2 = new Element(3);
+    list.add(element1);
+    list.add(element2);
+    expect(list.head.next.value).toEqual(1);
+  });
+  xtest('can be initialized with an array', () => {
+    const list = new List([1, 2, 3]);
+    expect(list.length).toEqual(3);
+    expect(list.head.value).toEqual(3);
+  });
+});
 
-  describe('List', () => {
-    xtest('is a constructor', () => {
-      const ll = new List();
-      expect(ll).toBeDefined();
-    });
-
-    xtest('allows you to append an element', () => {
-      const ll = new List();
-      const el = new Element(1);
-      ll.push(el);
-      expect(ll.head).toBe(el);
-
-      const ll2 = new List();
-      const elOne = new Element(1);
-      const elTwo = new Element(2);
-      ll2.push(elOne);
-      ll2.push(elTwo);
-      expect(ll2.head.next).toBe(elTwo);
-    });
-
-    xtest('allows you to prepend an element', () => {
-      const ll = new List();
-      const el = new Element(1);
-      ll.unshift(el);
-      expect(ll.head).toBe(el);
-
-      const ll2 = new List();
-      const elOne = new Element(1);
-      const elTwo = new Element(2);
-      ll2.unshift(elOne);
-      ll2.unshift(elTwo);
-      expect(ll2.head).toBe(elTwo);
-    });
-
-    xtest('allows you to remove the first element', () => {
-      const ll = new List();
-      ll.push(new Element(1));
-      ll.shift();
-      expect(ll.head).toBeUndefined();
-
-      ll.push(new Element(2));
-      ll.push(new Element(3));
-      ll.shift();
-      expect(ll.head.value).toBe(3);
-    });
-
-    xtest('allows you to remove the last element', () => {
-      const ll = new List();
-      ll.push(new Element(1));
-      ll.pop();
-      expect(ll.head).toBeUndefined();
-
-      ll.push(new Element(2));
-      ll.push(new Element(3));
-      ll.pop();
-      expect(ll.head.next).toBeUndefined();
-    });
-
-    xtest('allows you to convert an array to a List', () => {
-      const ll = List.fromArray([1, 2]);
-
-      expect(ll.head.value).toBe(1);
-      expect(ll.head.next.value).toBe(2);
-    });
-
-    xtest('allows you to convert a List into an array', () => {
-      const ll = new List();
-      ll.push(new Element(1));
-      ll.push(new Element(2));
-      const a = ll.toArray();
-
-      expect(a instanceof Array).toBe(true);
-      expect(a.length).toBe(2);
-      expect(a[0]).toBe(1);
-      expect(a[1]).toBe(2);
-
-      expect(ll.head.value).toBe(1);
-    });
-
-    xtest('allows you to reverse a List', () => {
-      const ll = List.fromArray([1, 2, 3]);
-      ll.reverse();
-
-      expect(ll.head.value).toBe(3);
-      expect(ll.head.next.value).toBe(2);
-      expect(ll.head.next.next.value).toBe(1);
-    });
+describe('Lists with multiple elements', () => {
+  let list;
+  beforeEach(() => {
+    list = new List([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  });
+  xtest('with correct length', () => {
+    expect(list.length).toEqual(10);
+  });
+  xtest('with correct head value', () => {
+    expect(list.head.value).toEqual(10);
+  });
+  xtest('can traverse the list', () => {
+    expect(list.head.next.next.next.value).toEqual(7);
+  });
+  xtest('can convert to an array', () => {
+    const oneList = new List([1]);
+    expect(oneList.toArray()).toEqual([1]);
+  });
+  xtest('head of list is final element from input array', () => {
+    const twoList = new List([1, 2]);
+    expect(twoList.head.value).toEqual(2);
+  });
+  xtest('can convert to an array', () => {
+    const oneList = new List([1]);
+    expect(oneList.toArray()).toEqual([1]);
+  });
+  xtest('can convert longer list to an array', () => {
+    expect(list.toArray()).toEqual([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]);
+  });
+  xtest('can be reversed', () => {
+    const twoList = new List([1, 2]);
+    expect(twoList.reverse().toArray()).toEqual([1, 2]);
+  });
+  xtest('can be reversed when it has more elements', () => {
+    const threeList = new List([1, 2, 3]);
+    expect(threeList.reverse().toArray()).toEqual([1, 2, 3]);
+  });
+  xtest('can reverse with many elements', () => {
+    expect(list.reverse().toArray()).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  });
+  xtest('can reverse a reversal', () => {
+    expect(list.reverse().reverse().toArray()).toEqual([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]);
   });
 });
