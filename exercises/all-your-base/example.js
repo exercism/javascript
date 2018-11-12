029c395333bf50f4b0396388ffb358eea63f2bea
Eric Ti Yu Chiang <eric.ti.yu.chiang@gmail.com>
2018-11-12 15:55:01 -0500

    Use named export for all-your-base exercise
    Per #436, change from default export to named export.  In the
    all-your-base exercise, export just `convert`.  The corresponding tests
    can consume like `convert(args)`.

--- exercises/all-your-base/example.js
+++ exercises/all-your-base/example.js
@@ -24,32 +24,26 @@ const convertFromDecimalToBase = (num, outputBase) => {
   return result;
 };
 
-const Converter = () => {};
-
-Converter.prototype = {
-  convert: (array, inputBase, outputBase) => {
-    if (isValidBase(inputBase)) {
-      throw new Error('Wrong input base');
-    }
-    if (isValidBase(outputBase)) {
-      throw new Error('Wrong output base');
-    }
-    const regexp = new RegExp('^0.', 'g');
-    const str = array.join('');
-    if (str.match(regexp)
-      || !isInputValid(array, inputBase)) {
-      throw new Error('Input has wrong format');
-    }
-    if (str === '0') {
-      return [0];
-    }
-    if (str === '1') {
-      return [1];
-    }
-    const decimalValue = array
-      .reduce((accumulator, value) => accumulator * inputBase + value, 0);
-    return convertFromDecimalToBase(decimalValue, outputBase);
-  },
+export const convert = (array, inputBase, outputBase) => {
+  if (isValidBase(inputBase)) {
+    throw new Error('Wrong input base');
+  }
+  if (isValidBase(outputBase)) {
+    throw new Error('Wrong output base');
+  }
+  const regexp = new RegExp('^0.', 'g');
+  const str = array.join('');
+  if (str.match(regexp)
+    || !isInputValid(array, inputBase)) {
+    throw new Error('Input has wrong format');
+  }
+  if (str === '0') {
+    return [0];
+  }
+  if (str === '1') {
+    return [1];
+  }
+  const decimalValue = array
+    .reduce((accumulator, value) => accumulator * inputBase + value, 0);
+  return convertFromDecimalToBase(decimalValue, outputBase);
 };
-
-export default Converter;
