d7a3e357b690125643ca73ae50c8b327d1033b5a
xarxziux <14932450+xarxziux@users.noreply.github.com>
2018-11-18 11:42:56 +0000

    Fix ESLint errors in Flatten Array test and example scripts
    As per issue #480:
    - Fix all `max-len` ESLint errors in flatten-array.spec.js file.
    - Fix all ESLint errors in example.js file.
    - Remove flatten-array from .eslintignore file.

--- exercises/flatten-array/flatten-array.spec.js
+++ exercises/flatten-array/flatten-array.spec.js
@@ -19,22 +19,28 @@ describe('FlattenArray', () => {
   });
 
   xtest('flattens a  3 level nested list', () => {
-    expect(flattener.flatten([1, [2, 3, 4], 5, [6, [7, 8]]])).toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
+    expect(flattener.flatten([1, [2, 3, 4], 5, [6, [7, 8]]]))
+      .toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
   });
 
   xtest('flattens a 5 level nested list', () => {
-    expect(flattener.flatten([0, 2, [[2, 3], 8, 100, 4, [[[50]]]], -2])).toEqual([0, 2, 2, 3, 8, 100, 4, 50, -2]);
+    expect(flattener.flatten([0, 2, [[2, 3], 8, 100, 4, [[[50]]]], -2]))
+      .toEqual([0, 2, 2, 3, 8, 100, 4, 50, -2]);
   });
 
   xtest('flattens a 6 level nested list', () => {
-    expect(flattener.flatten([1, [2, [[3]], [4, [[5]]], 6, 7], 8])).toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
+    expect(flattener.flatten([1, [2, [[3]], [4, [[5]]], 6, 7], 8]))
+      .toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
   });
 
   xtest('flattens a 6 level nested list with null values', () => {
-    expect(flattener.flatten([0, 2, [[2, 3], 8, [[100]], null, [[null]]], -2])).toEqual([0, 2, 2, 3, 8, 100, -2]);
+    expect(flattener.flatten([0, 2, [[2, 3], 8, [[100]], null, [[null]]], -2]))
+      .toEqual([0, 2, 2, 3, 8, 100, -2]);
   });
 
   xtest('returns an empty list if all values in nested list are null', () => {
-    expect(flattener.flatten([null, [[[null]]], null, null, [[null, null], null], null])).toEqual([]);
+    expect(flattener
+      .flatten([null, [[[null]]], null, null, [[null, null], null], null]))
+      .toEqual([]);
   });
 });
