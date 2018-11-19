93786d5a078af4fedb381d2a0a40486e65c85e87
xarxziux <14932450+xarxziux@users.noreply.github.com>
2018-11-19 06:31:15 +0000

    Fix ESLint errors in Flatten Array test and example scripts (#562)
    As per issue #480:
    - Fix all `max-len` ESLint errors in flatten-array.spec.js file.
    - Fix all ESLint errors in example.js file.
    - Remove flatten-array from .eslintignore file.

--- exercises/flatten-array/example.js
+++ exercises/flatten-array/example.js
@@ -1,11 +1,9 @@
 export default class Flattener {
   flatten(arr) {
     return arr
-      .reduce((acc, el) =>
-        Array.isArray(el)
-          ? acc.concat(this.flatten(el))
-          : acc.concat(el),
-      [])
+      .reduce((acc, el) => (Array.isArray(el)
+        ? acc.concat(this.flatten(el))
+        : acc.concat(el)), [])
       .filter(el => el !== null && el !== undefined);
   }
 }
