8437cd73bc6db5bd93cbb66f74f06b8ae955154f
konni2020 <42467294+konni2020@users.noreply.github.com>
2019-01-17 16:33:19 +0800

    Sync pig-latin test cases with canonical data (#575)
    * add test cases for pig latin (#574)
    
    * make test suite consistent with the canonical tests
    
    * update example.js
    
    * update example.js
    
    Closes #574

--- exercises/pig-latin/pig-latin.spec.js
+++ exercises/pig-latin/pig-latin.spec.js
@@ -1,48 +1,103 @@
 import translator from './pig-latin';
 
 describe('Pig Latin', () => {
-  test('translates a word beginning with a', () => {
-    expect(translator.translate('apple')).toEqual('appleay');
+  describe('ay is added to words that start with vowels', () => {
+    test('word beginning with a', () => {
+      expect(translator.translate('apple')).toEqual('appleay');
+    });
+
+    xtest('word beginning with e', () => {
+      expect(translator.translate('ear')).toEqual('earay');
+    });
+
+    xtest('word beginning with i', () => {
+      expect(translator.translate('igloo')).toEqual('iglooay');
+    });
+
+    xtest('word beginning with o', () => {
+      expect(translator.translate('object')).toEqual('objectay');
+    });
+
+    xtest('word beginning with u', () => {
+      expect(translator.translate('under')).toEqual('underay');
+    });
+
+    xtest('word beginning with a vowel and followed by a qu', () => {
+      expect(translator.translate('equal')).toEqual('equalay');
+    });
   });
 
-  xtest('translates a word beginning with e', () => {
-    expect(translator.translate('ear')).toEqual('earay');
+  describe('first letter and ay are moved to the end of words that start with consonants', () => {
+    xtest('word beginning with p', () => {
+      expect(translator.translate('pig')).toEqual('igpay');
+    });
+
+    xtest('word beginning with k', () => {
+      expect(translator.translate('koala')).toEqual('oalakay');
+    });
+
+    xtest('word beginning with x', () => {
+      expect(translator.translate('xenon')).toEqual('enonxay');
+    });
+
+    xtest('word beginning with q without a following u', () => {
+      expect(translator.translate('qat')).toEqual('atqay');
+    });
   });
 
-  xtest('translates a word beginning with p', () => {
-    expect(translator.translate('pig')).toEqual('igpay');
+  describe('some letter clusters are treated like a single consonant', () => {
+    xtest('word beginning with ch', () => {
+      expect(translator.translate('chair')).toEqual('airchay');
+    });
+
+    xtest('word beginning with qu', () => {
+      expect(translator.translate('queen')).toEqual('eenquay');
+    });
+
+    xtest('word beginning with qu and a preceding consonant', () => {
+      expect(translator.translate('square')).toEqual('aresquay');
+    });
+
+    xtest('word beginning with th', () => {
+      expect(translator.translate('therapy')).toEqual('erapythay');
+    });
+
+    xtest('word beginning with thr', () => {
+      expect(translator.translate('thrush')).toEqual('ushthray');
+    });
+
+    xtest('word beginning with sch', () => {
+      expect(translator.translate('school')).toEqual('oolschay');
+    });
   });
 
-  xtest('translates a word beginning with k', () => {
-    expect(translator.translate('koala')).toEqual('oalakay');
+  describe('some letter clusters are treated like a single vowel', () => {
+    xtest('word beginning with yt', () => {
+      expect(translator.translate('yttria')).toEqual('yttriaay');
+    });
+
+    xtest('word beginning with xr', () => {
+      expect(translator.translate('xray')).toEqual('xrayay');
+    });
   });
 
-  xtest('translates a word beginning with ch', () => {
-    expect(translator.translate('chair')).toEqual('airchay');
+  describe('position of y in a word determines if it is a consonant or a vowel', () => {
+    xtest('y is treated like a consonant at the beginning of a word', () => {
+      expect(translator.translate('yellow')).toEqual('ellowyay');
+    });
+
+    xtest('y is treated like a vowel at the end of a consonant cluster', () => {
+      expect(translator.translate('rhythm')).toEqual('ythmrhay');
+    });
+
+    xtest('y as second letter in two letter word', () => {
+      expect(translator.translate('my')).toEqual('ymay');
+    });
   });
 
-  xtest('translates a word beginning with qu', () => {
-    expect(translator.translate('queen')).toEqual('eenquay');
-  });
-
-  xtest('translates a word with a consonant preceding qu', () => {
-    expect(translator.translate('square')).toEqual('aresquay');
-  });
-
-  xtest('translates a word beginning with th', () => {
-    expect(translator.translate('therapy')).toEqual('erapythay');
-  });
-
-  xtest('translates a word beginning with thr', () => {
-    expect(translator.translate('thrush')).toEqual('ushthray');
-  });
-
-  xtest('translates a word beginning with sch', () => {
-    expect(translator.translate('school')).toEqual('oolschay');
-  });
-
-  xtest('translates a phrase', () => {
-    expect(translator.translate('quick fast run'))
-      .toEqual('ickquay astfay unray');
+  describe('phrases are translated', () => {
+    xtest('a whole phrase', () => {
+      expect(translator.translate('quick fast run')).toEqual('ickquay astfay unray');
+    });
   });
 });
