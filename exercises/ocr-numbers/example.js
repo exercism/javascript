d6199ab302890937872fb65c4dd75097539c4de9
xarxziux <14932450+xarxziux@users.noreply.github.com>
2019-01-23 11:19:53 +0000

    Restore updates to OCR Numbers exercise (#567)

--- exercises/ocr-numbers/example.js
+++ exercises/ocr-numbers/example.js
@@ -40,50 +40,44 @@ const PATTERNS = {
     ' _|',
     '   '],
 };
-export default class Parser {
-  // eslint-disable-next-line class-methods-use-this
-  convert(text) {
-    return Parser.splitIntoRows(text).map(Parser.valuesInRow).join(',');
-  }
 
-  static valuesInRow(row) {
-    return Parser.splitIntoDigits(row).map(Parser.getDigit).join('');
-  }
-
-  static splitIntoRows(text) {
-    const rows = [];
-    const lines = text.split('\n');
-    for (let rowNumber = 0; rowNumber < lines.length; rowNumber += 4) {
-      let row = '';
-      for (let rowLine = 0; rowLine < 4; rowLine += 1) {
-        row += `${lines[rowNumber + rowLine]}\n`;
-      }
-      rows.push(row.slice(0, -1));
+const splitIntoRows = (text) => {
+  const rows = [];
+  const lines = text.split('\n');
+  for (let rowNumber = 0; rowNumber < lines.length; rowNumber += 4) {
+    let row = '';
+    for (let rowLine = 0; rowLine < 4; rowLine += 1) {
+      row += `${lines[rowNumber + rowLine]}\n`;
     }
-    return rows;
+    rows.push(row.slice(0, -1));
   }
+  return rows;
+};
 
-  static splitIntoDigits(row) {
-    const digits = [];
-    const rows = row.split('\n');
-    for (let digitNumber = 0; digitNumber < rows[0].length; digitNumber += 3) {
-      let digit = '';
-      for (let rowNumber = 0; rowNumber < rows.length; rowNumber += 1) {
-        digit += rows[rowNumber].substr(digitNumber, 3);
-      }
-      digits.push(digit);
+const splitIntoDigits = (row) => {
+  const digits = [];
+  const rows = row.split('\n');
+  for (let digitNumber = 0; digitNumber < rows[0].length; digitNumber += 3) {
+    let digit = '';
+    for (let rowNumber = 0; rowNumber < rows.length; rowNumber += 1) {
+      digit += rows[rowNumber].substr(digitNumber, 3);
     }
-    return digits;
+    digits.push(digit);
   }
+  return digits;
+};
 
-  static getDigit(text) {
-    const digit = Object
-      .values(PATTERNS)
-      .map(x => x.join(''))
-      .indexOf(text);
-    if (digit === -1) {
-      return '?';
-    }
-    return digit;
+const getDigit = (text) => {
+  const digit = Object
+    .values(PATTERNS)
+    .map(x => x.join(''))
+    .indexOf(text);
+  if (digit === -1) {
+    return '?';
   }
-}
+  return digit;
+};
+
+const valuesInRow = row => splitIntoDigits(row).map(getDigit).join('');
+
+export const convert = text => splitIntoRows(text).map(valuesInRow).join(',');
