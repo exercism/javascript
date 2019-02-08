bbfc49fc33b7fa156b11b22f732e21b6a4de87cd
xarxziux <14932450+xarxziux@users.noreply.github.com>
2019-02-08 09:41:59 +0000

    Minor formatting fixes as identified by ESLint (#603)

--- exercises/difference-of-squares/example.js
+++ exercises/difference-of-squares/example.js
@@ -5,18 +5,20 @@ export class Squares {
 
   get squareOfSum() {
     let sum = 0;
-    for (let x = 1; x <= this.max; x++) {
+    for (let x = 1; x <= this.max; x += 1) {
       sum += x;
     }
     return sum * sum;
   }
+
   get sumOfSquares() {
     let sum = 0;
-    for (let x = 1; x <= this.max; x++) {
+    for (let x = 1; x <= this.max; x += 1) {
       sum += x * x;
     }
     return sum;
   }
+
   get difference() {
     return this.squareOfSum - this.sumOfSquares;
   }
