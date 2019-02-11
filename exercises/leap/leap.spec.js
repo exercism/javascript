f6d53b88dab2a0a9fef3b5274468f7410a214aa0
Tejas Bubane <tejasbubane@gmail.com>
2019-02-11 19:16:52 +0530

    Use exact boolean values in leap tests rather than truthy and falsy

--- exercises/leap/leap.spec.js
+++ exercises/leap/leap.spec.js
@@ -2,22 +2,22 @@ import { isLeap } from './leap';
 
 describe('A leap year', () => {
   test('year not divisible by 4: common year', () => {
-    expect(isLeap(2015)).toBeFalsy();
+    expect(isLeap(2015)).toBe(false);
   });
 
   xtest('year divisible by 4, not divisible by 100: leap year', () => {
-    expect(isLeap(2016)).toBeTruthy();
+    expect(isLeap(2016)).toBe(true);
   });
 
   xtest('year divisible by 100, not divisible by 400: common year', () => {
-    expect(isLeap(2100)).toBeFalsy();
+    expect(isLeap(2100)).toBe(false);
   });
 
   xtest('year divisible by 400: leap year', () => {
-    expect(isLeap(2000)).toBeTruthy();
+    expect(isLeap(2000)).toBe(true);
   });
 
   xtest('year divisible by 200, not divisible by 400: common year', () => {
-    expect(isLeap(1800)).toBeFalsy();
+    expect(isLeap(1800)).toBe(false);
   });
 });
