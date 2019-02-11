5a21981e21e00f19d1b8177d5f826676a019a5b6
Corey McCandless <cmccandless@users.noreply.github.com>
2019-02-11 11:19:36 -0500

    break example solution into separate lines for readability

--- exercises/acronym/example.js
+++ exercises/acronym/example.js
@@ -1,1 +1,5 @@
-export const parse = phrase => phrase.replace("'", '').match(/^[A-Z]|(?<=[^A-Z])[A-Z]|\b[a-z]/g).join('').toUpperCase();
+export const parse = phrase => phrase
+  .replace("'", '')
+  .match(/^[A-Z]|(?<=[^A-Z])[A-Z]|\b[a-z]/g)
+  .join('')
+  .toUpperCase();
