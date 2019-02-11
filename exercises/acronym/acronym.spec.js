cea608f287482372965ab990ec2ef26a63606c15
Corey McCandless <cmccandless@users.noreply.github.com>
2019-02-11 10:29:46 -0500

    acronym: update test suite
    Bring test suite up-to-date with canonical-data.json v1.7.0

--- exercises/acronym/acronym.spec.js
+++ exercises/acronym/acronym.spec.js
@@ -9,19 +9,32 @@ describe('Acronyms are produced from', () => {
     expect(parse('Ruby on Rails')).toEqual('ROR');
   });
 
-  xtest('inconsistently cased phrases', () => {
-    expect(parse('HyperText Markup Language')).toEqual('HTML');
-  });
-
   xtest('phrases with punctuation', () => {
     expect(parse('First In, First Out')).toEqual('FIFO');
   });
 
-  xtest('other phrases with punctuation', () => {
-    expect(parse('PHP: Hypertext Preprocessor')).toEqual('PHP');
+  xtest('phrases with all uppercase words', () => {
+    expect(parse('GNU Image Manipulation Program')).toEqual('GIMP');
   });
 
-  xtest('phrases with punctuation and sentence casing', () => {
+  xtest('phrases with punctuation without whitespace', () => {
     expect(parse('Complementary metal-oxide semiconductor')).toEqual('CMOS');
+  });
+  
+  xtest('long phrases', () => {
+    expect(parse('Rolling On The Floor Laughing So Hard That My Dogs Came Over And Licked Me')
+       .toEqual('ROTFLSHTMDCOALM');
+   });
+
+  xtest('phrases with consecutive delimiters', () => {
+    expect(parse('Something - I made up from thin air')).toEqual('SIMUFTA');
+  });
+
+  xtest('phrases with apostrophes', () => {
+    expect(parse("Halley's Comet")).toEqual('HC');
+  });
+
+  xtest('phrases with underscore emphasis', () => {
+    expect(parse('The Road _Not_ Taken')).toEqual('TRNT');
   });
 });
