95f2a20c84556f80432375f594b64bc9e9402d5c
Sarah Martinelli Benedetti <s-smb@hotmail.com>
2019-02-17 02:01:05 -0500

    add named export to strain (#615)

--- exercises/strain/example.js
+++ exercises/strain/example.js
@@ -1,18 +1,13 @@
-const Strain = {
-  strain: (array, filter, keepMatches) => {
-    const results = [];
-    for (let i = 0; i < array.length; i += 1) {
-      const item = array[i];
-      if (filter(item) === keepMatches) {
-        results.push(item);
-      }
+const strain = (array, filter, keepMatches) => {
+  const results = [];
+  for (let i = 0; i < array.length; i += 1) {
+    const item = array[i];
+    if (filter(item) === keepMatches) {
+      results.push(item);
     }
-    return results;
-  },
-
-  keep: (array, filter) => Strain.strain(array, filter, true),
-  discard: (array, filter) => Strain.strain(array, filter, false),
-
+  }
+  return results;
 };
 
-export default Strain;
+export const keep = (array, filter) => strain(array, filter, true);
+export const discard = (array, filter) => strain(array, filter, false);
--- exercises/strain/strain.spec.js
+++ exercises/strain/strain.spec.js
@@ -1,25 +1,25 @@
-import strain from './strain';
+import { keep, discard } from './strain';
 
 describe('strain', () => {
   test('keeps on empty array returns empty array', () => {
-    expect(strain.keep([], e => e < 10)).toEqual([]);
+    expect(keep([], e => e < 10)).toEqual([]);
   });
 
   xtest('keeps everything ', () => {
-    expect(strain.keep([1, 2, 3], e => e < 10)).toEqual([1, 2, 3]);
+    expect(keep([1, 2, 3], e => e < 10)).toEqual([1, 2, 3]);
   });
 
   xtest('keeps first and last', () => {
-    expect(strain.keep([1, 2, 3], e => e % 2 === 1)).toEqual([1, 3]);
+    expect(keep([1, 2, 3], e => e % 2 === 1)).toEqual([1, 3]);
   });
 
   xtest('keeps neither first nor last', () => {
-    expect(strain.keep([1, 2, 3, 4, 5], e => e % 2 === 0)).toEqual([2, 4]);
+    expect(keep([1, 2, 3, 4, 5], e => e % 2 === 0)).toEqual([2, 4]);
   });
 
   xtest('keeps strings', () => {
     const words = 'apple zebra banana zombies cherimoya zelot'.split(' ');
-    const result = strain.keep(words, word => word.indexOf('z') === 0);
+    const result = keep(words, word => word.indexOf('z') === 0);
     expect(result).toEqual('zebra zombies zelot'.split(' '));
   });
 
@@ -33,30 +33,30 @@ describe('strain', () => {
       [2, 2, 1],
       [1, 2, 5],
     ];
-    const result = strain.keep(rows, row => row.indexOf(5) > -1);
+    const result = keep(rows, row => row.indexOf(5) > -1);
     expect(result).toEqual([[5, 5, 5], [5, 1, 2], [1, 5, 2], [1, 2, 5]]);
   });
 
   xtest('empty discard', () => {
-    expect(strain.discard([], e => e < 10)).toEqual([]);
+    expect(discard([], e => e < 10)).toEqual([]);
   });
 
   xtest('discards nothing', () => {
-    expect(strain.discard([1, 2, 3], e => e > 10)).toEqual([1, 2, 3]);
+    expect(discard([1, 2, 3], e => e > 10)).toEqual([1, 2, 3]);
   });
 
   xtest('discards first and last', () => {
-    expect(strain.discard([1, 2, 3], e => e % 2 === 1)).toEqual([2]);
+    expect(discard([1, 2, 3], e => e % 2 === 1)).toEqual([2]);
   });
 
   xtest('discards neither first nor last', () => {
-    const result = strain.discard([1, 2, 3, 4, 5], e => e % 2 === 0);
+    const result = discard([1, 2, 3, 4, 5], e => e % 2 === 0);
     expect(result).toEqual([1, 3, 5]);
   });
 
   xtest('discards strings', () => {
     const words = 'apple zebra banana zombies cherimoya zelot'.split(' ');
-    const result = strain.discard(words, word => word.indexOf('z') === 0);
+    const result = discard(words, word => word.indexOf('z') === 0);
     expect(result).toEqual('apple banana cherimoya'.split(' '));
   });
 
@@ -70,7 +70,7 @@ describe('strain', () => {
       [2, 2, 1],
       [1, 2, 5],
     ];
-    const result = strain.discard(rows, row => row.indexOf(5) > -1);
+    const result = discard(rows, row => row.indexOf(5) > -1);
     expect(result).toEqual([[1, 2, 3], [2, 1, 2], [2, 2, 1]]);
   });
 });
