2441a044f0f8caf13ffbfdc9dc1ab52a26466cd5
Sarah Martinelli <s-smb@hotmail.com>
2019-02-18 17:20:19 -0500

    add named export to hexadecimal

--- exercises/hexadecimal/example.js
+++ exercises/hexadecimal/example.js
@@ -1,13 +1,9 @@
-export default function (hex) {
-  this.hex = hex;
+export const toDecimal = (hex) => {
+  const hexCharacters = [...hex];
 
-  this.toDecimal = () => {
-    const hexCharacters = [...this.hex];
+  for (let i = 0; i < hexCharacters.length; i += 1) {
+    if (/[^0-9a-fA-F]/.exec(hexCharacters[i])) { return 0; }
+  }
 
-    for (let i = 0; i < hexCharacters.length; i += 1) {
-      if (/[^0-9a-fA-F]/.exec(hexCharacters[i])) { return 0; }
-    }
-
-    return parseInt(this.hex, 16);
-  };
-}
+  return parseInt(hex, 16);
+};
