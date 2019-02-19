2413d981aa92e49c375254d623a41da027a10107
Sarah Martinelli Benedetti <s-smb@hotmail.com>
2019-02-19 06:32:07 -0500

    add named export to sieve (#618)

--- exercises/sieve/sieve.spec.js
+++ exercises/sieve/sieve.spec.js
@@ -1,16 +1,16 @@
-import Sieve from './sieve';
+import { primes } from './sieve';
 
 describe('Sieve', () => {
   test('finds primes up to 10', () => {
-    expect(new Sieve(10).primes).toEqual([2, 3, 5, 7]);
+    expect(primes(10)).toEqual([2, 3, 5, 7]);
   });
 
   xtest('finds primes up to 13, and considers the limit passed in', () => {
-    expect(new Sieve(13).primes).toEqual([2, 3, 5, 7, 11, 13]);
+    expect(primes(13)).toEqual([2, 3, 5, 7, 11, 13]);
   });
 
   xtest('finds primes up to 1000', () => {
-    expect(new Sieve(1000).primes).toEqual([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47,
+    expect(primes(1000)).toEqual([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47,
       53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151,
       157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257,
       263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373,
