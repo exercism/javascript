e7fdaa71215ec35dd98270c838f147e70de5c9f1
Sarah Martinelli Benedetti <s-smb@hotmail.com>
2019-02-19 06:37:31 -0500

    add named export to hexadecimal (#616)

--- exercises/hexadecimal/hexadecimal.spec.js
+++ exercises/hexadecimal/hexadecimal.spec.js
@@ -1,53 +1,53 @@
-import Hexadecimal from './hexadecimal';
+import { toDecimal } from './hexadecimal';
 
 describe('Hexadecimal', () => {
   test('hex 1 is decimal 1', () => {
-    const hex = new Hexadecimal('1');
-    expect(hex.toDecimal()).toEqual(1);
+    const hex = toDecimal('1');
+    expect(hex).toEqual(1);
   });
 
   xtest('hex c is decimal 12', () => {
-    const hex = new Hexadecimal('c');
-    expect(hex.toDecimal()).toEqual(12);
+    const hex = toDecimal('c');
+    expect(hex).toEqual(12);
   });
 
   xtest('hex 10 is decimal 16', () => {
-    const hex = new Hexadecimal('10');
-    expect(hex.toDecimal()).toEqual(16);
+    const hex = toDecimal('10');
+    expect(hex).toEqual(16);
   });
 
   xtest('hex af is decimal 175', () => {
-    const hex = new Hexadecimal('af');
-    expect(hex.toDecimal()).toEqual(175);
+    const hex = toDecimal('af');
+    expect(hex).toEqual(175);
   });
 
   xtest('hex 100 is decimal 256', () => {
-    const hex = new Hexadecimal('100');
-    expect(hex.toDecimal()).toEqual(256);
+    const hex = toDecimal('100');
+    expect(hex).toEqual(256);
   });
 
   xtest('hex 19ace is decimal 105166', () => {
-    const hex = new Hexadecimal('19ace');
-    expect(hex.toDecimal()).toEqual(105166);
+    const hex = toDecimal('19ace');
+    expect(hex).toEqual(105166);
   });
 
   xtest('invalid hex is decimal 0', () => {
-    const hex = new Hexadecimal('carrot');
-    expect(hex.toDecimal()).toEqual(0);
+    const hex = toDecimal('carrot');
+    expect(hex).toEqual(0);
   });
 
   xtest('hexadecimal value for HTML/CSS black color', () => {
-    const hex = new Hexadecimal('000000');
-    expect(hex.toDecimal()).toEqual(0);
+    const hex = toDecimal('000000');
+    expect(hex).toEqual(0);
   });
 
   xtest('hexadecimal value for HTML/CSS white color', () => {
-    const hex = new Hexadecimal('ffffff');
-    expect(hex.toDecimal()).toEqual(16777215);
+    const hex = toDecimal('ffffff');
+    expect(hex).toEqual(16777215);
   });
 
   xtest('hexadecimal value for HTML/CSS yellow color', () => {
-    const hex = new Hexadecimal('ffff00');
-    expect(hex.toDecimal()).toEqual(16776960);
+    const hex = toDecimal('ffff00');
+    expect(hex).toEqual(16776960);
   });
 });
