40dadc75d8668e8936c6f9f2dd0fffde0b400697
Sarah Martinelli Benedetti <s-smb@hotmail.com>
2019-02-25 02:41:59 -0500

    add named export to sum of multiples (#619)

--- exercises/sum-of-multiples/sum-of-multiples.spec.js
+++ exercises/sum-of-multiples/sum-of-multiples.spec.js
@@ -1,39 +1,39 @@
-import SumOfMultiples from './sum-of-multiples';
+import { sumOfMultiples } from './sum-of-multiples';
 
 describe('SumOfMultiples', () => {
   test('to 1', () => {
-    expect(SumOfMultiples([3, 5]).to(1)).toBe(0);
+    expect(sumOfMultiples([3, 5], 1)).toBe(0);
   });
 
   xtest('to 3', () => {
-    expect(SumOfMultiples([3, 5]).to(4)).toBe(3);
+    expect(sumOfMultiples([3, 5], 4)).toBe(3);
   });
 
   xtest('to 10', () => {
-    expect(SumOfMultiples([3, 5]).to(10)).toBe(23);
+    expect(sumOfMultiples([3, 5], 10)).toBe(23);
   });
 
   xtest('to 100', () => {
-    expect(SumOfMultiples([3, 5]).to(100)).toBe(2318);
+    expect(sumOfMultiples([3, 5], 100)).toBe(2318);
   });
 
   xtest('to 1000', () => {
-    expect(SumOfMultiples([3, 5]).to(1000)).toBe(233168);
+    expect(sumOfMultiples([3, 5], 1000)).toBe(233168);
   });
 
   xtest('[7, 13, 17] to 20', () => {
-    expect(SumOfMultiples([7, 13, 17]).to(20)).toBe(51);
+    expect(sumOfMultiples([7, 13, 17], 20)).toBe(51);
   });
 
   xtest('[4, 6] to 15', () => {
-    expect(SumOfMultiples([4, 6]).to(15)).toBe(30);
+    expect(sumOfMultiples([4, 6], 15)).toBe(30);
   });
 
   xtest('[5, 6, 8] to 150', () => {
-    expect(SumOfMultiples([5, 6, 8]).to(150)).toBe(4419);
+    expect(sumOfMultiples([5, 6, 8], 150)).toBe(4419);
   });
 
   xtest('[43, 47] to 10000', () => {
-    expect(SumOfMultiples([43, 47]).to(10000)).toBe(2203160);
+    expect(sumOfMultiples([43, 47], 10000)).toBe(2203160);
   });
 });
