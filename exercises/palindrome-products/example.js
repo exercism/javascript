8971117e59bcc6447868713be4d67e48bbc32ff8
Corey McCandless <corey.mccandless@atlasied.com>
2019-03-01 14:42:29 -0500

    update example solution

--- exercises/palindrome-products/example.js
+++ exercises/palindrome-products/example.js
@@ -1,36 +1,38 @@
 const isPalindrome = num => num.toString().split('').reverse().join('') === num.toString();
 
-export const generatePalindrome = (params) => {
-  let maxFactor,
-    minFactor,
-    maxProduct,
-    minProduct,
-    data;
-  maxFactor = params.maxFactor;
-  minFactor = params.minFactor || 1;
-  maxProduct = 1;
-  minProduct = Infinity;
-  data = [];
+export class Palindrome {
+  static generate(params) {
+    let maxFactor,
+      minFactor,
+      maxProduct,
+      minProduct,
+      data;
+    maxFactor = params.maxFactor;
+    minFactor = params.minFactor || 1;
+    maxProduct = 1;
+    minProduct = Infinity;
+    data = [];
 
-  for (let ii = minFactor; ii < maxFactor; ii++) {
-    for (let jj = ii; jj <= maxFactor; jj++) {
-      const product = ii * jj;
-      if (isPalindrome(product)) {
-        data[product] = [ii, jj];
-        maxProduct = Math.max(maxProduct, product);
-        minProduct = Math.min(minProduct, product);
+    for (let ii = minFactor; ii < maxFactor; ii++) {
+      for (let jj = ii; jj <= maxFactor; jj++) {
+        const product = ii * jj;
+        if (isPalindrome(product)) {
+          data[product] = [ii, jj];
+          maxProduct = Math.max(maxProduct, product);
+          minProduct = Math.min(minProduct, product);
+        }
       }
     }
-  }
 
-  return {
-    largest: {
-      value: maxProduct,
-      factors: data[maxProduct],
-    },
-    smallest: {
-      value: minProduct,
-      factors: data[minProduct],
-    },
-  };
-};
+    return {
+      largest: {
+        value: maxProduct,
+        factors: data[maxProduct],
+      },
+      smallest: {
+        value: minProduct,
+        factors: data[minProduct],
+      },
+    };
+  }
+}
