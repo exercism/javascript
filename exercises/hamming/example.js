e347bb20165ba2ea277bfdb059b639ac1b9e4951
Gabriel do Nascimento Ribeiro <gabriel376@hotmail.com>
2019-03-06 04:24:42 -0300

    update hamming to v2.3.0 (#632)

--- exercises/hamming/example.js
+++ exercises/hamming/example.js
@@ -1,6 +1,15 @@
 export const compute = (strand1, strand2) => {
   const len1 = strand1.length;
   const len2 = strand2.length;
+
+  if (!len1 && len2) {
+    throw new Error('left strand must not be empty');
+  }
+
+  if (len1 && !len2) {
+    throw new Error('right strand must not be empty');
+  }
+
   if (len1 !== len2) {
     throw new Error('left and right strands must be of equal length');
   }
--- exercises/hamming/hamming.spec.js
+++ exercises/hamming/hamming.spec.js
@@ -1,55 +1,23 @@
 import { compute } from './hamming';
 
 describe('Hamming', () => {
-  test('no difference between empty strands', () => {
+  test('empty strands', () => {
     expect(compute('', '')).toEqual(0);
   });
 
-  xtest('no difference between identical strands', () => {
+  xtest('single letter identical strands', () => {
     expect(compute('A', 'A')).toEqual(0);
   });
 
+  xtest('single letter different strands', () => {
+    expect(compute('G', 'T')).toEqual(1);
+  });
+
   xtest('long identical strands', () => {
-    expect(compute('GGACTGA', 'GGACTGA')).toEqual(0);
+    expect(compute('GGACTGAAATCTG', 'GGACTGAAATCTG')).toEqual(0);
   });
 
-  xtest('complete distance in single nucleotide strands', () => {
-    expect(compute('A', 'G')).toEqual(1);
-  });
-
-  xtest('complete distance in small strands', () => {
-    expect(compute('AG', 'CT')).toEqual(2);
-  });
-
-  xtest('small distance in small strands', () => {
-    expect(compute('AT', 'CT')).toEqual(1);
-  });
-
-  xtest('small distance', () => {
-    expect(compute('GGACG', 'GGTCG')).toEqual(1);
-  });
-
-  xtest('small distance in long strands', () => {
-    expect(compute('ACCAGGG', 'ACTATGG')).toEqual(2);
-  });
-
-  xtest('non-unique character in first strand', () => {
-    expect(compute('AAG', 'AAA')).toEqual(1);
-  });
-
-  xtest('non-unique character in second strand', () => {
-    expect(compute('AAA', 'AAG')).toEqual(1);
-  });
-
-  xtest('same nucleotides in different positions', () => {
-    expect(compute('TAG', 'GAT')).toEqual(2);
-  });
-
-  xtest('large distance', () => {
-    expect(compute('GATACA', 'GCATAA')).toEqual(4);
-  });
-
-  xtest('large distance in off-by-one strand', () => {
+  xtest('long different strands', () => {
     expect(compute('GGACGGATTCTG', 'AGGACGGATTCT')).toEqual(9);
   });
 
@@ -62,6 +30,18 @@ describe('Hamming', () => {
   xtest('disallow second strand longer', () => {
     expect(() => compute('ATA', 'AGTG')).toThrow(
       new Error('left and right strands must be of equal length'),
+    );
+  });
+
+  xtest('disallow left empty strand', () => {
+    expect(() => compute('', 'G')).toThrow(
+      new Error('left strand must not be empty'),
+    );
+  });
+
+  xtest('disallow right empty strand', () => {
+    expect(() => compute('G', '')).toThrow(
+      new Error('right strand must not be empty'),
     );
   });
 });
