6cd2d2ceaf55dcfa6974f57d447174a7218938c7
Tejas Bubane <tejasbubane@gmail.com>
2019-03-22 12:58:47 +0530

    Implement resistor-colors exercise
    Problem spec PR: https://github.com/exercism/problem-specifications/pull/1466

---
+++ exercises/resistor-colors/resistor-colors.spec.js
@@ -0,0 +1,19 @@
+import { value } from './resistor-colors.js';
+
+describe('Resistor Colors', () => {
+  test('Brown and black', () => {
+    expect(value(['brown', 'black'])).toEqual(10);
+  });
+
+  xtest('Blue and grey', () => {
+    expect(value(['blue', 'grey'])).toEqual(68);
+  });
+
+  xtest('Yellow and violet', () => {
+    expect(value(['yellow', 'violet'])).toEqual(47);
+  });
+
+  xtest('Orange and orange', () => {
+    expect(value(['orange', 'orange'])).toEqual(33);
+  });
+});
