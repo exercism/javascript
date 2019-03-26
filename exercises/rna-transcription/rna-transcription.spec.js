67906e584d488e3253b59ec63685716d89b22b03
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2019-03-26 19:10:54 +0100

    Sync rna-transcription to 1.3.0 (#650)
    Matching the data on [28 July 2018](https://github.com/exercism/problem-specifications/blob/294c831e290670d2932365754317e92f09baacbc/exercises/rna-transcription/canonical-data.json).

--- exercises/rna-transcription/rna-transcription.spec.js
+++ exercises/rna-transcription/rna-transcription.spec.js
@@ -1,6 +1,6 @@
-import { toRna } from './rna-transcription';
+import { toRna } from './rna-transcription'
 
-describe('Transcriptor', () => {
+describe('Transcription', () => {
   test('empty rna sequence', () => {
     expect(toRna('')).toEqual('');
   });
@@ -13,28 +13,15 @@ describe('Transcriptor', () => {
     expect(toRna('G')).toEqual('C');
   });
 
-  xtest('transcribes adenine to uracil', () => {
-    expect(toRna('A')).toEqual('U');
-  });
-
   xtest('transcribes thymine to adenine', () => {
     expect(toRna('T')).toEqual('A');
   });
 
+  xtest('transcribes adenine to uracil', () => {
+    expect(toRna('A')).toEqual('U');
+  });
+
   xtest('transcribes all dna nucleotides to their rna complements', () => {
-    expect(toRna('ACGTGGTCTTAA'))
-      .toEqual('UGCACCAGAAUU');
+    expect(toRna('ACGTGGTCTTAA')).toEqual('UGCACCAGAAUU');
   });
-
-  xtest('correctly handles invalid input', () => {
-    expect(() => toRna('U')).toThrow(new Error('Invalid input DNA.'));
-  });
-
-  xtest('correctly handles completely invalid input', () => {
-    expect(() => toRna('XXX')).toThrow(new Error('Invalid input DNA.'));
-  });
-
-  xtest('correctly handles partially invalid input', () => {
-    expect(() => toRna('ACGTXXXCTTAA')).toThrow(new Error('Invalid input DNA.'));
-  });
-});
+})
