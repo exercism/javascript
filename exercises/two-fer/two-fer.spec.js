3ca7abad0f5d317684dea91b6bf5b9e4c0780e81
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2019-03-26 16:41:46 +0100

    :fire: woops

--- exercises/two-fer/two-fer.spec.js
+++ exercises/two-fer/two-fer.spec.js
@@ -1,18 +1,14 @@
 import { twoFer } from './two-fer.js'
 
-describe('no name given', () => {
+describe('twoFer()', () => {
   test('no name given', () => {
     expect(twoFer()).toEqual("One for you, one for me.")
   })
-})
 
-describe('a name given', () => {
   xtest('a name given', () => {
     expect(twoFer("Alice")).toEqual("One for Alice, one for me.")
   })
-})
 
-describe('another name given', () => {
   xtest('another name given', () => {
     expect(twoFer("Bob")).toEqual("One for Bob, one for me.")
   })
