9e27b688ddb2c4736e4b6d13b402b79ef3d30ac9
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2019-03-26 17:11:00 +0100

    Remove doubly nested test <> description

--- exercises/resistor-color/resistor-color.spec.js
+++ exercises/resistor-color/resistor-color.spec.js
@@ -1,26 +1,20 @@
 import { colorCode, COLORS } from './resistor-color.js'
 
-describe('Color codes', () => {
-  describe('Black', () => {
-    xtest('Black', () => {
+describe('ResistorColor', () => {
+  describe('Color codes', () => {
+    test('Black', () => {
       expect(colorCode("black")).toEqual(0)
     })
-  })
 
-  describe('White', () => {
     xtest('White', () => {
       expect(colorCode("white")).toEqual(9)
     })
-  })
 
-  describe('Orange', () => {
     xtest('Orange', () => {
       expect(colorCode("orange")).toEqual(3)
     })
   })
-})
 
-describe('Colors', () => {
   xtest('Colors', () => {
     expect(COLORS).toEqual(["black","brown","red","orange","yellow","green","blue","violet","grey","white"])
   })
