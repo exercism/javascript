7d729679ba5e1fb5a497c5621056fc957e7ec4e4
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2019-04-15 12:19:49 +0200

    bracket-push: rename to matching-brackets
    This rename was proposed in:
    exercism/problem-specifications#1501
    
    The rationale for the name is:
    
    * to name the exercise by its story, not by what it potentially teaches
    * to avoid unnecessarily biasing the solution space

--- exercises/matching-brackets/matching-brackets.spec.js
+++ exercises/matching-brackets/matching-brackets.spec.js
@@ -1,35 +1,35 @@
-import { bracketPush } from './bracket-push';
+import { matchingBrackets } from './matching-brackets';
 
 describe('bracket push', () => {
   test('checks for appropriate bracketing in a set of brackets', () => {
-    expect(bracketPush('{}')).toEqual(true);
+    expect(matchingBrackets('{}')).toEqual(true);
   });
 
   xtest('returns false for unclosed brackets', () => {
-    expect(bracketPush('{{')).toEqual(false);
+    expect(matchingBrackets('{{')).toEqual(false);
   });
 
   xtest('returns false if brackets are out of order', () => {
-    expect(bracketPush('}{')).toEqual(false);
+    expect(matchingBrackets('}{')).toEqual(false);
   });
 
   xtest('checks bracketing in more than one pair of brackets', () => {
-    expect(bracketPush('{}[]')).toEqual(true);
+    expect(matchingBrackets('{}[]')).toEqual(true);
   });
 
   xtest('checks bracketing in nested brackets', () => {
-    expect(bracketPush('{[]}')).toEqual(true);
+    expect(matchingBrackets('{[]}')).toEqual(true);
   });
 
   xtest('rejects brackets that are properly balanced but improperly nested', () => {
-    expect(bracketPush('{[}]')).toEqual(false);
+    expect(matchingBrackets('{[}]')).toEqual(false);
   });
 
   xtest('checks bracket closure with deeper nesting', () => {
-    expect(bracketPush('{[)][]}')).toEqual(false);
+    expect(matchingBrackets('{[)][]}')).toEqual(false);
   });
 
   xtest('checks bracket closure in a long string of brackets', () => {
-    expect(bracketPush('{[]([()])}')).toEqual(true);
+    expect(matchingBrackets('{[]([()])}')).toEqual(true);
   });
 });
