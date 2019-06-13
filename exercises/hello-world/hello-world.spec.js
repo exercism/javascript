609b0f164a9f52530e19207a6c3af7e63220d2e3
Tejas Bubane <tejasbubane@gmail.com>
2019-06-13 23:00:27 +0530

    Sync `hello-world` exercise with canonical data v1.1.0

--- exercises/hello-world/hello-world.spec.js
+++ exercises/hello-world/hello-world.spec.js
@@ -1,7 +1,7 @@
 import { hello } from './hello-world';
 
 describe('Hello World', () => {
-  test('says hello', () => {
+  test('Say Hi!', () => {
     expect(hello()).toEqual('Hello, World!');
   });
 });
