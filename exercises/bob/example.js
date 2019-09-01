18ecb7441bf9e38e46a98a599d6b95aaf57b9260
Brendan-C <30474072+brendan-c@users.noreply.github.com>
2019-09-01 10:03:00 -0400

    Update bob/example.js (#719)
    Implemented `String.prototype.endsWith()` for brevity

--- exercises/bob/example.js
+++ exercises/bob/example.js
@@ -1,6 +1,6 @@
 const isSilence = message => message.replace(/\s+/g, '') === '';
 const isShouting = message => message.toUpperCase() === message && /[A-Z]/.test(message);
-const isAQuestion = message => message[message.length - 1] === '?';
+const isAQuestion = message => message.endsWith('?');
 
 export const hey = (message) => {
   if (isSilence(message)) {
