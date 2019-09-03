041ac555bb7c07338e751a48e72e4138e8197ea6
Dagmar Timmreck <DagmarTimmreck@users.noreply.github.com>
2019-09-03 12:31:20 +0200

    Matrix: sync with problem-specifications (#720)
    * Add testcase
    
    * Matrix: Sync with problem-decriptions

--- exercises/matrix/matrix.spec.js
+++ exercises/matrix/matrix.spec.js
@@ -13,8 +13,8 @@ describe('Matrix', () => {
     expect(new Matrix('1 2\n10 20').rows[1]).toEqual([10, 20]);
   });
 
-  xtest('can extract row from non-square matrix', () => {
-    expect(new Matrix('1 2 3\n4 5 6\n7 8 9\n8 7 6').rows[2]).toEqual([7, 8, 9]);
+  xtest('can extract row from non-square matrix with no corresponding column', () => {
+    expect(new Matrix('1 2 3\n4 5 6\n7 8 9\n8 7 6').rows[3]).toEqual([8, 7, 6]);
   });
 
   xtest('extract column from one number matrix', () => {
@@ -25,8 +25,8 @@ describe('Matrix', () => {
     expect(new Matrix('1 2 3\n4 5 6\n7 8 9').columns[2]).toEqual([3, 6, 9]);
   });
 
-  xtest('can extract column from non-square matrix', () => {
-    expect(new Matrix('1 2 3\n4 5 6\n7 8 9\n8 7 6').columns[2]).toEqual([3, 6, 9, 6]);
+  xtest('can extract column from non-square matrix with no corresponding row', () => {
+    expect(new Matrix('1 2 3 4\n5 6 7 8\n9 8 7 6').columns[3]).toEqual([4, 8, 6]);
   });
 
   xtest('can extract column from non-square matrix with more columns than rows', () => {
