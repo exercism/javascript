7efc58d72b0ff50192b3799f51d8a8f8255137e6
Pedro Romano <pedro@paparomeo.net>
2019-09-15 22:45:42 +0100

    Grains exercise using standard `BigInt` type.

--- exercises/grains/example.js
+++ exercises/grains/example.js
@@ -1,32 +1,28 @@
-import BigInt from './lib/big-integer';
-
 /**
  * Gets the number of grains on the nth square
  *
  * @param {Number} num Number to compute its square
  *
- * @return {String} Square of num
+ * @return {BigInt} Square of num
  */
 export const square = num => {
   if (num < 1 || num > 64) {
     throw new Error('square must be between 1 and 64');
   }
-  return BigInt(2)
-    .pow(num - 1)
-    .toString();
+  return BigInt(2) ** BigInt(num - 1);
 };
 
 /**
  * Gets the total number of grains on all squares
  *
- * @return {String} Sum of all squares
+ * @return {BigInt} Sum of all squares
  */
 export const total = () => {
   let total = BigInt(0);
 
   for (let squareNum = 1; squareNum <= 64; squareNum++) {
-    total = total.add(square(squareNum));
+    total += square(squareNum);
   }
 
-  return total.toString();
+  return total;
 };
--- exercises/grains/grains.spec.js
+++ exercises/grains/grains.spec.js
@@ -1,30 +1,30 @@
 /**
- * In JavaScript, integers beyond +/- 9007199254740991 cannot be accurately
- * represented. To see this in action, console.log() out the expected number
- * of grains on square #64:
+ * In JavaScript, integers beyond +/- 9007199254740991 (available as the
+ * Number.MIN_SAFE_INTEGER and NUMBER.MAX_SAFE_INTEGER constants) cannot be
+ * accurately represented by the default "Number" type. To see this in action,
+ * console.log() out the expected number of grains on square #64:
  *
  * console.log(9223372036854775808);
  * // =>       9223372036854776000
  * //                         ^^^^
  *
- * This is because, in JavaScript, integers are represented as 64-bit floating
- * point numbers. If you want to learn more, see:
+ * This is because, in JavaScript, "Number" type integers are actually
+ * represented internally as 64-bit floating point numbers. If you want to learn
+ * more, see:
  *
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/MAX_SAFE_INTEGER
  * http://stackoverflow.com/questions/307179/what-is-javascripts-highest-integer-value-that-a-number-can-go-to-without-losin
  *
- * So, an accurate solution to this problem requires the use of a
- * "big integer" type. There are multiple ways to use big integer types.
- * We have provided you with BigInteger.js. You can read more about it here:
+ * Historically, this limitation could only be overcome by use third-party
+ * implementations of a "bit integer" type like "BigInteger.js"
+ * (https://github.com/peterolson/BigInteger.js).
  *
- * https://github.com/peterolson/BigInteger.js
- * ^--- The "Methods" section of the README will be especially helpful.
+ * However, a new JavaScript arbitrary precision integer type named "BigInt",
+ * which you should use to solve this exercise, is in the late stages of
+ * ECMAScript standardisation and already available in most major browsers and
+ * Node.js >= 10.4.0. For more details see:
  *
- * https://github.com/peterolson/BigInteger.js/blob/master/spec/spec.js
- * ^--- Tests are a good way to understand, in addition to the README.
- *
- * To get you started, this folder has a file of the big-integer module.
- * See its tests in this folder for a quick primer on how to use it! ( :
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt
  */
 
 import { square, total } from './grains';
@@ -32,31 +32,31 @@ import { square, total } from './grains';
 describe('Grains', () => {
   describe('Returns The Number Of Grains On The Square', () => {
     test('1', () => {
-      expect(square(1)).toEqual('1');
+      expect(square(1).toString()).toEqual('1');
     });
 
     xtest('2', () => {
-      expect(square(2)).toEqual('2');
+      expect(square(2).toString()).toEqual('2');
     });
 
     xtest('3', () => {
-      expect(square(3)).toEqual('4');
+      expect(square(3).toString()).toEqual('4');
     });
 
     xtest('4', () => {
-      expect(square(4)).toEqual('8');
+      expect(square(4).toString()).toEqual('8');
     });
 
     xtest('16', () => {
-      expect(square(16)).toEqual('32768');
+      expect(square(16).toString()).toEqual('32768');
     });
 
     xtest('32', () => {
-      expect(square(32)).toEqual('2147483648');
+      expect(square(32).toString()).toEqual('2147483648');
     });
 
     xtest('64', () => {
-      expect(square(64)).toEqual('9223372036854775808');
+      expect(square(64).toString()).toEqual('9223372036854775808');
     });
 
     xtest('square 0 raises an exception', () => {
@@ -79,6 +79,6 @@ describe('Grains', () => {
   });
 
   xtest('returns the total number of grains on the board', () => {
-    expect(total()).toEqual('18446744073709551615');
+    expect(total().toString()).toEqual('18446744073709551615');
   });
 });
