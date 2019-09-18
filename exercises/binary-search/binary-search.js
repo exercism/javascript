d4cc5f5aed4dbaf5bfd80d79ad7c29f226a39ccb
Tejas Bubane <tejasbubane@gmail.com>
2019-09-18 19:28:40 +0530

    Add stubs for few exercises (#724)
    * alphametics
    * atbash-cipher
    * binary-search
    * bowling

---
+++ exercises/binary-search/binary-search.js
@@ -0,0 +1,18 @@
+//
+// This is only a SKELETON file for the 'Binary Search' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class BinarySearch {
+  constructor() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  get array() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  indexOf() {
+    throw new Error("Remove this statement and implement this function");
+  }
+}
