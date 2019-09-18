cc2e97163b6f73048ad28ec8c8871e17c27767ee
Tejas Bubane <tejasbubane@gmail.com>
2019-09-18 11:22:04 +0530

    Add stubs for few exercises
    * alphametics
    * atbash-cipher
    * binary-search
    * bowling

---
+++ exercises/bowling/bowling.js
@@ -0,0 +1,14 @@
+//
+// This is only a SKELETON file for the 'Bowling' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class Bowling {
+  roll() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  score() {
+    throw new Error("Remove this statement and implement this function");
+  }
+}
