46b8e1a70330b3e3464800dc7106b246a608fd1f
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2019-10-01 11:05:54 +0200

    Add new exercise: resistor-color-trio (#725)
    * Add resistor-color-trio exercise
    
    * Add resistor-color-trio as side exercise
    
    * Clean up solution
    
    * Fix error throwing test
    
    * Unlock the resistor-color-trio by bob

---
+++ exercises/resistor-color-trio/example.js
@@ -0,0 +1,62 @@
+const COLORS = [
+  'black', 'brown', 'red', 'orange', 'yellow', 'green',
+  'blue', 'violet', 'grey', 'white',
+]
+
+const ONE_KILOOHM = 1000
+
+class ArgumentError extends Error {}
+
+export class ResistorColorTrio {
+  constructor([tens, ones, zeros]) {
+    this.tens = tens
+    this.ones = ones
+    this.zeros = zeros
+  }
+
+  get value() {
+    if (!this.isValid) {
+      throw new ArgumentError('invalid color')
+    }
+
+    return this.significants() * this.multiplier()
+  }
+
+  get label() {
+    return `Resistor value: ${this}`
+  }
+
+  get isValid() {
+    return COLORS.indexOf(this.tens) > -1
+      && COLORS.indexOf(this.ones) > -1
+      && COLORS.indexOf(this.zeros) > -1
+  }
+
+  toString() {
+    const value = this.value
+    return value < ONE_KILOOHM
+      ? `${value} ohms`
+      : `${Math.floor(value / ONE_KILOOHM)} kiloohms`
+  }
+
+  /**
+   * @private
+   */
+  significants() {
+    return this.colorCode(this.tens) * 10 + this.colorCode(this.ones)
+  }
+
+  /**
+   * @private
+   */
+  multiplier() {
+    return Math.pow(10, this.colorCode(this.zeros))
+  }
+
+  /**
+   * @private
+   */
+  colorCode(color) {
+    return COLORS.indexOf(color)
+  }
+}
---
+++ exercises/resistor-color-trio/resistor-color-trio.spec.js
@@ -0,0 +1,38 @@
+import { ResistorColorTrio } from './resistor-color-trio.js';
+
+function makeLabel({ value, unit }) {
+  return `Resistor value: ${value} ${unit}`
+}
+
+describe('Resistor Color Trio', () => {
+  test('Orange and orange and black', () => {
+    expect(new ResistorColorTrio(["orange", "orange", "black"]).label)
+      .toEqual(makeLabel({ value: 33, unit: "ohms" }));
+  });
+
+  xtest('Blue and grey and brown', () => {
+    expect(new ResistorColorTrio(["blue", "grey", "brown"]).label)
+      .toEqual(makeLabel({ value: 680, unit: "ohms" }));
+  });
+
+  xtest('Red and black and red', () => {
+    expect(new ResistorColorTrio(["red", "black", "red"]).label)
+      .toEqual(makeLabel({ value: 2, unit: "kiloohms" }));
+  });
+
+  xtest('Green and brown and orange', () => {
+    expect(new ResistorColorTrio(["green", "brown", "orange"]).label)
+      .toEqual(makeLabel({ value: 51, unit: "kiloohms" }));
+  });
+
+  xtest('Yellow and violet and yellow', () => {
+    expect(new ResistorColorTrio(["yellow", "violet", "yellow"]).label)
+      .toEqual(makeLabel({ value: 470, unit: "kiloohms" }));
+  });
+
+  // optional: error
+  xtest('Invalid color', () => {
+    expect(() => new ResistorColorTrio(["yellow", "purple", "black"]).label)
+      .toThrowError(/invalid color/);
+  });
+});
