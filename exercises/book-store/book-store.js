c17475abbd093608d8842896f8c638eb03429837
Mireille Campourcy <11388201+mcampourcy@users.noreply.github.com>
2019-10-04 15:14:27 +0200

    :recycle: Add class implementation

---
+++ exercises/book-store/book-store.js
@@ -0,0 +1,10 @@
+//
+// This is only a SKELETON file for the 'Beer Song' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class BookStore {
+  static total() {
+    throw new Error("Remove this statement and implement this function");
+  }
+}
