397aa5746abaf5db52606f941e941d44ea0ef843
PakkuDon <pakku.pd@gmail.com>
2019-10-07 00:17:37 +1100

    Sync Pangram with 2.0.0

--- exercises/pangram/pangram.spec.js
+++ exercises/pangram/pangram.spec.js
@@ -5,27 +5,27 @@ describe('Pangram()', () => {
     expect(isPangram('')).toBe(false);
   });
 
-  xtest('recognizes a perfect lower case pangram', () => {
+  xtest('perfect lower case', () => {
     expect(isPangram('abcdefghijklmnopqrstuvwxyz')).toBe(true);
   });
 
-  xtest('pangram with only lower case', () => {
+  xtest('only lower case', () => {
     expect(isPangram('the quick brown fox jumps over the lazy dog')).toBe(true);
   });
 
-  xtest("missing character 'x'", () => {
+  xtest("missing the letter 'x'", () => {
     expect(isPangram('a quick movement of the enemy will jeopardize five gunboats')).toBe(false);
   });
 
-  xtest("another missing character, e.g. 'h'", () => {
+  xtest("missing the letter 'h'", () => {
     expect(isPangram('five boxing wizards jump quickly at it')).toBe(false);
   });
 
-  xtest('pangram with underscores', () => {
+  xtest('with underscores', () => {
     expect(isPangram('the_quick_brown_fox_jumps_over_the_lazy_dog')).toBe(true);
   });
 
-  xtest('pangram with numbers', () => {
+  xtest('with numbers', () => {
     expect(isPangram('the 1 quick brown fox jumps over the 2 lazy dogs')).toBe(true);
   });
 
@@ -33,11 +33,11 @@ describe('Pangram()', () => {
     expect(isPangram('7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog')).toBe(false);
   });
 
-  xtest('pangram with mixed case and punctuation', () => {
+  xtest('mixed case and punctuation', () => {
     expect(isPangram('"Five quacking Zephyrs jolt my wax bed."')).toBe(true);
   });
 
-  xtest('upper and lower case versions of the same character should not be counted separately', () => {
+  xtest('case insensitive', () => {
     expect(isPangram('the quick brown fox jumps over with lazy FX')).toBe(false);
   });
 });
