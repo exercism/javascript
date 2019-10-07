df352fa9628c1ea5f83f33e521fd4232a685fb25
victor <victorwdr1@gmail.com>
2019-10-07 16:02:25 -0300

    Add stubs for some exercises #735

---
+++ exercises/wordy/wordy.js
@@ -0,0 +1,24 @@
+//
+// This is only a SKELETON file for the 'Wordy' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+class ArgumentError extends Error {
+  constructor() {
+    throw new Error("Remove this statement and implement this function");
+  }
+}
+
+const re = new RegExp();
+
+class Wordy {
+  constructor() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  answer() {
+    throw new Error("Remove this statement and implement this function");
+  }
+}
+
+export { Wordy as WordProblem, ArgumentError };
