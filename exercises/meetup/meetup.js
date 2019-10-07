4a0f58d67f27fda8d433f934aaeef386f3f1033e
Rob Suttles <Robert.suttles58@gmail.com>
2019-10-07 00:15:28 -0500

    Add stub for meetup exercise (#748)
    Issue #705

---
+++ exercises/meetup/meetup.js
@@ -0,0 +1,8 @@
+//
+// This is only a SKELETON file for the 'Meetup' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const meetupDay = (day) =>  {
+  throw new Error("Remove this statement and implement this function");
+}
