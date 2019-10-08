952c01149428f1f1d8a82f65a37b8c02c5c95e42
niharika_dentalkart <niharika@dentalkart.com>
2019-10-09 04:52:07 +0530

    Fixes #705. Added stub file for Change

---
+++ exercises/change/change.js
@@ -0,0 +1,14 @@
+//
+// This is only a SKELETON file for the 'Allergies' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+export class Candidate {
+  constructor() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  addCoin() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+}
