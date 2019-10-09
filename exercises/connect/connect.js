f329d42e67b4c2dcdab1d48c6fbd38cbd2e4d04c
Diego Cáceres <dado90caceres@gmail.com>
2019-10-09 19:28:53 -0300

    Add constructor in Connect stub

--- exercises/connect/connect.js
+++ exercises/connect/connect.js
@@ -4,6 +4,10 @@
 //
 
 export class Board {
+  constructor() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
   winner() {
     throw new Error("Remove this statement and implement this function");
   }
