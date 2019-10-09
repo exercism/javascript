4537c41cb4da4c01125cd899e07733481fe5abbc
Maruthi <maruthi@DV-00851.fios-router.home>
2019-10-08 22:00:46 -0400

    Added stubs for some exercises

---
+++ exercises/simple-linked-list/simple-linked-list.js
@@ -0,0 +1,37 @@
+//
+// This is only a SKELETON file for the 'Simple Linked List' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class SimpleLinkedList {
+
+  constructor() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  add(nextValue) {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  remove(removeValue) {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  length() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  fromArray(numberArray) {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  toArray() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  reverse() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+}
+
---
+++ exercises/word-search/word-search.js
@@ -0,0 +1,17 @@
+//
+// This is only a SKELETON file for the 'Word Search' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class WordSearch {
+
+  constructor(ListOfStrings) {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  search(ListOfSearchTerms) {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+}
+
