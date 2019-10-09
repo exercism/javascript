b3b6f474e1429fd79033fabfc43af25e4e87869e
mluisamc <mluisamartinezc@gmail.com>
2019-10-09 09:24:20 +0200

    stub for two-fer exercise

--- exercises/two-fer/two-fer.js
+++ exercises/two-fer/two-fer.js
@@ -1,10 +1,10 @@
 //
-// This is only a SKELETON file for the 'Word Search' exercise. It's been provided as a
+// This is only a SKELETON file for the 'Two fer' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
 
-export class Words {
-  search() {
+export class TwoFer {
+  return() {
     throw new Error("Remove this statement and implement this function");
   }
 }
