94a98a778e45da6043b3fc0cd02063e5d77ff931
mluisamc <mluisamartinezc@gmail.com>
2019-10-09 14:24:09 +0200

    camelCase

--- exercises/two-fer/two-fer.js
+++ exercises/two-fer/two-fer.js
@@ -3,6 +3,6 @@
 // convenience to get you started writing code faster.
 //
 
-export const twofer = () => {
+export const twoFer = () => {
   throw new Error("Remove this statement and implement this function");
 };
