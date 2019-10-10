576d56037a9691216d362f016835f0d4024a4344
Kristin Faner <18252124+seeksort@users.noreply.github.com>
2019-10-09 21:17:19 -0500

    add scale-generator exercise (#754)
    * add scale-generator exercise

---
+++ exercises/scale-generator/scale-generator.js
@@ -0,0 +1,18 @@
+//
+// This is only a SKELETON file for the 'Scale Generator' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class Scale {
+  constructor(tonic) {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  chromatic() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  interval(intervals) {
+    throw new Error("Remove this statement and implement this function");
+  }
+}
