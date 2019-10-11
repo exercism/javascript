62fa577abd66ede5b45e39eaccaf1118dfeb754e
Victor Wagner <victorwdr@gmail.com>
2019-10-11 18:49:19 -0300

    remove functions that should not be stubed

--- exercises/word-search/word-search.js
+++ exercises/word-search/word-search.js
@@ -3,42 +3,6 @@
 // convenience to get you started writing code faster.
 //
 
-function searchHorizontally() {
-  throw new Error("Remove this statement and implement this function");
-}
-
-function flipCoordinates() {
-  throw new Error("Remove this statement and implement this function");
-}
-
-function flipGrid() {
-  throw new Error("Remove this statement and implement this function");
-}
-
-function diagonalFind() {
-  throw new Error("Remove this statement and implement this function");
-}
-
-function findAWordDiagonallyTopDown() {
-  throw new Error("Remove this statement and implement this function");
-}
-
-function findAWordDiagonallyBottomUp() {
-  throw new Error("Remove this statement and implement this function");
-}
-
-function formatCoordinates() {
-  throw new Error("Remove this statement and implement this function");
-}
-
-function searchDiagonally() {
-  throw new Error("Remove this statement and implement this function");
-}
-
-function findWordInAnyDirection() {
-  throw new Error("Remove this statement and implement this function");
-}
-
 class WordSearch {
   constructor() {
     throw new Error("Remove this statement and implement this function");
