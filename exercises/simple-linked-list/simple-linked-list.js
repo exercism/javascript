7cbcef9da9f6b60a5c2f4aed57f367994e2d35f4
Maruthi Mohan <19310076+MaruthiMohanReddyPutha@users.noreply.github.com>
2019-10-16 13:53:55 -0400

    Add stub for simple-linked-list exercise (#756)

---
+++ exercises/simple-linked-list/simple-linked-list.js
@@ -0,0 +1,33 @@
+//
+// This is only a SKELETON file for the 'Simple Linked List' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class SimpleLinkedList {
+
+  constructor() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  add(nextValue) {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  get length() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  get head() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  toArray() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  reverse() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+}
+
