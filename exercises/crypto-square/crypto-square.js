5db5bbdfb119c07e0392aa0098ca7ea77124d6fb
Diego Cáceres <diego-caceres@users.noreply.github.com>
2019-10-16 06:10:36 -0300

    Add stub for the 'crypto-square' exercise (#760)

---
+++ exercises/crypto-square/crypto-square.js
@@ -0,0 +1,26 @@
+//
+// This is only a SKELETON file for the 'Crypto Square' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class Crypto {
+  constructor() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  normalizePlaintext() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  size() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  plaintextSegments() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  ciphertext() {
+    throw new Error("Remove this statement and implement this function");
+  }
+}
