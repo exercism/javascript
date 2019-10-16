d38b0e8abb41a13985713b76ee965830323fcbc1
Andrey Botsiun <35426296+Boto1@users.noreply.github.com>
2019-10-16 14:28:45 +0300

    add stub file for 'react' excercise (#763)

---
+++ exercises/react/react.js
@@ -0,0 +1,46 @@
+//
+// This is only a SKELETON file for the 'React' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class InputCell {
+  constructor(value) {
+    throw new Error(
+      'Remove this statement and implement this function'
+    );
+  }
+
+  setValue(value) {
+    throw new Error(
+      'Remove this statement and implement this function'
+    );
+  }
+}
+
+export class ComputeCell {
+  constructor(inputCells, fn) {
+    throw new Error(
+      'Remove this statement and implement this function'
+    );
+  }
+
+  addCallback(cb) {
+    throw new Error(
+      'Remove this statement and implement this function'
+    );
+  }
+
+  removeCallback(cb) {
+    throw new Error(
+      'Remove this statement and implement this function'
+    );
+  }
+}
+
+export class CallbackCell {
+  constructor(fn) {
+    throw new Error(
+      'Remove this statement and implement this function'
+    );
+  }
+}
