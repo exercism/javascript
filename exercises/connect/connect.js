0effca8f1d66a75e220e6170bdb97b52703e5aec
Diego Cáceres <diego-caceres@users.noreply.github.com>
2019-10-17 02:41:25 -0300

    Add stub for 'connect' exercise (#759)

---
+++ exercises/connect/connect.js
@@ -0,0 +1,14 @@
+//
+// This is only a SKELETON file for the 'Connect' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class Board {
+  constructor() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  winner() {
+    throw new Error("Remove this statement and implement this function");
+  }
+}
