29e799f51a236e551c81925b3556c03b1b4c2397
Diego Cáceres <diego-caceres@users.noreply.github.com>
2019-10-18 02:19:45 -0300

    Add stub for the 'circular-buffer' exercise (#758)

---
+++ exercises/circular-buffer/circular-buffer.js
@@ -0,0 +1,40 @@
+//
+// This is only a SKELETON file for the 'Circular Buffer' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+class CircularBuffer {
+  constructor() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  write() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  read() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  forceWrite() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  clear() {
+    throw new Error("Remove this statement and implement this function");
+  }
+}
+
+export default CircularBuffer;
+
+export class BufferFullError extends Error {
+  constructor() {
+    throw new Error("Remove this statement and implement this function");
+  }
+}
+
+export class BufferEmptyError extends Error {
+  constructor() {
+    throw new Error("Remove this statement and implement this function");
+  }
+}
