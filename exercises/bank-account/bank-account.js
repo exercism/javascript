a6a35db4b4d87bce53899e36075fc27bdf12a7d4
Brendan Mckeown <brendan.mckeown@thinkbrownstone.com>
2019-10-18 12:52:39 -0400

    feat: add bank account exercise

---
+++ exercises/bank-account/bank-account.js
@@ -0,0 +1,8 @@
+//
+// This is only a SKELETON file for the 'Bank Account' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class BankAccount {}
+
+export class ValueError extends Error {}
