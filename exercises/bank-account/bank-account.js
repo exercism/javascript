55b619469b0397c2ed0c1e561621fd4b749efbb1
Brendan Mckeown <brendan.mckeown@thinkbrownstone.com>
2019-10-18 16:25:37 -0400

    Add stubbed functions to stub file for bank account exercise

--- exercises/bank-account/bank-account.js
+++ exercises/bank-account/bank-account.js
@@ -3,6 +3,32 @@
 // convenience to get you started writing code faster.
 //
 
-export class BankAccount {}
+export class BankAccount {
+
+  constructor() {
+    throw new Error('Remove this statement and implement this function');
+  }
+
+  open() {
+    throw new Error('Remove this statement and implement this function');
+  }
+
+  close() {
+    throw new Error('Remove this statement and implement this function');
+  }
+
+  deposit() {
+    throw new Error('Remove this statement and implement this function');
+  }
+
+  withdraw() {
+    throw new Error('Remove this statement and implement this function');
+  }
+
+  get balance() {
+    throw new Error('Remove this statement and implement this function');
+  }
+
+}
 
 export class ValueError extends Error {}
