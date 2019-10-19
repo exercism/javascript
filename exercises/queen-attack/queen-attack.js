9f04f73e5d929fcd4e2a4517357d792abd428c94
abhinav <abhinav.gupta@inventum.net>
2019-10-19 11:12:16 +0530

    Added stub for "Queen Attack" exercise

---
+++ exercises/queen-attack/queen-attack.js
@@ -0,0 +1,20 @@
+//
+// This is only a SKELETON file for the 'Queen Attack' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+class QueenAttack {
+    constructor(params = { white: [], black: []}) {
+        this.white = params.white;
+        this.black = params.black;
+        throw new Error("Remove this statement and implement this function");
+    }
+
+    toString() {
+        throw new Error("Remove this statement and implement this function");
+    }
+
+    canAttack() {
+        throw new Error("Remove this statement and implement this function");
+    }
+}
