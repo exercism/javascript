44d85e926c873c6e20aefe6e5e9b972e9523185f
TomPradat <tom.pradat@gmail.com>
2019-10-20 18:38:46 +0200

    Add stub for the 'change' exercise (#735)

---
+++ exercises/change/change.js
@@ -0,0 +1,10 @@
+//
+// This is only a SKELETON file for the 'Change' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class Change {
+  calculate(coinArray, target) {
+    throw new Error("Remove this statement and implement this function");
+  }
+}
