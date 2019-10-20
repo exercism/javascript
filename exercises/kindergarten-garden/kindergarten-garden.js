da942e22abdc27315182ae35937ee89e338b48b1
Brendan McKeown <brenmckeown@gmail.com>
2019-10-20 12:03:45 -0400

    Add stub file for "kindergarten garden" exercise (#770)

---
+++ exercises/kindergarten-garden/kindergarten-garden.js
@@ -0,0 +1,12 @@
+//
+// This is only a SKELETON file for the 'Kindergarten Garden' exercise.
+// It's been provided as a convenience to get you started writing code faster.
+//
+
+export class Garden {
+
+  constructor() {
+    throw new Error('Remove this statement and implement this function');
+  }
+
+}
