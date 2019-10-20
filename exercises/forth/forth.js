b7cb01e08f08a316c17ed308ce29370296f38fcc
Brendan McKeown <brenmckeown@gmail.com>
2019-10-20 11:56:46 -0400

    Add stub file for "Forth" exercise (#769)

---
+++ exercises/forth/forth.js
@@ -0,0 +1,20 @@
+//
+// This is only a SKELETON file for the 'Forth' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class Forth {
+
+  constructor() {
+    throw Error('Remove this statement and implement this function');
+  }
+
+  evaluate() {
+    throw Error('Remove this statement and implement this function');
+  }
+
+  get stack() {
+    throw Error('Remove this statement and implement this function');
+  }
+
+}
