f972a71c7144bb5be7a665b3ae69173e0f2c4952
Abhinav Gupta <abhnvashgpta@gmail.com>
2019-10-21 11:42:29 +0530

    Add stub for "Queen Attack" exercise (#772)

---
+++ exercises/queen-attack/queen-attack.js
@@ -0,0 +1,18 @@
+//
+// This is only a SKELETON file for the 'Queen Attack' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class QueenAttack {
+    constructor() {
+        throw new Error("Remove this statement and implement this function");
+    }
+
+    toString() {
+        throw new Error("Remove this statement and implement this function");
+    }
+
+    canAttack() {
+        throw new Error("Remove this statement and implement this function");
+    }
+}
