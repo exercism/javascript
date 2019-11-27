6c1a896ad2bfe46f7d1fa38b912319a682042839
Ovidiu Miu <ovidiu141@users.noreply.github.com>
2019-11-27 20:00:13 +0200

    Sync 'Clock' tests to v2.4.0 (#788)

--- exercises/clock/clock.spec.js
+++ exercises/clock/clock.spec.js
@@ -70,6 +70,10 @@ describe('Clock', () => {
       expect(new Clock(1, -4820).toString()).toEqual('16:40');
     });
 
+    xtest('negative sixty minutes is previous hour', () => {
+      expect(new Clock(2, -60).toString()).toEqual('01:00');
+    });
+
     xtest('negative hour and minutes both roll over', () => {
       expect(new Clock(-25, -160).toString()).toEqual('20:20');
     });
@@ -77,133 +81,139 @@ describe('Clock', () => {
     xtest('negative hour and minutes both roll over continuously', () => {
       expect(new Clock(-121, -5810).toString()).toEqual('22:10');
     });
+  });
 
-    describe('Adding and subtracting minutes', () => {
-      xtest('add minutes', () => {
-        expect(new Clock(10, 0).plus(3).toString()).toEqual('10:03');
-      });
-
-      xtest('add no minutes', () => {
-        expect(new Clock(6, 41).plus(0).toString()).toEqual('06:41');
-      });
-
-      xtest('add to next hour', () => {
-        expect(new Clock(0, 45).plus(40).toString()).toEqual('01:25');
-      });
-
-      xtest('add more than one hour', () => {
-        expect(new Clock(10, 0).plus(61).toString()).toEqual('11:01');
-      });
-
-      xtest('add more than two hours with carry', () => {
-        expect(new Clock(0, 45).plus(160).toString()).toEqual('03:25');
-      });
-
-      xtest('add across midnight', () => {
-        expect(new Clock(23, 59).plus(2).toString()).toEqual('00:01');
-      });
-
-      xtest('add more than one day (1500 min = 25 hrs)', () => {
-        expect(new Clock(5, 32).plus(1500).toString()).toEqual('06:32');
-      });
-
-      xtest('add more than two days', () => {
-        expect(new Clock(1, 1).plus(3500).toString()).toEqual('11:21');
-      });
-
-      xtest('subtract minutes', () => {
-        expect(new Clock(10, 3).minus(3).toString()).toEqual('10:00');
-      });
-
-      xtest('subtract to previous hour', () => {
-        expect(new Clock(10, 3).minus(30).toString()).toEqual('09:33');
-      });
-
-      xtest('subtract more than an hour', () => {
-        expect(new Clock(10, 3).minus(70).toString()).toEqual('08:53');
-      });
-
-      xtest('subtract across midnight', () => {
-        expect(new Clock(0, 3).minus(4).toString()).toEqual('23:59');
-      });
-
-      xtest('subtract more than two hours', () => {
-        expect(new Clock(0, 0).minus(160).toString()).toEqual('21:20');
-      });
-
-      xtest('subtract more than two hours with borrow', () => {
-        expect(new Clock(6, 15).minus(160).toString()).toEqual('03:35');
-      });
-
-      xtest('subtract more than one day (1500 min = 25 hrs)', () => {
-        expect(new Clock(5, 32).minus(1500).toString()).toEqual('04:32');
-      });
-
-      xtest('subtract more than two days', () => {
-        expect(new Clock(2, 20).minus(3000).toString()).toEqual('00:20');
-      });
+  describe('Adding minutes', () => {
+    xtest('add minutes', () => {
+      expect(new Clock(10, 0).plus(3).toString()).toEqual('10:03');
     });
 
-    describe('Construct two separate clocks, set times, test if they are equal', () => {
-      xtest('clocks with same time', () => {
-        expect(new Clock(15, 37).equals(new Clock(15, 37))).toBeTruthy();
-      });
+    xtest('add no minutes', () => {
+      expect(new Clock(6, 41).plus(0).toString()).toEqual('06:41');
+    });
 
-      xtest('clocks a minute apart', () => {
-        expect(new Clock(15, 36).equals(new Clock(15, 37))).toBeFalsy();
-      });
+    xtest('add to next hour', () => {
+      expect(new Clock(0, 45).plus(40).toString()).toEqual('01:25');
+    });
 
-      xtest('clocks an hour apart', () => {
-        expect(new Clock(14, 37).equals(new Clock(15, 37))).toBeFalsy();
-      });
+    xtest('add more than one hour', () => {
+      expect(new Clock(10, 0).plus(61).toString()).toEqual('11:01');
+    });
 
-      xtest('clocks with hour overflow', () => {
-        expect(new Clock(10, 37).equals(new Clock(34, 37))).toBeTruthy();
-      });
+    xtest('add more than two hours with carry', () => {
+      expect(new Clock(0, 45).plus(160).toString()).toEqual('03:25');
+    });
 
-      xtest('clocks with hour overflow by several days', () => {
-        expect(new Clock(3, 11).equals(new Clock(99, 11))).toBeTruthy();
-      });
+    xtest('add across midnight', () => {
+      expect(new Clock(23, 59).plus(2).toString()).toEqual('00:01');
+    });
 
-      xtest('clocks with negative hour', () => {
-        expect(new Clock(22, 40).equals(new Clock(-2, 40))).toBeTruthy();
-      });
+    xtest('add more than one day (1500 min = 25 hrs)', () => {
+      expect(new Clock(5, 32).plus(1500).toString()).toEqual('06:32');
+    });
 
-      xtest('clocks with negative hour that wraps', () => {
-        expect(new Clock(17, 3).equals(new Clock(-31, 3))).toBeTruthy();
-      });
+    xtest('add more than two days', () => {
+      expect(new Clock(1, 1).plus(3500).toString()).toEqual('11:21');
+    });
+  });
 
-      xtest('clocks with negative hour that wraps multiple times', () => {
-        expect(new Clock(13, 49).equals(new Clock(-83, 49))).toBeTruthy();
-      });
+  describe('Subtract minutes', () => {
+    xtest('subtract minutes', () => {
+      expect(new Clock(10, 3).minus(3).toString()).toEqual('10:00');
+    });
 
-      xtest('clocks with minute overflow', () => {
-        expect(new Clock(0, 1).equals(new Clock(0, 1441))).toBeTruthy();
-      });
+    xtest('subtract to previous hour', () => {
+      expect(new Clock(10, 3).minus(30).toString()).toEqual('09:33');
+    });
 
-      xtest('clocks with minute overflow by several days', () => {
-        expect(new Clock(2, 2).equals(new Clock(2, 4322))).toBeTruthy();
-      });
+    xtest('subtract more than an hour', () => {
+      expect(new Clock(10, 3).minus(70).toString()).toEqual('08:53');
+    });
 
-      xtest('clocks with negative minute', () => {
-        expect(new Clock(2, 40).equals(new Clock(3, -20))).toBeTruthy();
-      });
+    xtest('subtract across midnight', () => {
+      expect(new Clock(0, 3).minus(4).toString()).toEqual('23:59');
+    });
 
-      xtest('clocks with negative minute that wraps', () => {
-        expect(new Clock(4, 10).equals(new Clock(5, -1490))).toBeTruthy();
-      });
+    xtest('subtract more than two hours', () => {
+      expect(new Clock(0, 0).minus(160).toString()).toEqual('21:20');
+    });
 
-      xtest('clocks with negative minute that wraps multiple times', () => {
-        expect(new Clock(6, 15).equals(new Clock(6, -4305))).toBeTruthy();
-      });
+    xtest('subtract more than two hours with borrow', () => {
+      expect(new Clock(6, 15).minus(160).toString()).toEqual('03:35');
+    });
 
-      xtest('clocks with negative hours and minutes', () => {
-        expect(new Clock(7, 32).equals(new Clock(-12, -268))).toBeTruthy();
-      });
+    xtest('subtract more than one day (1500 min = 25 hrs)', () => {
+      expect(new Clock(5, 32).minus(1500).toString()).toEqual('04:32');
+    });
 
-      xtest('clocks with negative hours and minutes that wrap', () => {
-        expect(new Clock(18, 7).equals(new Clock(-54, -11513))).toBeTruthy();
-      });
+    xtest('subtract more than two days', () => {
+      expect(new Clock(2, 20).minus(3000).toString()).toEqual('00:20');
+    });
+  });
+
+  describe('Compare two clocks for equality', () => {
+    xtest('clocks with same time', () => {
+      expect(new Clock(15, 37).equals(new Clock(15, 37))).toBe(true);
+    });
+
+    xtest('clocks a minute apart', () => {
+      expect(new Clock(15, 36).equals(new Clock(15, 37))).toBe(false);
+    });
+
+    xtest('clocks an hour apart', () => {
+      expect(new Clock(14, 37).equals(new Clock(15, 37))).toBe(false);
+    });
+
+    xtest('clocks with hour overflow', () => {
+      expect(new Clock(10, 37).equals(new Clock(34, 37))).toBe(true);
+    });
+
+    xtest('clocks with hour overflow by several days', () => {
+      expect(new Clock(3, 11).equals(new Clock(99, 11))).toBe(true);
+    });
+
+    xtest('clocks with negative hour', () => {
+      expect(new Clock(22, 40).equals(new Clock(-2, 40))).toBe(true);
+    });
+
+    xtest('clocks with negative hour that wraps', () => {
+      expect(new Clock(17, 3).equals(new Clock(-31, 3))).toBe(true);
+    });
+
+    xtest('clocks with negative hour that wraps multiple times', () => {
+      expect(new Clock(13, 49).equals(new Clock(-83, 49))).toBe(true);
+    });
+
+    xtest('clocks with minute overflow', () => {
+      expect(new Clock(0, 1).equals(new Clock(0, 1441))).toBe(true);
+    });
+
+    xtest('clocks with minute overflow by several days', () => {
+      expect(new Clock(2, 2).equals(new Clock(2, 4322))).toBe(true);
+    });
+
+    xtest('clocks with negative minute', () => {
+      expect(new Clock(2, 40).equals(new Clock(3, -20))).toBe(true);
+    });
+
+    xtest('clocks with negative minute that wraps', () => {
+      expect(new Clock(4, 10).equals(new Clock(5, -1490))).toBe(true);
+    });
+
+    xtest('clocks with negative minute that wraps multiple times', () => {
+      expect(new Clock(6, 15).equals(new Clock(6, -4305))).toBe(true);
+    });
+
+    xtest('clocks with negative hours and minutes', () => {
+      expect(new Clock(7, 32).equals(new Clock(-12, -268))).toBe(true);
+    });
+
+    xtest('clocks with negative hours and minutes that wrap', () => {
+      expect(new Clock(18, 7).equals(new Clock(-54, -11513))).toBe(true);
+    });
+
+    xtest('full clock and zeroed clock', () => {
+      expect(new Clock(24, 0).equals(new Clock(0, 0))).toBe(true);
     });
   });
 });
