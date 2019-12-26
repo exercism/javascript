60905051872e8abc1538c8bd9c1db7a3b3451d48
Ovidiu Miu <ovidiu141@users.noreply.github.com>
2019-12-26 08:06:20 +0200

    Sync Grains tests with canonical data 1.2.0 (#797)

--- exercises/grains/example.js
+++ exercises/grains/example.js
@@ -1,35 +1,32 @@
 import BigInt from './lib/big-integer';
 
 /**
- * Computes the number of grains on the squares of a
+ * Gets the number of grains on the nth square
  *
- * chess board, starting with one grain on the first
- * square, and doubling with each successive square.
+ * @param {Number} num Number to compute its square
+ *
+ * @return {String} Square of num
  */
-export class Grains {
-  /**
-   * Gets the number of grains on the nth square
-   *
-   * @param {Number} num Number to compute its square
-   *
-   * @return {String} Square of num
-   */
-  square(num) {
-    return BigInt(2).pow(num - 1).toString();
+export const square = num => {
+  if (num < 1 || num > 64) {
+    throw new Error('square must be between 1 and 64');
+  }
+  return BigInt(2)
+    .pow(num - 1)
+    .toString();
+};
+
+/**
+ * Gets the total number of grains on all squares
+ *
+ * @return {String} Sum of all squares
+ */
+export const total = () => {
+  let total = BigInt(0);
+
+  for (let squareNum = 1; squareNum <= 64; squareNum++) {
+    total = total.add(square(squareNum));
   }
 
-  /**
-   * Gets the total number of grains on all squares
-   *
-   * @return {String} Sum of all squares
-   */
-  total() {
-    let total = BigInt(0);
-
-    for (let squareNum = 1; squareNum <= 64; squareNum++) {
-      total = total.add(this.square(squareNum));
-    }
-
-    return total.toString();
-  }
-}
+  return total.toString();
+};
