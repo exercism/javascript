625411967477db1c838d7916ec743b441c0921db
Ovidiu Miu <ovidiu141@users.noreply.github.com>
2020-01-14 10:30:16 +0200

    Sync largest-series-product with canonical-data 1.2.0 (#808)

--- exercises/largest-series-product/example.js
+++ exercises/largest-series-product/example.js
@@ -1,39 +1,28 @@
-const getDigits = numberString => [...numberString].map(digit => parseInt(digit, 10));
-
-const slices = (digits, sliceSize) => {
-  const result = [];
-  let slice = [];
-
-  if (sliceSize > digits.length) {
-    throw new Error('Slice size is too big.');
+export const largestProduct = (digits, seriesLength) => {
+  if (seriesLength === 0) {
+    return 1;
+  }
+  if (seriesLength > digits.length) {
+    throw new Error('Span must be smaller than string length');
+  }
+  if (seriesLength < 0) {
+    throw new Error('Span must be greater than zero');
   }
 
-  for (let i = 0; i < digits.length - sliceSize + 1; i += 1) {
-    for (let j = 0; j < sliceSize; j += 1) {
-      slice.push(digits[i + j]);
+  if (!/^[0-9]+$/g.test(digits)) {
+    throw new Error('Digits input must only contain digits');
+  }
+
+  let largestProduct = 0;
+  for (let i = 0; i <= digits.length - seriesLength; i++) {
+    const product = digits
+      .substring(i, i + seriesLength)
+      .split('')
+      .map(digit => Number(digit))
+      .reduce((a, b) => a * b);
+    if (product > largestProduct) {
+      largestProduct = product;
     }
-    result.push(slice);
-    slice = [];
   }
-
-  return result;
-};
-
-export const largestProduct = (numberString, size) => {
-  if (numberString.match('[^0-9]')) {
-    throw new Error('Invalid input.');
-  }
-  if (size < 0) {
-    throw new Error('Invalid input.');
-  }
-
-  let product;
-  let max = 0;
-  slices(getDigits(numberString), size).forEach((slice) => {
-    product = slice.reduce((a, b) => a * b, 1);
-    if (product > max) {
-      max = product;
-    }
-  });
-  return max;
+  return largestProduct;
 };
