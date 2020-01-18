06c23af46bff1d4202df2e95f94eada6905627cd
Brendan Mckeown <brendan.mckeown@thinkbrownstone.com>
2020-01-18 14:42:49 -0500

    bank-account: add error message to ValueError

--- exercises/bank-account/bank-account.js
+++ exercises/bank-account/bank-account.js
@@ -31,4 +31,10 @@ export class BankAccount {
 
 }
 
-export class ValueError extends Error {}
+export class ValueError extends Error {
+
+  constructor() {
+    super('Bank account error');
+  }
+
+}
