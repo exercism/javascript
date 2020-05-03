136e0fb1d39737cf083593b4c4ee99703c071312
Noa Karni <shirakarni32@gmail.com>
2020-05-04 01:14:25 +0300

    Fix tests for Anagram

--- exercises/anagram/anagram.js
+++ exercises/anagram/anagram.js
@@ -1,12 +1,3 @@
-// //
-// // This is only a SKELETON file for the 'Anagram' exercise. It's been provided as a
-// // convenience to get you started writing code faster.
-// //
-
-// export const findAnagrams = () => {
-//   throw new Error("Remove this statement and implement this function");
-// }
-
 var alphagram = function(word) {
   return word.split('').sort().join('');
 };
