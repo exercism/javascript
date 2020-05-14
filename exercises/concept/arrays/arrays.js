71f309a0f4d47fb22b9fd2ce20c711e3a34cef10
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2020-05-14 18:10:22 +0200

    Clean-up array-analysis
    - Clean-up exercises
    - Order config.json lexicographically
    - Add exercise to \_sidebar, stories and reference/types.
    - Add exercise to concept README.md

--- exercises/concept/arrays/arrays.js
+++ exercises/concept/arrays/arrays.js
@@ -1,94 +1,96 @@
+// @ts-check
+
 /**
  * Retrieve card from array at position index
- * 
+ *
  * @param {number[]} array
  * @param {number} index
- * 
+ *
  * @returns {number} Value of card retrieved
  */
 export function getItem(array, index) {
-  throw new Error('Implement the getItem function');
+  throw new Error('Implement the getItem function')
 }
 
 /**
- * Exchange card from array with newCard at posisiton index
- * 
+ * Exchange card from array with newCard at position index
+ *
  * @param {number[]} array
  * @param {number} index
  * @param {number} newCard
- * 
+ *
  * @returns {number[]} Array after exchanged card with newCard
  */
 export function setItem(array, index, newCard) {
-  throw new Error('Implement the setItem function');
+  throw new Error('Implement the setItem function')
 }
 
 /**
  * Insert newCard at the end of array
- * 
+ *
  * @param {number[]} array
  * @param {number} newCard
- * 
+ *
  * @returns {number[]} Array after newCard inserted
  */
 export function insertItemAtTop(array, newCard) {
-  throw new Error('Implement the insertItemAtTop function');
+  throw new Error('Implement the insertItemAtTop function')
 }
 
 /**
  * Remove card from array at position index
- * 
+ *
  * @param {number[]} array
  * @param {number} index
- * 
+ *
  * @returns {number[]} Array after card removed
  */
 export function removeItem(array, index) {
-  throw new Error('Implement the removeItem function');
+  throw new Error('Implement the removeItem function')
 }
 
 /**
  * Remove card from the end of array
- * 
+ *
  * @param {number[]} array
- * 
+ *
  * @returns {number[]} Array after card removed
  */
 export function removeItemFromTop(array) {
-  throw new Error('Implement the removeItemFromTop function');
+  throw new Error('Implement the removeItemFromTop function')
 }
 
 /**
  * Insert newCard at beginning of array
- * 
+ *
  * @param {number[]} array
  * @param {number} newCard
- * 
+ *
  * @returns {number[]} Array after newCard inserted
  */
 export function insertItemAtBottom(array, newCard) {
-  throw new Error('Implement the insertItemAtBottom function');
+  throw new Error('Implement the insertItemAtBottom function')
 }
 
 /**
  * Remove card from the beginning of array
- * 
+ *
  * @param {number[]} array
- * 
+ *
  * @returns {number[]} Array after card removed
  */
 export function removeItemAtBottom(array) {
-  throw new Error('Implement the removeItemAtBottom function');
+  throw new Error('Implement the removeItemAtBottom function')
 }
 
 /**
  * Check the size of array, Does it equals to stackSize
- * 
+ *
  * @param {number[]} array
  * @param {number} stackSize
- * 
- * @returns {boolean} Whether the size of array equals to stackSizde
+ *
+ * @returns {boolean} Whether the size of array equals to stackSize
  */
 export function checkSizeOfStack(array, stackSize) {
-  throw new Error('Implement the checkLengthOfStack function');
+  throw new Error('Implement the checkLengthOfStack function')
 }
--- exercises/concept/arrays/arrays.spec.js
+++ exercises/concept/arrays/arrays.spec.js
@@ -1,3 +1,5 @@
+//@ts-check
+
 import {
   getItem,
   setItem,
@@ -6,113 +8,148 @@ import {
   removeItemFromTop,
   insertItemAtBottom,
   removeItemAtBottom,
-  checkSizeOfStack
-} from './arrays';
+  checkSizeOfStack,
+} from './arrays'
+
+/**
+ * @template T the expected return type
+ * @typedef {Array<[number[], number, T]>} TestSingleMatrix
+ */
+
+/**
+ * @template T the expected return type
+ * @typedef {Array<[number[], T]>} TestAllMatrix
+ **/
+
+/**
+ * @template T the expected return type
+ * @typedef {Array<[number[], number, number, T]>} TestSetMatrix
+ **/
 
 describe('arrays', () => {
   describe('getItem', () => {
+    /** @type {TestSingleMatrix<number>} */
     const getItemTestCases = [
       [[1, 2, 3], 0, 1],
       [[1, 2, 3], 1, 2],
-      [[1, 2, 3], 2, 3]
-    ];
+      [[1, 2, 3], 2, 3],
+    ]
 
     getItemTestCases.forEach(([array, item, expected]) => {
       test(`getItem([${array}], ${item})`, () => {
-        expect(getItem(array, item)).toBe(expected);
-      });
-    });
-  });
+        expect(getItem(array, item)).toBe(expected)
+      })
+    })
+  })
 
   describe('setItem', () => {
+    /** @type {TestSetMatrix<Array<number>>} */
     const setItemTestCases = [
       [[1, 2, 3], 0, 7, [7, 2, 3]],
       [[1, 2, 3], 1, 8, [1, 8, 3]],
-      [[1, 2, 3], 2, 9, [1, 2, 9]]
-    ];
+      [[1, 2, 3], 2, 9, [1, 2, 9]],
+    ]
 
     setItemTestCases.forEach(([array, index, newCard, expected]) => {
       test(`setItem([${array}], ${index}, ${newCard})`, () => {
-        expect(setItem(array, index, newCard)).toStrictEqual(expected);
-      });
-    });
-  });
+        expect(setItem(array, index, newCard)).toStrictEqual(expected)
+      })
+    })
+  })
 
   describe('insertItemAtTop', () => {
+    /** @type {TestSingleMatrix<Array<number>>} */
     const insertItemAtTopCases = [
       [[1], 2, [1, 2]],
       [[2, 5], 3, [2, 5, 3]],
       [[3, 4, 9, 7], 8, [3, 4, 9, 7, 8]],
-      [[5, 9, 7, 1], 8, [5, 9, 7, 1, 8]]
-    ];
+      [[5, 9, 7, 1], 8, [5, 9, 7, 1, 8]],
+    ]
 
     insertItemAtTopCases.forEach(([array, newCard, expected]) => {
       test(`insertItemAtTop([${array}], ${newCard})`, () => {
-        expect(insertItemAtTop(array, newCard)).toStrictEqual(expected);
-      });
-    });
-  });
+        expect(insertItemAtTop(array, newCard)).toStrictEqual(expected)
+      })
+    })
+  })
 
   describe('removeItem', () => {
+    /** @type {TestSingleMatrix<Array<number>>} */
     const removeItemTestCases = [
       [[1, 2, 3, 4], 0, [2, 3, 4]],
       [[1, 2, 3, 4], 1, [1, 3, 4]],
       [[1, 2, 3, 4], 2, [1, 2, 4]],
-      [[1, 2, 3, 4], 3, [1, 2, 3]]
-    ];
+      [[1, 2, 3, 4], 3, [1, 2, 3]],
+    ]
 
     removeItemTestCases.forEach(([array, index, expected]) => {
       test(`removeItem([${array}], ${index})`, () => {
-        expect(removeItem(array, index)).toStrictEqual(expected);
-      });
-    });
-  });
+        expect(removeItem(array, index)).toStrictEqual(expected)
+      })
+    })
+  })
 
   describe('removeItemFromTop', () => {
+    /** @type {TestAllMatrix<Array<number>>} */
     const removeItemFromTopTestCases = [
       [[1], []],
       [[1, 2], [1]],
-      [[1, 2, 3], [1, 2]],
-    ];
+      [
+        [1, 2, 3],
+        [1, 2],
+      ],
+    ]
 
     removeItemFromTopTestCases.forEach(([array, expected]) => {
       test(`removeItemFromTop([${array}])`, () => {
-        expect(removeItemFromTop(array)).toStrictEqual(expected);
-      });
-    });
-  });
+        expect(removeItemFromTop(array)).toStrictEqual(expected)
+      })
+    })
+  })
 
   describe('insertItemAtBottom', () => {
+    /** @type {TestSingleMatrix<Array<number>>} */
     const insertItemAtBottomCases = [
       [[1], 2, [2, 1]],
       [[3, 1, 2], 1, [1, 3, 1, 2]],
       [[9, 9, 9, 9], 9, [9, 9, 9, 9, 9]],
-      [[5, 9, 7, 1], 8, [8, 5, 9, 7, 1]]
-    ];
+      [[5, 9, 7, 1], 8, [8, 5, 9, 7, 1]],
+    ]
 
     insertItemAtBottomCases.forEach(([array, newCard, expected]) => {
       test(`insertItemAtBottom([${array}], ${newCard})`, () => {
-        expect(insertItemAtBottom(array, newCard)).toStrictEqual(expected);
-      });
-    });
-  });
+        expect(insertItemAtBottom(array, newCard)).toStrictEqual(expected)
+      })
+    })
+  })
 
   describe('removeItemAtBottom', () => {
+    /** @type {TestAllMatrix<Array<number>>} */
     const removeItemAtBottomCases = [
       [[], []],
-      [[3, 1, 2], [1, 2]],
-      [[8, 8, 8, 8], [8, 8, 8]],
-      [[8, 5, 9, 7, 1], [5, 9, 7, 1]]
-    ];
+      [
+        [3, 1, 2],
+        [1, 2],
+      ],
+      [
+        [8, 8, 8, 8],
+        [8, 8, 8],
+      ],
+      [
+        [8, 5, 9, 7, 1],
+        [5, 9, 7, 1],
+      ],
+    ]
 
     removeItemAtBottomCases.forEach(([array, expected]) => {
       test(`removeItemAtBottom([${array}]])`, () => {
-        expect(removeItemAtBottom(array)).toStrictEqual(expected);
-      });
-    });
-  });
+        expect(removeItemAtBottom(array)).toStrictEqual(expected)
+      })
+    })
+  })
 
   describe('checkSizeOfStack', () => {
+    /** @type {TestSingleMatrix<boolean>} */
     const checkSizeOfStackTestCases = [
       [[], 0, true],
       [[], 1, false],
@@ -122,12 +159,12 @@ describe('arrays', () => {
       [[9, 8, 7, 1, 4], 4, false],
       [[9, 8, 7, 1, 4], 5, true],
       [[9, 8, 7, 1, 4], 6, false],
-    ];
+    ]
 
     checkSizeOfStackTestCases.forEach(([array, stackSize, expected]) => {
       test(`checkSizeOfStack([${array}], ${stackSize})`, () => {
-        expect(checkSizeOfStack(array, stackSize)).toBe(expected);
-      });
-    });
-  });
-});
+        expect(checkSizeOfStack(array, stackSize)).toBe(expected)
+      })
+    })
+  })
+})
