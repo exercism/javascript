bf52f48ac79101bbfd96bcbcceda47157fe3e04a
Brent Chuang <burennto@gmail.com>
2020-05-26 15:15:00 +1000

    Fix spec for complex-numbers exercise

--- exercises/complex-numbers/complex-numbers.spec.js
+++ exercises/complex-numbers/complex-numbers.spec.js
@@ -136,63 +136,63 @@ describe('Complex numbers', () => {
 
   xtest('Absolute value of a positive purely real number', () => {
     const expected = 5;
-    const actual = new ComplexNumber(5, 0).abs;
+    const actual = new ComplexNumber(5, 0).abs();
 
     expect(actual).toEqual(expected);
   });
 
   xtest('Absolute value of a negative purely real number', () => {
     const expected = 5;
-    const actual = new ComplexNumber(-5, 0).abs;
+    const actual = new ComplexNumber(-5, 0).abs();
 
     expect(actual).toEqual(expected);
   });
 
   xtest('Absolute value of a purely imaginary number with positive imaginary part', () => {
     const expected = 5;
-    const actual = new ComplexNumber(0, 5).abs;
+    const actual = new ComplexNumber(0, 5).abs();
 
     expect(actual).toEqual(expected);
   });
 
   xtest('Absolute value of a purely imaginary number with negative imaginary part', () => {
     const expected = 5;
-    const actual = new ComplexNumber(0, -5).abs;
+    const actual = new ComplexNumber(0, -5).abs();
 
     expect(actual).toEqual(expected);
   });
 
   xtest('Absolute value of a number with real and imaginary part', () => {
     const expected = 5;
-    const actual = new ComplexNumber(3, 4).abs;
+    const actual = new ComplexNumber(3, 4).abs();
 
     expect(actual).toEqual(expected);
   });
 
   xtest('Conjugate a purely real number', () => {
     const expected = new ComplexNumber(5, 0);
-    const actual = new ComplexNumber(5, 0).conj;
+    const actual = new ComplexNumber(5, 0).conj();
 
     expect(actual).toEqual(expected);
   });
 
   xtest('Conjugate a purely imaginary number', () => {
     const expected = new ComplexNumber(0, -5);
-    const actual = new ComplexNumber(0, 5).conj;
+    const actual = new ComplexNumber(0, 5).conj();
 
     expect(actual).toEqual(expected);
   });
 
   xtest('Conjugate a number with real and imaginary part', () => {
     const expected = new ComplexNumber(1, -1);
-    const actual = new ComplexNumber(1, 1).conj;
+    const actual = new ComplexNumber(1, 1).conj();
 
     expect(actual).toEqual(expected);
   });
 
   xtest('Euler\'s identity/formula', () => {
     const expected = new ComplexNumber(-1, 0);
-    const actual = new ComplexNumber(0, Math.PI).exp;
+    const actual = new ComplexNumber(0, Math.PI).exp();
 
     expect(actual.real).toBeCloseTo(expected.real);
     expect(actual.imag).toBeCloseTo(expected.imag);
@@ -200,7 +200,7 @@ describe('Complex numbers', () => {
 
   xtest('Exponential of 0', () => {
     const expected = new ComplexNumber(1, 0);
-    const actual = new ComplexNumber(0, 0).exp;
+    const actual = new ComplexNumber(0, 0).exp();
 
     expect(actual.real).toBeCloseTo(expected.real);
     expect(actual.imag).toBeCloseTo(expected.imag);
@@ -208,7 +208,7 @@ describe('Complex numbers', () => {
 
   xtest('Exponential of a purely real number', () => {
     const expected = new ComplexNumber(Math.E, 0);
-    const actual = new ComplexNumber(1, 0).exp;
+    const actual = new ComplexNumber(1, 0).exp();
 
     expect(actual.real).toBeCloseTo(expected.real);
     expect(actual.imag).toBeCloseTo(expected.imag);
@@ -216,7 +216,7 @@ describe('Complex numbers', () => {
 
   xtest('Exponential of a number with real and imaginary part', () => {
     const expected = new ComplexNumber(-2, 0);
-    const actual = new ComplexNumber(Math.LN2, Math.PI).exp;
+    const actual = new ComplexNumber(Math.LN2, Math.PI).exp();
 
     expect(actual.real).toBeCloseTo(expected.real);
     expect(actual.imag).toBeCloseTo(expected.imag);
