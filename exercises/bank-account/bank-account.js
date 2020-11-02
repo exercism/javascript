6cc0907105afcf456ea4804f73dc80fb347e8425
TomPradat <tom.pradat@gmail.com>
2020-11-02 02:29:23 +0100

    New exercise : bank account (#888)
    * feat: add bank account exercise
    
    * Add README and config.json entry for bank account exercise
    
    * Add stubbed functions to stub file for bank account exercise
    
    * bank-account: increase difficulty and unlock with space-age
    
    * bank-account: add error message to ValueError
    
    * :ok_hand: Sync exercise config files
    
    Co-authored-by: Brendan Mckeown <brendan.mckeown@thinkbrownstone.com>

---
+++ exercises/bank-account/bank-account.js
@@ -0,0 +1,40 @@
+//
+// This is only a SKELETON file for the 'Bank Account' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class BankAccount {
+
+  constructor() {
+    throw new Error('Remove this statement and implement this function');
+  }
+
+  open() {
+    throw new Error('Remove this statement and implement this function');
+  }
+
+  close() {
+    throw new Error('Remove this statement and implement this function');
+  }
+
+  deposit() {
+    throw new Error('Remove this statement and implement this function');
+  }
+
+  withdraw() {
+    throw new Error('Remove this statement and implement this function');
+  }
+
+  get balance() {
+    throw new Error('Remove this statement and implement this function');
+  }
+
+}
+
+export class ValueError extends Error {
+
+  constructor() {
+    super('Bank account error');
+  }
+
+}
