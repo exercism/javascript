98afadeddbbbfb905b601a5822ae3ca9a4fe280c
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2020-12-18 01:20:29 +0100

    Sync exericses H*

--- exercises/high-scores/high-scores.spec.js
+++ exercises/high-scores/high-scores.spec.js
@@ -16,28 +16,30 @@ describe('High Scores Test Suite', () => {
     expect(new HighScores(input).personalBest).toEqual(100);
   });
 
-  xtest('Personal top three from a list of scores', () => {
-    const input = [10, 30, 90, 30, 100, 20, 10, 0, 30, 40, 40, 70, 70];
-    expect(new HighScores(input).personalTopThree).toEqual([100, 90, 70]);
-  });
+  describe('Top 3 scores', () => {
+    xtest('Personal top three from a list of scores', () => {
+      const input = [10, 30, 90, 30, 100, 20, 10, 0, 30, 40, 40, 70, 70];
+      expect(new HighScores(input).personalTopThree).toEqual([100, 90, 70]);
+    });
 
-  xtest('Personal top highest to lowest', () => {
-    const input = [20, 10, 30];
-    expect(new HighScores(input).personalTopThree).toEqual([30, 20, 10]);
-  });
+    xtest('Personal top highest to lowest', () => {
+      const input = [20, 10, 30];
+      expect(new HighScores(input).personalTopThree).toEqual([30, 20, 10]);
+    });
 
-  xtest('Personal top when there is a tie', () => {
-    const input = [40, 20, 40, 30];
-    expect(new HighScores(input).personalTopThree).toEqual([40, 40, 30]);
-  });
+    xtest('Personal top when there is a tie', () => {
+      const input = [40, 20, 40, 30];
+      expect(new HighScores(input).personalTopThree).toEqual([40, 40, 30]);
+    });
 
-  xtest('Personal top when there are less than 3', () => {
-    const input = [30, 70];
-    expect(new HighScores(input).personalTopThree).toEqual([70, 30]);
-  });
+    xtest('Personal top when there are less than 3', () => {
+      const input = [30, 70];
+      expect(new HighScores(input).personalTopThree).toEqual([70, 30]);
+    });
 
-  xtest('Personal top when there is only one', () => {
-    const input = [40];
-    expect(new HighScores(input).personalTopThree).toEqual([40]);
+    xtest('Personal top when there is only one', () => {
+      const input = [40];
+      expect(new HighScores(input).personalTopThree).toEqual([40]);
+    });
   });
 });
