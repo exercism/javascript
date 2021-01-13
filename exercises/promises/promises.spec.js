16b5507b1e8ecb477f5fc950fe1e8ff80cbed8e7
Shane Laymance <shane.laymance@gmail.com>
2021-01-12 23:27:12 -0800

    Add more tests for promisify

--- exercises/promises/promises.spec.js
+++ exercises/promises/promises.spec.js
@@ -7,19 +7,15 @@ import {
 } from './promises';
 
 describe('promises', () => {
-  const createCallbackFn = speed => (arg, callback) => setTimeout(() => {
-    try {
-      callback(null, arg);
-    } catch (err) {
-      callback(err);
-    }
-  }, speed);
+  const createCallbackFn = speed => (value, callback) =>
+    setTimeout(() => callback(null, value), speed);
+  const failedCallback = new Error('Failed callback');
+  const createFailedCallback = speed => (_, callback) =>
+    setTimeout(() => callback(failedCallback), speed);
 
-  let fastCallbackFn;
-
-  beforeEach(() => {
-    fastCallbackFn = createCallbackFn(0);
-  });
+  const slowestCallbackFn = createCallbackFn(20);
+  const slowerCallbackFn = createCallbackFn(10);
+  const fastCallbackFn = createCallbackFn(0);
 
   describe('promisify', () => {
     test('promisify returns a function', () => {
@@ -31,7 +27,16 @@ describe('promises', () => {
       expect(fastPromise('fast')).toBeInstanceOf(Promise);
     });
 
-    test('')
+    test('promisified function resolves to the callback\'s success value', () => {
+      const fastPromise = promisify(fastCallbackFn);
+      const successValue = 'success';
+      expect(fastPromise(successValue)).resolves.toEqual(successValue);
+    });
+
+    test('promisified function will reject a callback\'s error', () => {
+      const failedPromise = promisify(createFailedCallback(0));
+      expect(failedPromise(null)).rejects.toEqual(failedCallback);
+    });
   });
 
   describe('all', () => { });
