59da273da4b5803d8a7ef1040cd9691a64072653
Erik Schierboom <erik_schierboom@hotmail.com>
2021-01-29 14:54:00 +0100

    [v3] Update fetch-configlet script to work with configlet v3

--- bin/fetch-configlet.ps1
+++ bin/fetch-configlet.ps1
@@ -1,24 +1,26 @@
-Function DownloadUrl ([string] $FileName, $Headers) {
-    $latestUrl = "https://api.github.com/repos/exercism/configlet/releases/latest"
-    $json = Invoke-RestMethod -Headers $Headers -Uri $latestUrl
-    $json.assets | Where-Object { $_.browser_download_url -match $FileName } | Select-Object -ExpandProperty browser_download_url
+$ErrorActionPreference = "Stop"
+$ProgressPreference = "SilentlyContinue"
+
+$requestOpts = @{
+    Headers = If ($env:GITHUB_TOKEN) { @{ Authorization = "Bearer ${env:GITHUB_TOKEN}" } } Else { @{ } }
+    MaximumRetryCount = 3
+    RetryIntervalSec = 1
 }
 
-Function Headers {
-    If ($GITHUB_TOKEN) { @{ Authorization = "Bearer ${GITHUB_TOKEN}" } } Else { @{ } }
-}
-
-Function Arch {
-    If ([Environment]::Is64BitOperatingSystem) { "64bit" } Else { "32bit" }
-}
-
-$arch = Arch
-$headers = Headers
+$arch = If ([Environment]::Is64BitOperatingSystem) { "64bit" } Else { "32bit" }
 $fileName = "configlet-windows-$arch.zip"
+
+Function Get-DownloadUrl {
+    $latestUrl = "https://api.github.com/repos/exercism/configlet/releases/latest"
+    Invoke-RestMethod -Uri $latestUrl -PreserveAuthorizationOnRedirect @requestOpts
+    | Select-Object -ExpandProperty assets
+    | Where-Object { $_.browser_download_url -match $FileName }
+    | Select-Object -ExpandProperty browser_download_url
+}
+
+$downloadUrl = Get-DownloadUrl
 $outputDirectory = "bin"
 $outputFile = Join-Path -Path $outputDirectory -ChildPath $fileName
-$zipUrl = DownloadUrl -FileName $fileName -Headers $headers
-
-Invoke-WebRequest -Headers $headers -Uri $zipUrl -OutFile $outputFile
+Invoke-WebRequest -Uri $downloadUrl -OutFile $outputFile @requestOpts
 Expand-Archive $outputFile -DestinationPath $outputDirectory -Force
 Remove-Item -Path $outputFile
