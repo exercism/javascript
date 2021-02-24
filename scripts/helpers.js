e480c6983d01fe538cb53f7cdcb9ad4ad9fbd8ef
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2021-02-24 06:20:40 +0100

    Update sync and checksum script

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,23 +1,82 @@
 /**
- * This file provides helper functions
- * & is NOT intended to be run as a script.
+ * This file provides helper functions & is NOT intended to be run as a script.
  */
 
 const shell = require('shelljs');
 const path = require('path');
+const crypto = require('crypto');
 
-const exerciseDirs = shell.ls('-d', path.join('exercises', '*'));
+const exerciseDirs = shell.ls(
+  '-d',
+  path.join('exercises', '{practice,concept}', '*')
+);
 
 export const assignments = shell.env['ASSIGNMENT']
   ? [shell.env['ASSIGNMENT']]
-  : exerciseDirs
-      .map((dir) => path.basename(dir))
-      .filter((exercise) => !exercise.deprecated);
+  : knownAssignments();
+
+function knownAssignments() {
+  return exerciseDirs.map((directory) =>
+    path.join(path.basename(path.dirname(directory)), path.basename(directory))
+  );
+}
+
+export function assertAssignment(assignment, shouldExist = true) {
+  assignment = assignment.replace(/[/\\]/g, path.sep);
+
+  if (assignment.split(path.sep).length === 2) {
+    if (!shouldExist) {
+      return true;
+    }
+
+    if (shell.test(`-d`, path.join('exercises', assignment))) {
+      return true;
+    }
+  }
+
+  shell.echo("[Failure] that's not a valid assignment reference");
+  const chalk = require('chalk');
+
+  if (assignment.split(path.sep).length === 1) {
+    // prettier-ignore
+    shell.echo(`
+Expected ${chalk.cyan(`{type}${path.sep}{slug}`)}, actual: ${chalk.yellow(assignment)}.
+- Use ${chalk.green(`concept${path.sep}${assignment}`)} if ${chalk.yellow(assignment)} is a concept exercise.
+- Use ${chalk.green(`practice${path.sep}${assignment}`)} if ${chalk.yellow(assignment)} is a practice exercise.
+    `.trim());
+  }
+
+  const suggestions = knownAssignments().filter((known) =>
+    known.includes(assignment)
+  );
+
+  if (suggestions.length > 0 && suggestions.length < 5) {
+    shell.echo(
+      '\nDid you mean:\n' +
+        suggestions.map((suggestion) => `- ${suggestion}`).join('\n')
+    );
+  }
+
+  return false;
+}
+
+export function sha(str) {
+  return crypto.createHash('md5').update(str).digest('hex');
+}
 
 export function findExerciseDirectory(input) {
-  return exerciseDirs.find((exerciseDir) => {
+  const directory = exerciseDirs.find((exerciseDir) => {
     return input.indexOf(exerciseDir) !== -1;
   });
+
+  if (!directory) {
+    return null;
+  }
+
+  return path.join(
+    path.basename(path.dirname(directory)),
+    path.basename(directory)
+  );
 }
 
 export function hasStub(assignment) {
@@ -43,7 +102,6 @@ export function shouldCleanup() {
   return envIsThruthy('CLEANUP');
 }
 
-// Preapre all exercises (see above) & run a given command
 export function prepareAndRun(command, infoStr, failureStr) {
   if (shouldPrepare()) {
     const assignment = shell.env['ASSIGNMENT'];
@@ -72,28 +130,98 @@ export function prepareAndRun(command, infoStr, failureStr) {
   }
 }
 
-// Delete tmp directory
 export function cleanUp() {
   shell.rm('-rf', 'tmp_exercises');
+  shell.rm('-f', 'exercise-package.json');
+  shell.rm('-f', 'exercise-package.json.sha');
 }
 
-// These packages will be skipped while performing checksum
-const SKIP_PACKAGES_FOR_CHECKSUM = ['shelljs', '@babel/node'];
+// These packages will be skipped while performing checksum. In other words,
+// these packages are only interesting for maintaining this repository and not
+// for the student.
+const SKIP_PACKAGES_FOR_CHECKSUM = [
+  'shelljs',
+  '@babel/node',
+  'prettier',
+  'diff',
+  'chalk',
+];
 
-// Filter out some unwanted packages and create package.json for exercises
-export function createExercisePackageJson(assignmentVersion) {
+// These fields may differ between package.json files.
+const SKIP_FIELDS_FOR_CHECKSUM = [
+  'name',
+  'description',
+  'author',
+  'contributors',
+];
+
+export function createExercisePackageJson(writeSha = false) {
   const packageFile = shell.cat('package.json').toString();
   const packageJson = JSON.parse(packageFile);
 
-  packageJson['version'] = assignmentVersion;
+  // Filter out some unwanted packages and create package.json for exercises
   SKIP_PACKAGES_FOR_CHECKSUM.forEach(
     (pkg) => delete packageJson['devDependencies'][pkg]
   );
 
   const shellStr = new shell.ShellString(
-    JSON.stringify(packageJson, null, 2) + '\n'
+    JSON.stringify(packageJson, undefined, 2) + '\n'
   );
+
   shellStr.to('exercise-package.json');
+
+  // Now remove all fields that should not be considered for checksum
+  SKIP_FIELDS_FOR_CHECKSUM.forEach((field) => {
+    delete packageJson[field];
+  });
+
+  delete packageJson['repository']['directory'];
+
+  if (writeSha) {
+    const checksumShellStr = new shell.ShellString(shaPackageJson(packageJson));
+    checksumShellStr.to('exercise-package.json.sha');
+  }
+}
+
+export function mergePackageJsons(basePackageJson, packageJson) {
+  const mergedPackageJson = JSON.parse(JSON.stringify(basePackageJson));
+
+  SKIP_FIELDS_FOR_CHECKSUM.forEach((key) => {
+    mergedPackageJson[key] = packageJson[key];
+  });
+
+  const extraRepositoryKeys = Object.keys(packageJson.repository).filter(
+    (key) => !basePackageJson.repository[key]
+  );
+
+  extraRepositoryKeys.forEach((key) => {
+    mergedPackageJson.repository[key] = packageJson.repository[key];
+  });
+
+  return mergedPackageJson;
+}
+
+export function prepareExercisePackageJson(destinationPath, writeSha = false) {
+  const packageFile = shell.cat(destinationPath).toString();
+  const packageJson = JSON.parse(packageFile);
+
+  // Now remove all fields that should not be considered for checksum
+  SKIP_FIELDS_FOR_CHECKSUM.forEach((field) => {
+    delete packageJson[field];
+  });
+
+  delete packageJson['repository']['directory'];
+
+  if (writeSha) {
+    const checksumShellStr = new shell.ShellString(shaPackageJson(packageJson));
+    checksumShellStr.to(`${destinationPath}.sha`);
+  }
+
+  return packageJson;
+}
+
+export function shaPackageJson(packageJson) {
+  return sha(JSON.stringify(packageJson, undefined, 2).trim() + '\n');
 }
 
 /**
@@ -140,6 +268,10 @@ export function registerExitHandler() {
   function exitHandler(options, exitCode) {
     cleanUp();
 
+    if (options.error) {
+      console.error(options.error);
+    }
+
     if (options.cleanup) {
       /* clean exit */
     }
@@ -163,5 +295,7 @@ export function registerExitHandler() {
   process.on('SIGUSR2', exitHandler.bind(null, { exit: true }));
 
   //catches uncaught exceptions
-  process.on('uncaughtException', exitHandler.bind(null, { exit: true }));
+  process.on('uncaughtException', (error) =>
+    exitHandler({ exit: true, error })
+  );
 }
