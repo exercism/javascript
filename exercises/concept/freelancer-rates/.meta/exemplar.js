7c04135c56dac7a21da15309f2be9dda8b1fe08e
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2021-02-28 23:32:31 +0100

    Match exemplar with stub

--- exercises/concept/freelancer-rates/.meta/exemplar.js
+++ exercises/concept/freelancer-rates/.meta/exemplar.js
@@ -1,4 +1,23 @@
 // @ts-check
+//
+// ☝🏽 The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion on the web
+// and supported IDEs when implementing this exercise. You don't need to
+// understand types, JSDoc, or TypeScript in order to complete this JavaScript
+// exercise, and can completely ignore this comment block and directive.
+
+// 👋🏽 Hi again!
+//
+// A quick reminder about exercise stubs:
+//
+// 💡 You're allowed to completely clear any stub before you get started. Often
+// we recommend using the stub, because they are already set-up correctly to
+// work with the tests, which you can find in ./annalyns-infiltration.spec.js.
+//
+// 💡 You don't need to write JSDoc comment blocks yourself; it is not expected
+// in idiomatic JavaScript, but some companies and style-guides do enforce them.
+//
+// Get those rates calculated!
 
 /**
  * The day rate, given a rate per hour
