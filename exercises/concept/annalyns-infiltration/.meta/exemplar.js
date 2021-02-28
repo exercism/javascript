3701d94a0e4d305bbebc677be703ac7eea13bca9
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2021-02-28 23:16:39 +0100

    Match exemplar with stub

--- exercises/concept/annalyns-infiltration/.meta/exemplar.js
+++ exercises/concept/annalyns-infiltration/.meta/exemplar.js
@@ -6,6 +6,19 @@
 // understand types, JSDoc, or TypeScript in order to complete this JavaScript
 // exercise, and can completely ignore this comment block and directive.
 
+// 👋🏽 Hi again!
+//
+// A quick reminder about exercise stubs:
+//
+// 💡 You're allowed to completely clear any stub before you get started. Often
+// we recommend using the stub, because they are already set-up correctly to
+// work with the tests, which you can find in ./annalyns-infiltration.spec.js.
+//
+// 💡 You don't need to write JSDoc comment blocks yourself; it is not expected
+// in idiomatic JavaScript, but some companies and style-guides do enforce them.
+//
+// Now help Annalyn free her best friend!
+
 /**
  * The fast attack is available when the knight is sleeping
  *
