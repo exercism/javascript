0a1450b8b7c97d41176102d3b35fbd03534a9609
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2021-02-28 23:09:25 +0100

    Match exemplar with stub

--- exercises/concept/annalyns-infiltration/.meta/exemplar.js
+++ exercises/concept/annalyns-infiltration/.meta/exemplar.js
@@ -1,15 +1,57 @@
+// @ts-check
+//
+// ☝🏽 The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion on the web
+// and supported IDEs when implementing this exercise. You don't need to
+// understand types, JSDoc, or TypeScript in order to complete this JavaScript
+// exercise, and can completely ignore this comment block and directive.
+
+/**
+ * The fast attack is available when the knight is sleeping
+ *
+ * @param {boolean} knightIsAwake
+ *
+ * @return {boolean} Whether or not you can execute a fast attack.
+ */
 export function canExecuteFastAttack(knightIsAwake) {
   return !knightIsAwake;
 }
 
+/**
+ * A useful spy captures information, which they can't do if everyone's asleep.
+ *
+ * @param {boolean} knightIsAwake
+ * @param {boolean} archerIsAwake
+ * @param {boolean} prisonerIsAwake
+ *
+ * @returns {boolean} Whether or not you can spy on someone.
+ */
 export function canSpy(knightIsAwake, archerIsAwake, prisonerIsAwake) {
   return knightIsAwake || archerIsAwake || prisonerIsAwake;
 }
 
+/**
+ * You'll get caught by the archer if you signal while they're awake.
+ *
+ * @param {boolean} archerIsAwake
+ * @param {boolean} prisonerIsAwake
+ *
+ * @returns {boolean} Whether or not you can send a signal to the prisoner.
+ */
 export function canSignalPrisoner(archerIsAwake, prisonerIsAwake) {
   return prisonerIsAwake && !archerIsAwake;
 }
 
+/**
+ * The final stage in the plan: freeing Annalyn's best friend.
+ *
+ * @param {boolean} knightIsAwake
+ * @param {boolean} archerIsAwake
+ * @param {boolean} prisonerIsAwake
+ * @param {boolean} petDogIsPresent
+ *
+ * @returns {boolean} Whether or not you can free Annalyn's friend.
+ */
 export function canFreePrisoner(
   knightIsAwake,
   archerIsAwake,
