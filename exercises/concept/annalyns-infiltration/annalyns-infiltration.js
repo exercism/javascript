71ca085afda148134f418d017565af558a20f45c
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2021-02-28 23:08:57 +0100

    Add welcome block

--- exercises/concept/annalyns-infiltration/annalyns-infiltration.js
+++ exercises/concept/annalyns-infiltration/annalyns-infiltration.js
@@ -1,8 +1,23 @@
 // @ts-check
 //
-// The line above enables type checking for this file. Various IDEs interpret
-// the @ts-check directive. It will give you helpful autocompletion when
-// implementing this exercise.
+// ☝🏽 The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion on the web
+// and supported IDEs when implementing this exercise. You don't need to
+// understand types, JSDoc, or TypeScript in order to complete this JavaScript
+// exercise, and can completely ignore this comment block and directive.
+
+// 👋🏽 Hi again!
+//
+// A quick reminder about exercise stubs:
+//
+// 💡 You're allowed to completely clear any stub before you get started. Often
+// we recommend using the stub, because they are already set-up correctly to
+// work with the tests, which you can find in ./annalyns-infiltration.spec.js.
+//
+// 💡 You don't need to write JSDoc comment blocks yourself; it is not expected
+// in idiomatic JavaScript, but some companies and style-guides do enforce them.
+//
+// Now help Annalyn free her best friend!
 
 /**
  * The fast attack is available when the knight is sleeping
