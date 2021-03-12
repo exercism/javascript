099064b51f2b1849916edbea4239ec4747d1c058
junedev <12543047+junedev@users.noreply.github.com>
2021-03-12 22:30:58 +0100

    exemplar solution part 1

--- exercises/concept/vehicle-purchase/.meta/exemplar.js
+++ exercises/concept/vehicle-purchase/.meta/exemplar.js
@@ -1,76 +1,38 @@
-FIXME
-
 // @ts-check
 //
-// ☝🏽 The line above enables type checking for this file. Various IDEs interpret
-// the @ts-check directive. It will give you helpful autocompletion on the web
-// and supported IDEs when implementing this exercise. You don't need to
-// understand types, JSDoc, or TypeScript in order to complete this JavaScript
-// exercise, and can completely ignore this comment block and directive.
-
-// 👋🏽 Hi there!
-//
-// On the JavaScript track we provide you with stubs. These stubs provide a
-// starting point to solving the exercise.
-//
-// In general, each variable/constant and each declared function will have a
-// JSDoc comment block above it, explaining what the variable/constant holds or
-// the function is supposed to accomplish.
-//
-// 💡 Often, the JSDoc comment blocks have annotations, such as @param and
-// @returns which are usually highlighted with a different color if the IDE
-// you're in recognizes them. It's these annotations that are used when
-// referring to the constant, variable, or function from somewhere else that
-// IDEs display.
-//
-// You don't need to write these yourself; it is not expected in idiomatic
-// JavaScript, but some companies and style-guides do enforce them.
-//
-// 💡 You're allowed to completely clear a stub before you get started. Often
-// we recommend using the stub, because they are already set-up correctly to
-// work with the tests, which you can find in ./lasagna.spec.js
-//
-// Good luck preparing some lasagna!
+// The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion when
+// implementing this exercise.
 
 /**
- * The amount of minutes the lasagna should be in the oven
+ * Determines whether or not you need a licence to operate a certain kind of vehicle.
+ * 
+ * @param {string} kind
+ * @returns {boolean} whether a license is required
  */
-export const EXPECTED_MINUTES_IN_OVEN = 40;
-
-/**
- * The amount of minutes it takes to prepare a single layer.
- */
-const PREPARATION_MINUTES_PER_LAYER = 2;
-
-/**
- * Determine the amount of minutes the lasagna still needs to remain in the
- * oven to be properly prepared.
- *
- * @param {number} actualMinutesInOven
- * @returns {number} the number of minutes remaining
- */
-export function remainingMinutesInOven (actualMinutesInOven) {
-  return EXPECTED_MINUTES_IN_OVEN - actualMinutesInOven;
+export function needsLicense(kind) {
+  return kind === "car" || kind === "truck"
 }
 
 /**
- * Given a number of layers, determine the total preparation time.
- *
- * @param {number} numberOfLayers
- * @returns {number} the total preparation time
+ * Helps choosing between two options by recommending the one that
+ * comes first in dictionary order.
+ * 
+ * @param {string} option1 
+ * @param {string} option2 
+ * @returns {string} a sentence of advice which option to choose
  */
-export function preparationTimeInMinutes (numberOfLayers) {
-  return numberOfLayers * PREPARATION_MINUTES_PER_LAYER;
+export function chooseVehicle(option1, option2) {
+  const selection = ""
+  if (option1 < option2) {
+    selection = option1
+  } else {
+    selection = option2
+  }
+
+  return selection + " is clearly the better choice."
 }
 
-/**
- * Calculate the total working time. That is, the time to prepare all the layers
- * of lasagna, and the time already spent in the oven.
- *
- * @param {number} numberOfLayers
- * @param {number} actualMinutesInOven
- * @returns {number} the total working time
- */
-export function totalTimeInMinutes (numberOfLayers, actualMinutesInOven) {
-  return preparationTimeInMinutes(numberOfLayers) + actualMinutesInOven;
+export function calculatePrice(originalPrice, discount) {
+  // FIXME continue here
 }
