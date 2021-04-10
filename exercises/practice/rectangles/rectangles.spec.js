c86daf75352b9cce710b9c70ede7567d3166ab58
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2021-04-10 23:08:37 +0200

    Remove wrapper class

--- exercises/practice/rectangles/rectangles.spec.js
+++ exercises/practice/rectangles/rectangles.spec.js
@@ -1,97 +1,79 @@
-import { Rectangles } from './rectangles.js';
+import { count } from './rectangles';
 
 describe('Rectangles', () => {
   test('no rows', () => {
     const expected = 0;
-    const actual = Rectangles.count([]);
+    const actual = count([]);
 
     expect(actual).toEqual(expected);
   });
 
   xtest('no columns', () => {
     const expected = 0;
-    const actual = Rectangles.count(['']);
+    const actual = count(['']);
 
     expect(actual).toEqual(expected);
   });
 
   xtest('no rectangles', () => {
     const expected = 0;
-    const actual = Rectangles.count([' ']);
+    const actual = count([' ']);
 
     expect(actual).toEqual(expected);
   });
 
   xtest('one rectangle', () => {
     const expected = 1;
-    const actual = Rectangles.count(['+-+', '| |', '+-+']);
+    const actual = count(['+-+', '| |', '+-+']);
 
     expect(actual).toEqual(expected);
   });
 
   xtest('two rectangles without shared parts', () => {
     const expected = 2;
-    const actual = Rectangles.count([
-      '  +-+',
-      '  | |',
-      '+-+-+',
-      '| |  ',
-      '+-+  ',
-    ]);
+    const actual = count(['  +-+', '  | |', '+-+-+', '| |  ', '+-+  ']);
 
     expect(actual).toEqual(expected);
   });
 
   xtest('five rectangles with shared parts', () => {
     const expected = 5;
-    const actual = Rectangles.count([
-      '  +-+',
-      '  | |',
-      '+-+-+',
-      '| | |',
-      '+-+-+',
-    ]);
+    const actual = count(['  +-+', '  | |', '+-+-+', '| | |', '+-+-+']);
 
     expect(actual).toEqual(expected);
   });
 
   xtest('rectangle of height 1 is counted', () => {
     const expected = 1;
-    const actual = Rectangles.count(['+--+', '+--+']);
+    const actual = count(['+--+', '+--+']);
 
     expect(actual).toEqual(expected);
   });
 
   xtest('rectangle of width 1 is counted', () => {
     const expected = 1;
-    const actual = Rectangles.count(['++', '||', '++']);
+    const actual = count(['++', '||', '++']);
 
     expect(actual).toEqual(expected);
   });
 
   xtest('1x1 square is counted', () => {
     const expected = 1;
-    const actual = Rectangles.count(['++', '++']);
+    const actual = count(['++', '++']);
 
     expect(actual).toEqual(expected);
   });
 
   xtest('only complete rectangles are counted', () => {
     const expected = 1;
-    const actual = Rectangles.count([
-      '  +-+',
-      '    |',
-      '+-+-+',
-      '| | -',
-      '+-+-+',
-    ]);
+    const actual = count(['  +-+', '    |', '+-+-+', '| | -', '+-+-+']);
 
     expect(actual).toEqual(expected);
   });
 
   xtest('rectangles can be of different sizes', () => {
     const expected = 3;
-    const actual = Rectangles.count([
+    const actual = count([
       '+------+----+',
       '|      |    |',
       '+---+--+    |',
@@ -104,7 +86,7 @@ describe('Rectangles', () => {
 
   xtest('corner is required for a rectangle to be complete', () => {
     const expected = 2;
-    const actual = Rectangles.count([
+    const actual = count([
       '+------+----+',
       '|      |    |',
       '+------+    |',
@@ -117,7 +99,7 @@ describe('Rectangles', () => {
 
   xtest('large input with many rectangles', () => {
     const expected = 60;
-    const actual = Rectangles.count([
+    const actual = count([
       '+---+--+----+',
       '|   +--+----+',
       '+---+--+    |',
