17f3268cfe6b3df8627d04f49b68972be1dfa44c
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2021-04-26 03:16:41 +0200

    Mark tests as unskipped
    In concept exercises we don't skip tests because students don't have skipped tests on the web editor, so this makes it a similar experience via the CLI. They also don't see the tests on the web editor.

--- exercises/concept/ozans-playlist/ozans-playlist.spec.js
+++ exercises/concept/ozans-playlist/ozans-playlist.spec.js
@@ -8,13 +8,13 @@ import {
 
 describe("Ozan's playlist", () => {
   describe('removeDuplicates', () => {
-    xtest('works for an empty playlist', () => {
+    test('works for an empty playlist', () => {
       const playlist = [];
 
       expect(removeDuplicates(playlist)).toEqual([]);
     });
 
-    xtest('works for a non-empty playlist', () => {
+    test('works for a non-empty playlist', () => {
       const TRACK_1 = 'Two Paintings and a Drum - Carl Cox';
       const TRACK_2 = 'Leash Called Love - The Sugarcubes';
       const playlist = [TRACK_1, TRACK_2, TRACK_1];
@@ -28,13 +28,13 @@ describe("Ozan's playlist", () => {
     const TRACK_1 = 'Big Science - Laurie Anderson';
     const TRACK_2 = 'Tightrope - Laurie Anderson';
 
-    xtest('returns true when the track is in the playlist', () => {
+    test('returns true when the track is in the playlist', () => {
       const playlist = [TRACK_1, TRACK_2];
 
       expect(hasTrack(playlist, TRACK_1)).toBe(true);
     });
 
-    xtest('returns false when the track is not in the playlist', () => {
+    test('returns false when the track is not in the playlist', () => {
       const playlist = [TRACK_2];
 
       expect(hasTrack(playlist, TRACK_1)).toBe(false);
@@ -45,14 +45,14 @@ describe("Ozan's playlist", () => {
     const TRACK_1 = 'Jigsaw Feeling - Siouxsie and the Banshees';
     const TRACK_2 = 'Feeling Good - Nina Simone';
 
-    xtest('adds a track that is not already in the playlist', () => {
+    test('adds a track that is not already in the playlist', () => {
       const playlist = [];
       const expected = [TRACK_1];
 
       expect(addTrack(playlist, TRACK_1)).toEqual(expected);
     });
 
-    xtest('does not add a track that is already in the playlist', () => {
+    test('does not add a track that is already in the playlist', () => {
       const playlist = [TRACK_1, TRACK_2];
       const expected = [TRACK_1, TRACK_2];
 
@@ -64,14 +64,14 @@ describe("Ozan's playlist", () => {
     const TRACK_1 = 'Ancestors - Tanya Tagaq';
     const TRACK_2 = 'Take This Hammer - Lead Belly';
 
-    xtest('works if the track is present in the playlist', () => {
+    test('works if the track is present in the playlist', () => {
       const playlist = [TRACK_1, TRACK_2];
       const expected = [TRACK_2];
 
       expect(deleteTrack(playlist, TRACK_1)).toEqual(expected);
     });
 
-    xtest('works if the track is not present in the playlist', () => {
+    test('works if the track is not present in the playlist', () => {
       const playlist = [TRACK_2];
       const expected = [TRACK_2];
 
@@ -80,13 +80,13 @@ describe("Ozan's playlist", () => {
   });
 
   describe('listArtists', () => {
-    xtest('works for an empty playlist', () => {
+    test('works for an empty playlist', () => {
       const playlist = [];
 
       expect(listArtists(playlist)).toEqual([]);
     });
 
-    xtest('works for a non-empty playlist', () => {
+    test('works for a non-empty playlist', () => {
       const playlist = [
         'Onu Alma Beni Al - Sezen Aksu',
         'Famous Blue Raincoat - Leonard Cohen',
