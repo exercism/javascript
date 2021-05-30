32e8a667b2fb6bd7037c25a49e7a632ca245335f
junedev <12543047+junedev@users.noreply.github.com>
2021-05-30 17:50:49 +0200

    add stub and tests

--- exercises/concept/high-score-board/high-score-board.js
+++ exercises/concept/high-score-board/high-score-board.js
@@ -1,17 +1,67 @@
 // @ts-check
-//
-// The line above enables type checking for this file. Various IDEs interpret
-// the @ts-check directive. It will give you helpful autocompletion when
-// implementing this exercise.
-
-FIXME;
 
 /**
- * Determines whether or not you need a licence to operate a certain kind of vehicle.
+ * Creates a new score board with an initial entry.
  *
- * @param {string} kind
- * @returns {boolean} whether a license is required
+ * @returns {Object.<string, number>} new score board
  */
-export function needsLicense(kind) {
-  throw new Error('Please implement the needsLicense method');
+export function createScoreBoard () {
+  throw new Error('Please implement the createScoreBoard function');
+}
+
+/**
+ * Adds a player to a score board.
+ *
+ * @param {Object.<string, number>} scoreBoard
+ * @param {string} player
+ * @param {number} score
+ * @returns {Object.<string, number>} updated score board
+ */
+export function addPlayer (scoreBoard, player, score) {
+  throw new Error('Please implement the addPlayer function');
+}
+
+/**
+ * Removes a player from a score board.
+ *
+ * @param {Object.<string, number>} scoreBoard
+ * @param {string} player
+ * @returns {Object.<string, number>} updated score board
+ */
+export function removePlayer (scoreBoard, player) {
+  throw new Error('Please implement the removePlayer function');
+}
+
+/**
+ * Increases a player's score by the given amount.
+ *
+ * @param {Object.<string, number>} scoreBoard
+ * @param {string} player
+ * @param {number} points
+ * @returns {Object.<string, number>} updated score board
+ */
+export function updateScore (scoreBoard, player, points) {
+  throw new Error('Please implement the addToScore function');
+}
+
+/**
+ * Applies 100 bonus points to all players on the board.
+ *
+ * @param {Object.<string, number>} scoreBoard
+ * @returns {Object.<string, number>} updated score board
+ */
+export function applyMondayBonus (scoreBoard) {
+  throw new Error('Please implement the applyMondayBonus function');
+}
+
+/**
+ * Normalizes a score with the provided normalization function.
+ *
+ * @param {Object} params
+ * @param {number} params.score
+ * @param {function} params.normalizeFn
+ * @returns {number} normalized score
+ */
+export function normalizeScore (params) {
+  throw new Error('Please implement the normalizeScore function');
 }
