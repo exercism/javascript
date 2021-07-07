51e2f03ca5fbe939130fcd7bddd0c114d1f26cea
Erik Schierboom <erik_schierboom@hotmail.com>
2021-07-07 12:07:24 +0200

    Simplify the `hello-world` exercise's stub. The goal of this is to make things easier for students to get started and to introduce as little syntax as possible.

--- exercises/practice/hello-world/hello-world.js
+++ exercises/practice/hello-world/hello-world.js
@@ -3,6 +3,4 @@
 // convenience to get you started writing code faster.
 //
 
-export const hello = () => {
-  throw new Error('Remove this statement and implement this function');
-};
+export const hello = () => 'Goodbye, Mars!';
