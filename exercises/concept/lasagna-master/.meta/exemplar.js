1171fdeb4f8d23b043d4de1dc2e7380c16108308
junedev <12543047+junedev@users.noreply.github.com>
2021-07-28 21:57:59 +0200

    add exemplar solution

--- exercises/concept/lasagna-master/.meta/exemplar.js
+++ exercises/concept/lasagna-master/.meta/exemplar.js
@@ -1,3 +1,4 @@
+/// <reference path="./global.d.ts" />
 // @ts-check
 
 /**
@@ -9,12 +10,80 @@
  */
 export function cookingStatus(time) {
   if (time === undefined) {
-    return 'You forgot to set the timer.'
+    return 'You forgot to set the timer.';
   }
 
   if (time === 0) {
-    return 'Lasagna is done.'
+    return 'Lasagna is done.';
   }
 
-  return 'Not done, please wait.'
+  return 'Not done, please wait.';
+}
+
+/**
+ * Estimates the preparation time based on the number of layers.
+ *
+ * @param {string[]} layers
+ * @param {number} avgPrepTime
+ * @returns {number} total preparation time
+ */
+export function preparationTime(layers, avgPrepTime = 2) {
+  return layers.length * avgPrepTime;
+}
+
+/**
+ * Calculates how many noodles and much sauce are needed for the
+ * given layers.
+ *
+ * @param {string[]} layers
+ * @returns {Quantities} quantities needed for the given layers
+ */
+export function quantities(layers) {
+  let noodles = 0;
+  let sauce = 0;
+
+  for (let i = 0; i < layers.length; i++) {
+    if (layers[i] === 'noodles') {
+      noodles += 50;
+    }
+
+    if (layers[i] === 'sauce') {
+      sauce += 0.2;
+    }
+  }
+
+  return { noodles, sauce };
+}
+
+/**
+ * Adds the secret ingredient from the ingredient list that a
+ * friend provided to your ingredient list.
+ * 
+ * @param {string[]} friendsList 
+ * @param {string[]} myList 
+ */
+export function addSecretIngredient(friendsList, myList) {
+  const lastIndex = friendsList.length -1
+  myList.push(friendsList[lastIndex])
+}
+
+/**
+ * Calculates the amounts of ingredients needed for a certain 
+ * amount of portions.
+ * Assumes the original amounts were meant for 2 portions.
+ * Does not modify the original recipe.
+ * 
+ * @param {Entry<string, number>} recipe 
+ * @param number targetPortions 
+ * @returns {Entry<string, number>} recipe with amounts for target portions
+ */
+export function scaleRecipe(recipe, targetPortions) {
+  const factor = targetPortions / 2
+  const result = {}
+
+  for(const key in recipe) {
+    result[key] = recipe[key] * factor
+  }
+
+  return result
 }
