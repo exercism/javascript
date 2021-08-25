3daee464e09f770e751d3788bb39c41e77c28367
junedev <12543047+junedev@users.noreply.github.com>
2021-08-25 12:04:58 +0200

    fix stub and tests

--- exercises/concept/errors/errors.js
+++ exercises/concept/errors/errors.js
@@ -1,8 +1,4 @@
 // @ts-check
-//
-// The line above enables type checking for this file. Various IDEs interpret
-// the @ts-check directive. It will give you helpful autocompletion when
-// implementing this exercise.
 
 export class ArgumentError extends Error {}
 
@@ -14,33 +10,35 @@ export class OverheatingError extends Error {
 }
 
 /**
- * Check if the humidity level is high enough
+ * Check if the humidity level is not too high.
  *
- * @param {number|null} humidityPercentage
- *
+ * @param {number} humidityPercentage
+ * @throws {Error}
  */
 export function checkHumidityLevel(humidityPercentage) {
   throw new Error('Implement the checkHumidity function');
 }
 
 /**
- * Check if the temperature is not too high
+ * Check if the temperature is not too high.
  *
  * @param {number|null} temperature
- *
+ * @throws {ArgumentError|OverheatingError}
  */
 export function reportOverheating(temperature) {
   throw new Error('Implement the reportOverheating function');
 }
 
 /**
- *
+ *  Triggers the needed action depending on the result of the machine check.
+ * 
  * @param {{
  * check: function,
  * alertDeadSensor: function,
  * alertOverheating: function,
  * shutdown: function
  * }} actions
+ * @throws {ArgumentError|OverheatingError|Error}
  */
 export function monitorTheMachine(actions) {
   throw new Error('Implement the monitorTheMachine function');
