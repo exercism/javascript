3d2858d83fdb3dff4d5405625aa88292c40f667c
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2021-09-05 04:25:44 +0200

    Move prettier check to bin file and extract when not present

--- bin/format.sh
+++ bin/format.sh
@@ -1,9 +1,27 @@
 #!/usr/bin/env bash
 
 if [ -z "$EXERCISM_PRETTIER_VERSION" ]; then
-	echo "This script requires the EXERCISM_PRETTIER_VERSION variable to work."
-	echo "Please see https://github.com/exercism/v3/blob/master/docs/maintainers/style-guide.md for guidance."
-	exit 1
+  echo "Pulling prettier version from package.json"
+  EXERCISM_PRETTIER_VERSION=$(npm list prettier | grep -Po '.*\sprettier@\K.*')
+fi
+
+if [ -z "$EXERCISM_PRETTIER_VERSION" ]; then
+  echo "---------------------------------------------------"
+  echo "This script requires the EXERCISM_PRETTIER_VERSION variable to work."
+  echo "Please see https://exercism.org/docs/building/markdown/style-guide for guidance."
+  echo "---------------------------------------------------"
+  echo "This is what npm list reports:"
+  echo "$(npm list prettier)"
+  echo ""
+  echo "This is the version that can be extracted:"
+  echo "$(npm list prettier | grep -Po '.*\sprettier@\K.*')"
+  echo ""
+  echo "These files are found in the repo root:"
+  echo "$(ls -p | grep -v /)"
+  echo "---------------------------------------------------"
+  exit 1
+else
+ echo "Running format with prettier@$EXERCISM_PRETTIER_VERSION"
 fi
 
 npx "prettier@$EXERCISM_PRETTIER_VERSION" --write "**/*.{js,jsx,ts,tsx,css,sass,scss,html,json,md,yml}"
