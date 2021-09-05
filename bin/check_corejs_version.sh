62e617758e01924e95293a1b71c5a00bb81a1f37
Florian Imdahl <git@ffflorian.de>
2021-09-05 21:38:21 +0200

    feat: Check corejs version in babel configuration versus package.json

---
+++ bin/check_corejs_version.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+babel_version="$(sed -n "s/.*corejs: '\([^\\']*\)'.*/\1/p" babel.config.js)"
+
+IFS='.' read -r -a babel_arr <<< "$babel_version"
+
+package_version="$(sed -n 's/.*"core-js": "\^\?\([^\"]*\)".*/\1/p' package.json)"
+
+IFS='.' read -r -a package_arr <<< "$package_version"
+
+if [ ${babel_arr[0]} != ${package_arr[0]} ] || [ ${babel_arr[1]} != ${package_arr[1]} ]; then
+  echo "core-js version in package.json does not match the version in babel.config.js!"
+  echo "babel.config.js version: ${babel_version}"
+  echo "package.json version: ${package_version}"
+  exit 1
+fi
