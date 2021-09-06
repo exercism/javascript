4f3213ff6a8d0a6314a798a296bd4fed9c199c24
Florian Imdahl <git@ffflorian.de>
2021-09-06 12:47:32 +0200

    improve regex

--- bin/check_corejs_version.sh
+++ bin/check_corejs_version.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-babel_version="$(sed -n "s/.*corejs: '\([0-9]\+\)\.\([0-9]\+\).*/\1.\2/p" babel.config.js)"
-package_version="$(sed -n 's/.*"core-js": "^\?\([0-9]\+\)\.\([0-9]\+\).*/\1.\2/p' package.json)"
+babel_version="$(sed -n "s/.*corejs: '\([0-9]\+\.[0-9]\+\).*/\1/p" babel.config.js)"
+package_version="$(sed -n 's/.*"core-js": "^\?\([0-9]\+\.[0-9]\+\).*/\1/p' package.json)"
 
 if [ ${babel_version} != ${package_version} ]; then
   echo "core-js version (major.minor) in package.json does not match the version in babel.config.js!"
