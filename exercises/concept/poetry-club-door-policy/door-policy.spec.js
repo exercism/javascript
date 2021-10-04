2637c9d3d32ec3de0c53d4c7aaa958ff5f1e26a8
Rob Styles <rob@styles.to>
2021-10-04 13:03:25 +0100

    Seperate .each tests, fix whitespace in SHIRE_HORSE_WITH_SPACES, break into describe blocks

--- exercises/concept/poetry-club-door-policy/door-policy.spec.js
+++ exercises/concept/poetry-club-door-policy/door-policy.spec.js
@@ -1,125 +1,142 @@
-/** @format */
-
 // @ts-check
 
 import {
-  frontDoorResponse as frontDoorPatron,
-  backDoorResponse as backDoorPatron,
+  frontDoorResponse,
+  backDoorResponse,
   frontDoorPassword,
   backDoorPassword,
 } from './door-policy';
 
-const recite = (poem, patron) => {
-  return poem.map((line) => patron(line));
+const recite = (poem, responseFn) => {
+  return poem.map((line) => responseFn(line));
 };
 
 //Shire Horse, by Michael Lockwood
-const SHIRE_HORSE = `
-Stands so high
-Huge hooves too
-Impatiently waits for
-Reins and harness
-Eager to leave
-`
-  .trim()
-  .split('\n');
+const SHIRE_HORSE = [
+  'Stands so high',
+  'Huge hooves too',
+  'Impatiently waits for',
+  'Reins and harness',
+  'Eager to leave',
+];
 
 //Shire Horse, by Michael Lockwood
-const SHIRE_HORSE_WITH_SPACES = `
-Stands so high
-Huge hooves too
-Impatiently waits for
-Reins and harness
-Eager to leave
-`.split('\n');
+//with leading and trailing whitespace
+const SHIRE_HORSE_WITH_SPACES = [
+  '  Stands so high',
+  '  Huge hooves too  ',
+  '    Impatiently waits for',
+  ' Reins and harness   ',
+  '         Eager to leave  ',
+];
 
 //Summer, by John Albert Caballero
-const SUMMER = `
-Sunshine warming my toes,
-Underwater fun with my friends.
-Making homemade ice cream on the porch,
-Many long nights catching fireflies.
-Early morning walks to the creek,
-Reveling in the freedom of lazy days.
-`.split('\n');
+const SUMMER = [
+  'Sunshine warming my toes,',
+  'Underwater fun with my friends.',
+  'Making homemade ice cream on the porch,',
+  'Many long nights catching fireflies.',
+  'Early morning walks to the creek,',
+  'Reveling in the freedom of lazy days.',
+];
 
 //Sophia, by John Albert Caballero
-const SOPHIA = `
-Serene, calming quality
-Organized, you always have it together
-Picturesque, strikingly beautiful
-Honest, so genuine
-Imaginative, a creative mind
-Alluring, so attractive
-`
-  .trim()
-  .split('\n');
+const SOPHIA = [
+  'Serene, calming quality',
+  'Organized, you always have it together',
+  'Picturesque, strikingly beautiful',
+  'Honest, so genuine',
+  'Imaginative, a creative mind',
+  'Alluring, so attractive',
+];
 
 //Code Work, by Derk-Jan Karrenbeld
-const CODE_WORK = `
-Compilers intensily bestow
-On commencing without ego
-Different processes ajar
-Exit with zero quick
-`
-  .trim()
-  .split('\n');
+const CODE_WORK = [
+  'Compilers intensily bestow',
+  'On commencing without ego',
+  'Different processes ajar',
+  'Exit with zero quick',
+];
 
-describe('strings', () => {
-  describe('front door response', () => {
-    test('should output a character per line', () => {
-      const key = recite(SHIRE_HORSE, frontDoorPatron);
-      expect(key.length).toBe(SHIRE_HORSE.length);
-    });
+describe('front door response', () => {
+  test('should output a character per line', () => {
+    const expectedLetters = recite(SHIRE_HORSE, frontDoorResponse);
+    expect(expectedLetters.length).toBe(SHIRE_HORSE.length);
+  });
 
-    test('should take the first character of each line', () => {
-      const key = recite(SHIRE_HORSE, frontDoorPatron);
-      const expectedFirstLetters = ['S', 'H', 'I', 'R', 'E'];
-      expect(key).toEqual(expectedFirstLetters);
-    });
-
-    test('should generate the correct password', () => {
-      expect(frontDoorPassword('SHIRE')).toBe('Shire');
-    });
-
-    test.each([
-      { poem: SUMMER, firstLetters: 'SUMMER', password: 'Summer' },
-      { poem: SOPHIA, firstLetters: 'SOPHIA', password: 'Sophia' },
-      { poem: CODE_WORK, firstLetters: 'CODE', password: 'Code' },
-    ])(
-      'should be correct for $password',
-      ({ poem, firstLetters, password }) => {
-        expect(recite(poem, frontDoorPatron).join('')).toBe(firstLetters);
-        expect(frontDoorPassword(firstLetters)).toBe(password);
-      }
+  xtest('should take the first character of each line', () => {
+    const expectedLetters = ['S', 'H', 'I', 'R', 'E'];
+    expect(recite(SHIRE_HORSE, frontDoorResponse)).toStrictEqual(
+      expectedLetters
     );
   });
 
-  describe('back door response', () => {
-    test('should output a character per line', () => {
-      const key = recite(SHIRE_HORSE, backDoorPatron);
-      expect(key.length).toBe(SHIRE_HORSE.length);
-    });
+  xtest('should take the first characters from SUMMER', () => {
+    const expectedLetters = ['S', 'U', 'M', 'M', 'E', 'R'];
+    expect(recite(SUMMER, frontDoorResponse)).toStrictEqual(expectedLetters);
+  });
 
-    test('should take the last letter character of each line', () => {
-      const key = recite(SHIRE_HORSE, backDoorPatron);
-      expect(key).toEqual(['h', 'o', 'r', 's', 'e']);
-    });
+  xtest('should take the first characters from SOPHIA', () => {
+    const expectedLetters = ['S', 'O', 'P', 'H', 'I', 'A'];
+    expect(recite(SOPHIA, frontDoorResponse)).toStrictEqual(expectedLetters);
+  });
 
-    test('should generate the correct password', () => {
-      expect(backDoorPassword('horse')).toBe('Horse, please');
-    });
+  xtest('should take the first characters from CODE', () => {
+    const expectedLetters = ['C', 'O', 'D', 'E'];
+    expect(recite(CODE_WORK, frontDoorResponse)).toStrictEqual(expectedLetters);
+  });
 
-    test.each([
-      { poem: CODE_WORK, lastLetters: 'work', password: 'Work, please' },
-      {
-        poem: SHIRE_HORSE_WITH_SPACES,
-        lastLetters: 'horse',
-        password: 'Horse, please',
-      },
-    ])('should be correct for $password', ({ poem, lastLetters, password }) => {
-      expect(recite(poem, backDoorPatron).join('')).toBe(lastLetters);
-      expect(backDoorPassword(lastLetters)).toBe(password);
-    });
+  xtest('should ignore whitespace when taking first characters', () => {
+    const expectedLetters = ['S', 'H', 'I', 'R', 'E'];
+    expect(recite(SHIRE_HORSE_WITH_SPACES, frontDoorResponse)).toStrictEqual(
+      expectedLetters
+    );
+  });
+});
+
+describe('front door password', () => {
+  xtest('should title case SHIRE', () => {
+    expect(frontDoorPassword('SHIRE')).toBe('Shire');
+  });
+
+  xtest('should title case SUMMER', () => {
+    expect(frontDoorPassword('SUMMER')).toBe('Summer');
+  });
+
+  xtest('should title case SOPHIA', () => {
+    expect(frontDoorPassword('SOPHIA')).toBe('Sophia');
+  });
+
+  xtest('should title case CODE', () => {
+    expect(frontDoorPassword('CODE')).toBe('Code');
+  });
+});
+
+describe('back door response', () => {
+  xtest('should output a character per line', () => {
+    const actualLetters = recite(SHIRE_HORSE, backDoorResponse);
+    expect(actualLetters.length).toBe(SHIRE_HORSE.length);
+  });
+
+  xtest('should take the last letter character of each line of SHIRE_HORSE', () => {
+    const actualLetters = recite(SHIRE_HORSE, backDoorResponse);
+    const expectedLetters = ['h', 'o', 'r', 's', 'e'];
+    expect(actualLetters).toStrictEqual(expectedLetters);
+  });
+
+  xtest('should take the last letter character of each line of CODE_WORK', () => {
+    const actualLetters = recite(CODE_WORK, backDoorResponse);
+    const expectedLetters = ['w', 'o', 'r', 'k'];
+    expect(actualLetters).toStrictEqual(expectedLetters);
+  });
+});
+
+describe('back door password', () => {
+  xtest("should generate the correct pass phrase from 'horse'", () => {
+    expect(backDoorPassword('horse')).toBe('Horse, please');
+  });
+
+  xtest("should generate the correct pass phrase from 'work'", () => {
+    expect(backDoorPassword('work')).toBe('Work, please');
   });
 });
