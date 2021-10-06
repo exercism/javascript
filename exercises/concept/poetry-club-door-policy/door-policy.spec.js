f9039a517d6f2ad5c7a0a7deddedafd1c846c549
junedev <12543047+junedev@users.noreply.github.com>
2021-10-06 21:03:34 +0200

    adapt tests for better website output

--- exercises/concept/poetry-club-door-policy/door-policy.spec.js
+++ exercises/concept/poetry-club-door-policy/door-policy.spec.js
@@ -11,60 +11,31 @@ const recite = (poem, responseFn) => {
   return poem.map((line) => responseFn(line));
 };
 
-//Shire Horse, by Michael Lockwood
-const SHIRE_HORSE = [
-  'Stands so high',
-  'Huge hooves too',
-  'Impatiently waits for',
-  'Reins and harness',
-  'Eager to leave',
-];
-
-//Shire Horse, by Michael Lockwood
-//with leading and trailing whitespace
-const SHIRE_HORSE_WITH_SPACES = [
-  'Stands so high   ',
-  'Huge hooves too  ',
-  'Impatiently waits for      ',
-  'Reins and harness    ',
-  'Eager to leave   ',
-];
-
-//Summer, by John Albert Caballero
-const SUMMER = [
-  'Sunshine warming my toes,',
-  'Underwater fun with my friends.',
-  'Making homemade ice cream on the porch,',
-  'Many long nights catching fireflies.',
-  'Early morning walks to the creek,',
-  'Reveling in the freedom of lazy days.',
-];
-
-//Sophia, by John Albert Caballero
-const SOPHIA = [
-  'Serene, calming quality',
-  'Organized, you always have it together',
-  'Picturesque, strikingly beautiful',
-  'Honest, so genuine',
-  'Imaginative, a creative mind',
-  'Alluring, so attractive',
-];
-
-//Code Work, by Derk-Jan Karrenbeld
-const CODE_WORK = [
-  'Compilers intensily bestow',
-  'On commencing without ego',
-  'Different processes ajar',
-  'Exit with zero quick',
-];
-
 describe('front door response', () => {
   test('should output a character per line', () => {
+    // Shire Horse, by Michael Lockwood
+    const SHIRE_HORSE = [
+      'Stands so high',
+      'Huge hooves too',
+      'Impatiently waits for',
+      'Reins and harness',
+      'Eager to leave',
+    ];
+
     const expectedLetters = recite(SHIRE_HORSE, frontDoorResponse);
     expect(expectedLetters.length).toBe(SHIRE_HORSE.length);
   });
 
   test('should take the first character of each line', () => {
+    // Shire Horse, by Michael Lockwood
+    const SHIRE_HORSE = [
+      'Stands so high',
+      'Huge hooves too',
+      'Impatiently waits for',
+      'Reins and harness',
+      'Eager to leave',
+    ];
+
     const expectedLetters = ['S', 'H', 'I', 'R', 'E'];
     expect(recite(SHIRE_HORSE, frontDoorResponse)).toStrictEqual(
       expectedLetters
@@ -72,16 +43,44 @@ describe('front door response', () => {
   });
 
   test('should take the first characters from SUMMER', () => {
+    // Summer, by John Albert Caballero
+    const SUMMER = [
+      'Sunshine warming my toes,',
+      'Underwater fun with my friends.',
+      'Making homemade ice cream on the porch,',
+      'Many long nights catching fireflies.',
+      'Early morning walks to the creek,',
+      'Reveling in the freedom of lazy days.',
+    ];
+
     const expectedLetters = ['S', 'U', 'M', 'M', 'E', 'R'];
     expect(recite(SUMMER, frontDoorResponse)).toStrictEqual(expectedLetters);
   });
 
   test('should take the first characters from SOPHIA', () => {
+    // Sophia, by John Albert Caballero
+    const SOPHIA = [
+      'Serene, calming quality',
+      'Organized, you always have it together',
+      'Picturesque, strikingly beautiful',
+      'Honest, so genuine',
+      'Imaginative, a creative mind',
+      'Alluring, so attractive',
+    ];
+
     const expectedLetters = ['S', 'O', 'P', 'H', 'I', 'A'];
     expect(recite(SOPHIA, frontDoorResponse)).toStrictEqual(expectedLetters);
   });
 
   test('should take the first characters from CODE', () => {
+    // Code Work, by Derk-Jan Karrenbeld
+    const CODE_WORK = [
+      'Compilers intensily bestow',
+      'On commencing without ego',
+      'Different processes ajar',
+      'Exit with zero quick',
+    ];
+
     const expectedLetters = ['C', 'O', 'D', 'E'];
     expect(recite(CODE_WORK, frontDoorResponse)).toStrictEqual(expectedLetters);
   });
@@ -107,23 +106,59 @@ describe('front door password', () => {
 
 describe('back door response', () => {
   test('should output a character per line', () => {
+    // Shire Horse, by Michael Lockwood
+    const SHIRE_HORSE = [
+      'Stands so high',
+      'Huge hooves too',
+      'Impatiently waits for',
+      'Reins and harness',
+      'Eager to leave',
+    ];
+
     const actualLetters = recite(SHIRE_HORSE, backDoorResponse);
     expect(actualLetters.length).toBe(SHIRE_HORSE.length);
   });
 
   test('should take the last letter character of each line of SHIRE_HORSE', () => {
+    // Shire Horse, by Michael Lockwood
+    const SHIRE_HORSE = [
+      'Stands so high',
+      'Huge hooves too',
+      'Impatiently waits for',
+      'Reins and harness',
+      'Eager to leave',
+    ];
+
     const actualLetters = recite(SHIRE_HORSE, backDoorResponse);
     const expectedLetters = ['h', 'o', 'r', 's', 'e'];
     expect(actualLetters).toStrictEqual(expectedLetters);
   });
 
   test('should igniore whitespace when taking the last letter character of each line of SHIRE_HORSE_WITH_SPACES', () => {
+    // Shire Horse, by Michael Lockwood
+    // with trailing whitespace
+    const SHIRE_HORSE_WITH_SPACES = [
+      'Stands so high   ',
+      'Huge hooves too\t',
+      'Impatiently waits for \t \t',
+      'Reins and harness    ',
+      'Eager to leave   ',
+    ];
+
     const actualLetters = recite(SHIRE_HORSE_WITH_SPACES, backDoorResponse);
     const expectedLetters = ['h', 'o', 'r', 's', 'e'];
     expect(actualLetters).toStrictEqual(expectedLetters);
   });
 
   test('should take the last letter character of each line of CODE_WORK', () => {
+    // Code Work, by Derk-Jan Karrenbeld
+    const CODE_WORK = [
+      'Compilers intensily bestow',
+      'On commencing without ego',
+      'Different processes ajar',
+      'Exit with zero quick',
+    ];
+
     const actualLetters = recite(CODE_WORK, backDoorResponse);
     const expectedLetters = ['w', 'o', 'r', 'k'];
     expect(actualLetters).toStrictEqual(expectedLetters);
