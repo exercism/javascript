d4383de1534e4712e819e959d0a03e70564b0e69
Ramón Huidobro <hola@ramonh.dev>
2021-10-21 21:20:37 +0200

    Fix wording

--- exercises/concept/lasagna-master/lasagna-master.spec.js
+++ exercises/concept/lasagna-master/lasagna-master.spec.js
@@ -50,7 +50,7 @@ describe('preparationTime', () => {
     expect(preparationTime(fewLayers, 3.5)).toBe(7);
   });
 
-  test('uses the default if no custom was passed', () => {
+  test('uses the default if no custom time was passed', () => {
     const manyLayers = [
       'sauce',
       'noodles',
