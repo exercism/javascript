47333f2302b461ffddfc0b270a0e22d197c277a4
junedev <12543047+junedev@users.noreply.github.com>
2021-10-23 14:32:01 +0200

    add more test cases

--- exercises/concept/custom-signs/custom-signs.spec.js
+++ exercises/concept/custom-signs/custom-signs.spec.js
@@ -11,6 +11,7 @@ describe('buildSign', () => {
   test('occasion is Birthday', () => {
     expect(buildSign('Birthday', 'Jack')).toBe('Happy Birthday Jack!');
   });
+
   test('occasion is Anniversary', () => {
     expect(buildSign('Anniversary', 'Jill')).toBe('Happy Anniversary Jill!');
   });
@@ -22,8 +23,15 @@ describe('buildBirthdaySign', () => {
       'Happy Birthday! What a young fellow you are.'
     );
   });
+
   test('age is 50 or older', () => {
     expect(buildBirthdaySign(51)).toBe(
+      'Happy Birthday! What a old fellow you are.'
+    );
+  });
+
+  test('age is 50', () => {
+    expect(buildBirthdaySign(50)).toBe(
       'Happy Birthday! What a old fellow you are.'
     );
   });
@@ -32,15 +40,33 @@ describe('buildBirthdaySign', () => {
 describe('graduationFor', () => {
   const expected = `Congratulations Rob
   Class of 2021`;
-  test('year of graduation is 2021', () => {
+  test('Robs graduation, 2021', () => {
     expect(graduationFor('Rob', 2021)).toBe(expected);
+  });
+
+  test('Jills graduation, 1999', () => {
+    const expected = `Congratulations Jill
+    Class of 1999`;
+    expect(graduationFor('Jill', 1999)).toBe(expected);
   });
 });
 
 describe('costOf', () => {
   test('sign is total of characters followed by the currency', () => {
-    expect(costOf(graduationFor('Rob', 2021), 'dollars')).toBe(
-      'Your sign cost 90.00 dollars'
-    );
+    const sign = 'Happy Birthday!';
+    const expected = 'Your sign cost 50.00 dollars';
+    expect(costOf(sign, 'dollars')).toBe(expected);
+  });
+
+  test('includes line breaks in the calculation', () => {
+    const sign = 'Congratulations Rob\nClass of 2021';
+    const expected = 'Your sign cost 86.00 dollars';
+    expect(costOf(sign, 'dollars')).toBe(expected);
+  });
+
+  test('handles different currency arguments', () => {
+    const sign = 'Happy Easter, little sister!';
+    const expected = 'Your sign cost 76.00 euros.';
+    expect(costOf(sign, 'euros')).toBe(expected);
   });
 });
