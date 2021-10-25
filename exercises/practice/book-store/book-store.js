c99304c15aaa433899aec1d71c1aa3e232f7f433
Kmelow <camilo.c@protonmail.com>
2021-10-25 23:09:49 +0200

    Add practice exercise book store (#1426)

---
+++ exercises/practice/book-store/book-store.js
@@ -0,0 +1,8 @@
+//
+// This is only a SKELETON file for the 'BookStore' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const cost = (books) => {
+  throw new Error('Remove this statement and implement this function');
+};
