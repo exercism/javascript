005eac176735f5690213023c30fa4bf3d86c795d
Camilo Coelho <camilo.c@protonmail.com>
2021-10-31 18:33:11 +0100

    Exercise files

---
+++ exercises/practice/markdown/markdown.js
@@ -0,0 +1,68 @@
+//
+// This is only a SKELETON file for the 'Matrix' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const parse = (markdown) => {
+  if (markdown.includes(' __')) {
+    markdown = markdown.replace(' __', ' <strong>');
+  }
+  if (markdown.includes(' _')) {
+    markdown = markdown.replace(' _', ' <em>');
+  }
+
+  if (markdown.includes('__ ')) {
+    markdown = markdown.replace('__ ', '</strong> ');
+  }
+  if (markdown.includes('_ ')) {
+    markdown = markdown.replace('_ ', '</em> ');
+  }
+
+  if (markdown.startsWith('__')) {
+    markdown = markdown.replace('__', '<strong>');
+  }
+  if (markdown.endsWith('__')) {
+    markdown = markdown.replace('__', '</strong>');
+  }
+
+  if (markdown.startsWith('_')) {
+    markdown = markdown.replace('_', '<em>');
+  }
+  if (markdown.endsWith('_')) {
+    markdown = markdown.replace('_', '</em>');
+  }
+
+  if (markdown.startsWith('# ')) {
+    markdown = markdown.replace('# ', '<h1>');
+    if (markdown.match(/\n\*/gm)) {
+      markdown = markdown.replace('\n* ', '</h1><ul><li>');
+      markdown = markdown.replaceAll('\n* ', '</li><li>');
+      markdown = markdown.replace('\n', '</li></ul><p>');
+      markdown = markdown + '</p>';
+    } else {
+      markdown = markdown + '</h1>';
+    }
+  } else if (markdown.startsWith('## ')) {
+    markdown = markdown.replace('## ', '<h2>') + '</h2>';
+  } else if (markdown.startsWith('### ')) {
+    markdown = markdown.replace('### ', '<h3>') + '</h3>';
+  } else if (markdown.startsWith('#### ')) {
+    markdown = markdown.replace('#### ', '<h4>') + '</h4>';
+  } else if (markdown.startsWith('##### ')) {
+    markdown = markdown.replace('##### ', '<h5>') + '</h5>';
+  } else if (markdown.startsWith('###### ')) {
+    markdown = markdown.replace('###### ', '<h6>') + '</h6>';
+  } else if (markdown.startsWith('* ')) {
+    markdown = markdown.replace('* ', '<ul><li>');
+    markdown = markdown.replaceAll('\n* ', '</li><li>');
+    markdown = markdown + '</li></ul>';
+  } else {
+    markdown = `<p>${markdown}</p>`;
+  }
+
+  markdown = markdown.replace('\n* ', '<ul><li>');
+  markdown = markdown.replaceAll('\n* ', '</li><li>');
+  markdown = markdown.replace('\n', '<ul><li>');
+
+  return markdown;
+};
