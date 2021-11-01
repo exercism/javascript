a7f919844c8d57667130100bd4213281e93a183f
Luca Pizzini <lpizzini7@gmail.com>
2021-11-01 21:46:53 +0100

    fix: removed unnecessary class from nucleotide count (#1488)

--- exercises/practice/nucleotide-count/.meta/proof.ci.js
+++ exercises/practice/nucleotide-count/.meta/proof.ci.js
@@ -1,15 +1,13 @@
 const count = (str, nuc) =>
   [...str].filter((nucleotide) => nucleotide === nuc).length;
 
-export class NucleotideCounts {
-  static parse(strand) {
-    if (strand.replace(/A|C|G|T/g, '').length) {
-      throw new Error('Invalid nucleotide in strand');
-    } else {
-      return `${count(strand, 'A')} ${count(strand, 'C')} ${count(
-        strand,
-        'G'
-      )} ${count(strand, 'T')}`;
-    }
+export function countNucleotides(strand) {
+  if (strand.replace(/A|C|G|T/g, '').length) {
+    throw new Error('Invalid nucleotide in strand');
   }
+
+  return `${count(strand, 'A')} ${count(strand, 'C')} ${count(
+    strand,
+    'G'
+  )} ${count(strand, 'T')}`;
 }
