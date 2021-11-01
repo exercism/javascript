a64d589b5d4e59f7d5a8342461c9a637fc5ad455
Pizzo15 <lpizzini7@gmail.com>
2021-11-01 15:50:51 +0100

    fix: removed unnecessary class from nucleotide count

--- exercises/practice/nucleotide-count/nucleotide-count.spec.js
+++ exercises/practice/nucleotide-count/nucleotide-count.spec.js
@@ -1,28 +1,28 @@
-import { NucleotideCounts } from './nucleotide-count';
+import { nucleotideCounts } from './nucleotide-count';
 
 describe('count all nucleotides in a strand', () => {
   test('empty strand', () => {
-    expect(NucleotideCounts.parse('')).toEqual('0 0 0 0');
+    expect(nucleotideCounts('')).toEqual('0 0 0 0');
   });
 
   xtest('can count one nucleotide in single-character input', () => {
-    expect(NucleotideCounts.parse('G')).toEqual('0 0 1 0');
+    expect(nucleotideCounts('G')).toEqual('0 0 1 0');
   });
 
   xtest('strand with repeated nucleotide', () => {
-    expect(NucleotideCounts.parse('GGGGGGG')).toEqual('0 0 7 0');
+    expect(nucleotideCounts('GGGGGGG')).toEqual('0 0 7 0');
   });
 
   xtest('strand with multiple nucleotides', () => {
     expect(
-      NucleotideCounts.parse(
+      nucleotideCounts(
         'AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC'
       )
     ).toEqual('20 12 17 21');
   });
 
   xtest('strand with invalid nucleotides', () => {
-    expect(() => NucleotideCounts.parse('AGXXACT')).toThrow(
+    expect(() => nucleotideCounts('AGXXACT')).toThrow(
       new Error('Invalid nucleotide in strand')
     );
   });
