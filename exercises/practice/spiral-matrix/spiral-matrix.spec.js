14aa00c745390ac8b5260169eb78ef2cfc674013
Rugved Tipare <73734376+codeiotic@users.noreply.github.com>
2021-11-13 17:01:43 +0530

    Update spiral-matrix.spec.js

--- exercises/practice/spiral-matrix/spiral-matrix.spec.js
+++ exercises/practice/spiral-matrix/spiral-matrix.spec.js
@@ -1,16 +1,16 @@
-import { SpiralMatrix } from './spiral-matrix';
+import { spiralMatrix } from './spiral-matrix';
 
 describe('Spiral Matrix', () => {
   test('empty spiral', () => {
     const expected = [];
-    const actual = SpiralMatrix.ofSize(0);
+    const actual = spiralMatrix(0);
 
     expect(actual).toEqual(expected);
   });
 
   xtest('trivial spiral', () => {
     const expected = [[1]];
-    const actual = SpiralMatrix.ofSize(1);
+    const actual = spiralMatrix(1);
 
     expect(actual).toEqual(expected);
   });
@@ -20,7 +20,7 @@ describe('Spiral Matrix', () => {
       [1, 2],
       [4, 3],
     ];
-    const actual = SpiralMatrix.ofSize(2);
+    const actual = spiralMatrix(2);
 
     expect(actual).toEqual(expected);
   });
@@ -31,7 +31,7 @@ describe('Spiral Matrix', () => {
       [8, 9, 4],
       [7, 6, 5],
     ];
-    const actual = SpiralMatrix.ofSize(3);
+    const actual = spiralMatrix(3);
 
     expect(actual).toEqual(expected);
   });
@@ -43,7 +43,7 @@ describe('Spiral Matrix', () => {
       [11, 16, 15, 6],
       [10, 9, 8, 7],
     ];
-    const actual = SpiralMatrix.ofSize(4);
+    const actual = spiralMatrix(4);
 
     expect(actual).toEqual(expected);
   });
@@ -56,7 +56,7 @@ describe('Spiral Matrix', () => {
       [14, 23, 22, 21, 8],
       [13, 12, 11, 10, 9],
     ];
-    const actual = SpiralMatrix.ofSize(5);
+    const actual = spiralMatrix(5);
 
     expect(expected).toEqual(actual);
   });
