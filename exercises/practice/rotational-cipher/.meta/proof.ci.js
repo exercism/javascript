7403780fbcc64b1999e367cd3461f4cacb3024b6
Kerollos Magdy <kerolloz@yahoo.com>
2021-11-13 16:27:33 +0000

    fix: remove unnessary class in rotational-cipher
    fixes #1511

--- exercises/practice/rotational-cipher/.meta/proof.ci.js
+++ exercises/practice/rotational-cipher/.meta/proof.ci.js
@@ -1,17 +1,16 @@
-export class RotationalCipher {
-  static rotate(text, shift) {
-    return [...text]
-      .map((c) => {
-        const isUpper = c.match(/[A-Z]/);
-        const isAlpha = c.match(/[a-z]/i);
-        const charShift = (isUpper ? 'A' : 'a').charCodeAt(0);
+export function rotate(text, shift) {
+  return [...text]
+    .map((c) => {
+      const isUpper = c.match(/[A-Z]/);
+      const isAlpha = c.match(/[a-z]/i);
+      const charShift = (isUpper ? 'A' : 'a').charCodeAt(0);
 
-        return isAlpha
-          ? String.fromCharCode(
-              ((c.charCodeAt(0) - charShift + shift) % 26) + charShift
-            )
-          : c;
-      })
-      .join('');
-  }
+      return isAlpha
+        ? String.fromCharCode(
+            ((c.charCodeAt(0) - charShift + shift) % 26) + charShift
+          )
+        : c;
+    })
+    .join('');
 }
+
