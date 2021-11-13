709f1d4dbe59b5f1f27218dc16bcd3a421bc5730
Rugved Tipare <73734376+codeiotic@users.noreply.github.com>
2021-11-13 17:04:48 +0530

    Update proof.ci.js

--- exercises/practice/spiral-matrix/.meta/proof.ci.js
+++ exercises/practice/spiral-matrix/.meta/proof.ci.js
@@ -1,39 +1,37 @@
-export class SpiralMatrix {
-  static ofSize(size) {
-    const spiral = Array(size)
-      .fill()
-      .map(() => Array(0));
+export const spiralMatrix = (size) => {
+  const spiral = Array(size)
+    .fill()
+    .map(() => Array(0));
 
-    const totalNumbers = size ** 2;
-    let currentNumber = 1;
-    let topLeft = 0;
-    let bottomRight = size - 1;
+  const totalNumbers = size ** 2;
+  let currentNumber = 1;
+  let topLeft = 0;
+  let bottomRight = size - 1;
 
-    while (currentNumber <= totalNumbers) {
-      for (let x = topLeft; x <= bottomRight; x += 1) {
-        spiral[topLeft][x] = currentNumber;
-        currentNumber += 1;
-      }
-
-      for (let y = topLeft + 1; y <= bottomRight; y += 1) {
-        spiral[y][bottomRight] = currentNumber;
-        currentNumber += 1;
-      }
-
-      for (let x = bottomRight - 1; x >= topLeft; x -= 1) {
-        spiral[bottomRight][x] = currentNumber;
-        currentNumber += 1;
-      }
-
-      for (let y = bottomRight - 1; y >= topLeft + 1; y -= 1) {
-        spiral[y][topLeft] = currentNumber;
-        currentNumber += 1;
-      }
-
-      topLeft += 1;
-      bottomRight -= 1;
+  while (currentNumber <= totalNumbers) {
+    for (let x = topLeft; x <= bottomRight; x += 1) {
+      spiral[topLeft][x] = currentNumber;
+      currentNumber += 1;
     }
 
-    return spiral;
+    for (let y = topLeft + 1; y <= bottomRight; y += 1) {
+      spiral[y][bottomRight] = currentNumber;
+      currentNumber += 1;
+    }
+
+    for (let x = bottomRight - 1; x >= topLeft; x -= 1) {
+      spiral[bottomRight][x] = currentNumber;
+      currentNumber += 1;
+    }
+
+    for (let y = bottomRight - 1; y >= topLeft + 1; y -= 1) {
+      spiral[y][topLeft] = currentNumber;
+      currentNumber += 1;
+    }
+
+    topLeft += 1;
+    bottomRight -= 1;
   }
-}
+
+  return spiral;
+};
