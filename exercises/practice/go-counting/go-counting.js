bb048b0b73c704e32b384b95c3e164e37d9afdb3
Luca Pizzini <lpizzini7@gmail.com>
2021-12-19 20:41:24 +0100

    Added new exercise: Go Counting (#1559)

---
+++ exercises/practice/go-counting/go-counting.js
@@ -0,0 +1,18 @@
+//
+// This is only a SKELETON file for the 'Go Counting' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class GoCounting {
+  constructor(board) {
+    throw new Error('Remove this statement and implement this function');
+  }
+
+  getTerritory(x, y) {
+    throw new Error('Remove this statement and implement this function');
+  }
+
+  getTerritories() {
+    throw new Error('Remove this statement and implement this function');
+  }
+}
---
+++ exercises/practice/go-counting/go-counting.spec.js
@@ -0,0 +1,119 @@
+import { GoCounting } from './go-counting';
+
+describe('Go Counting', () => {
+  describe('getTerritory', () => {
+    test('Black corner territory on 5x5 board', () => {
+      const board = ['  B  ', ' B B ', 'B W B', ' W W ', '  W  '];
+      const goCounting = new GoCounting(board);
+      const expectedTerritory = {
+        owner: 'BLACK',
+        territory: [
+          [0, 0],
+          [0, 1],
+          [1, 0],
+        ],
+      };
+      expect(goCounting.getTerritory(0, 1)).toEqual(expectedTerritory);
+    });
+
+    test('White center territory on 5x5 board', () => {
+      const board = ['  B  ', ' B B ', 'B W B', ' W W ', '  W  '];
+      const goCounting = new GoCounting(board);
+      const expectedTerritory = { owner: 'WHITE', territory: [[2, 3]] };
+      expect(goCounting.getTerritory(2, 3)).toEqual(expectedTerritory);
+    });
+
+    test('Open corner territory on 5x5 board', () => {
+      const board = ['  B  ', ' B B ', 'B W B', ' W W ', '  W  '];
+      const goCounting = new GoCounting(board);
+      const expectedTerritory = {
+        owner: 'NONE',
+        territory: [
+          [0, 3],
+          [0, 4],
+          [1, 4],
+        ],
+      };
+      expect(goCounting.getTerritory(1, 4)).toEqual(expectedTerritory);
+    });
+
+    test('A stone and not a territory on 5x5 board', () => {
+      const board = ['  B  ', ' B B ', 'B W B', ' W W ', '  W  '];
+      const goCounting = new GoCounting(board);
+      const expectedTerritory = { owner: 'NONE', territory: [] };
+      expect(goCounting.getTerritory(1, 1)).toEqual(expectedTerritory);
+    });
+
+    test('Invalid because X is too low for 5x5 board', () => {
+      const board = ['  B  ', ' B B ', 'B W B', ' W W ', '  W  '];
+      const goCounting = new GoCounting(board);
+      const expectedTerritory = { error: 'Invalid coordinate' };
+      expect(goCounting.getTerritory(-1, 1)).toEqual(expectedTerritory);
+    });
+
+    test('Invalid because X is too high for 5x5 board', () => {
+      const board = ['  B  ', ' B B ', 'B W B', ' W W ', '  W  '];
+      const goCounting = new GoCounting(board);
+      const expectedTerritory = { error: 'Invalid coordinate' };
+      expect(goCounting.getTerritory(5, 1)).toEqual(expectedTerritory);
+    });
+
+    test('Invalid because Y is too low for 5x5 board', () => {
+      const board = ['  B  ', ' B B ', 'B W B', ' W W ', '  W  '];
+      const goCounting = new GoCounting(board);
+      const expectedTerritory = { error: 'Invalid coordinate' };
+      expect(goCounting.getTerritory(1, -1)).toEqual(expectedTerritory);
+    });
+
+    test('Invalid because Y is too high for 5x5 board', () => {
+      const board = ['  B  ', ' B B ', 'B W B', ' W W ', '  W  '];
+      const goCounting = new GoCounting(board);
+      const expectedTerritory = { error: 'Invalid coordinate' };
+      expect(goCounting.getTerritory(1, 5)).toEqual(expectedTerritory);
+    });
+  });
+
+  describe('getTerritories', () => {
+    test('One territory is the whole board', () => {
+      const board = [' '];
+      const goCounting = new GoCounting(board);
+      const expectedTerritories = {
+        territoryBlack: [],
+        territoryWhite: [],
+        territoryNone: [[0, 0]],
+      };
+      expect(goCounting.getTerritories()).toEqual(expectedTerritories);
+    });
+
+    test('Two territory rectangular board', () => {
+      const board = [' BW ', ' BW '];
+      const goCounting = new GoCounting(board);
+      const expectedTerritories = {
+        territoryBlack: [
+          [0, 0],
+          [0, 1],
+        ],
+        territoryWhite: [
+          [3, 0],
+          [3, 1],
+        ],
+        territoryNone: [],
+      };
+      expect(goCounting.getTerritories()).toEqual(expectedTerritories);
+    });
+
+    test('Two region rectangular board', () => {
+      const board = [' B '];
+      const goCounting = new GoCounting(board);
+      const expectedTerritories = {
+        territoryBlack: [
+          [0, 0],
+          [2, 0],
+        ],
+        territoryWhite: [],
+        territoryNone: [],
+      };
+      expect(goCounting.getTerritories()).toEqual(expectedTerritories);
+    });
+  });
+});
