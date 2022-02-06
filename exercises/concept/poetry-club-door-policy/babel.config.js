97c8f4b88fa0db7256feda59e8b0743ce0c0fb8b
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2022-02-06 20:36:08 +0100

    Sync babel changes to all exercises

--- exercises/concept/poetry-club-door-policy/babel.config.js
+++ exercises/concept/poetry-club-door-policy/babel.config.js
@@ -1,15 +1,4 @@
 module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: 'current',
-        },
-        useBuiltIns: 'entry',
-        corejs: '3.21',
-      },
-    ],
-  ],
-  plugins: ['@babel/plugin-syntax-bigint'],
+  presets: ['@exercism/babel-preset-javascript'],
+  plugins: [],
 };
