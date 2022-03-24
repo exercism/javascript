c5c929c73afe5869eefe0c2ddf58fd1f11fe6d85
Niklas van Schrick <mc.taucher2003@gmail.com>
2022-03-24 22:21:38 +0100

    Fix wrong test case in Translation Service (#1698)

--- exercises/concept/translation-service/service.spec.js
+++ exercises/concept/translation-service/service.spec.js
@@ -185,7 +185,14 @@ describe('Premium service', () => {
     await expect(actual).rejects.toThrow(Error);
   });
 
-  test('it ensures the quality of the translation', async () => {
+  test('it recognizes sufficient quality', async () => {
+    const actual = service.premium('‘arlogh Qoylu’pu’?', 40);
+    const expected = 'What time is it?';
+
+    await expect(actual).resolves.toBe(expected);
+  });
+
+  test('it recognizes insufficient quality', async () => {
     const actual = service.premium('majQa’', 100);
     const expected = QualityThresholdNotMet;
 
@@ -193,11 +200,6 @@ describe('Premium service', () => {
   });
 
   test('it ensures the quality even after a request', async () => {
-    const actual = service.premium('‘arlogh Qoylu’pu’?', 40);
-    const expected = 'What time is it?';
-
-    await expect(actual).resolves.toBe(expected);
-
     const actualQuality = service.premium('‘arlogh Qoylu’pu’?', 100);
     const expectedQuality = QualityThresholdNotMet;
     await expect(actualQuality).rejects.toThrow(expectedQuality);
