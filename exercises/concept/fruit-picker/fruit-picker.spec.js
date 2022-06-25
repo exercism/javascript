ea513fc44ca46d81fde40ae3c0661213dae4dea6
Tim Austin <tim@neenjaw.com>
2022-06-25 13:13:48 -0600

    migrate to one describe per task

--- exercises/concept/fruit-picker/fruit-picker.spec.js
+++ exercises/concept/fruit-picker/fruit-picker.spec.js
@@ -12,30 +12,36 @@ jest.mock('./grocer', () => ({
   order: jest.fn(),
 }));
 
-describe('fruit-picker', () => {
-  afterEach(() => {
-    jest.resetAllMocks();
-  });
+afterEach(() => {
+  jest.resetAllMocks();
+});
 
+describe('task 1', () => {
   test('onSuccess should call notify with a success message', () => {
     expect(onSuccess()).toEqual(undefined);
     expect(notify).toHaveBeenCalledTimes(1);
     expect(notify).toHaveBeenCalledWith({ message: 'SUCCESS' });
   });
+});
 
+describe('task 2', () => {
   test('onError should call notify with an error message', () => {
     expect(onError()).toEqual(undefined);
     expect(notify).toHaveBeenCalledTimes(1);
     expect(notify).toHaveBeenCalledWith({ message: 'ERROR' });
   });
+});
 
-  test('order from grocer passes callback arguments forward to order', () => {
+describe('task 3', () => {
+  test('order from grocer passes callback function arguments forward', () => {
     const query = { variety: 'apple', quantity: 10 };
     orderFromGrocer(query, onSuccess, onError);
     expect(order).toHaveBeenCalledTimes(1);
     expect(order).toHaveBeenCalledWith(query, onSuccess, onError);
   });
+});
 
+describe('task 4', () => {
   test('postOrder composes a request to the grocer using the defined callbacks', () => {
     const variety = 'banana';
     const quantity = 5;
