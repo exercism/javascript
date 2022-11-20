006adbb97b6943f6d4fea1c640242a58622fa49c
meatball <69751659+meatball133@users.noreply.github.com>
2022-11-20 16:39:52 +0100

    fix bug created

--- exercises/concept/train-driver/.meta/exemplar.js
+++ exercises/concept/train-driver/.meta/exemplar.js
@@ -22,7 +22,7 @@
  */
 export function fixListOfWagons(eachWagonsID) {
   const [first, second, ...rest] = eachWagonsID;
-  return [1, ...rest, first, second];
+  return [...rest, first, second];
 }
 
 /**
