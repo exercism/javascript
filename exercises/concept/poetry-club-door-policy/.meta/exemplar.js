4faea08e9b0a50fe254057b1100e4f0a20f27000
habere-et-dispertire <github+private@dispertire.org>
2023-09-08 10:32:49 +0100

    Use non-interpolating string delimiters (#2265)
    * Use non-interpolating string delimiters
    
    * [CI] Format code
    
    * Single quotes to doube quotes
    
    * Back to single quotes
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- exercises/concept/poetry-club-door-policy/.meta/exemplar.js
+++ exercises/concept/poetry-club-door-policy/.meta/exemplar.js
@@ -42,5 +42,5 @@ export function backDoorResponse(line) {
  * @returns {string} the back door password
  */
 export function backDoorPassword(word) {
-  return frontDoorPassword(word) + `, please`;
+  return frontDoorPassword(word) + ', please';
 }
