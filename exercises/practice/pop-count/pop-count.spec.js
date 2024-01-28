b3b5242f6210b6e7f16bdebaf212b32b50b168aa
Cool-Katt <stani_s1@abv.bg>
2024-01-28 11:29:16 +0200

    [New practice exercise] Pop count (#2373)

---
+++ exercises/practice/pop-count/pop-count.spec.js
@@ -0,0 +1,27 @@
+import { eggCount } from './pop-count';
+
+describe('PopCount', () => {
+  test('0 eggs', () => {
+    const expected = 0;
+    const actual = eggCount(0);
+    expect(actual).toEqual(expected);
+  });
+
+  xtest('1 egg', () => {
+    const expected = 1;
+    const actual = eggCount(16);
+    expect(actual).toEqual(expected);
+  });
+
+  xtest('4 eggs', () => {
+    const expected = 4;
+    const actual = eggCount(89);
+    expect(actual).toEqual(expected);
+  });
+
+  xtest('13 eggs', () => {
+    const expected = 13;
+    const actual = eggCount(2000000000);
+    expect(actual).toEqual(expected);
+  });
+});
