6dfd186bd7b32562f2b732e714eaa88cc264d842
Cool-Katt <stani_s1@abv.bg>
2024-01-29 17:50:10 +0200

    Added some credits, prerequisites and fixed linting errors

--- exercises/practice/parallel-letter-frequency/.meta/proof.ci.js
+++ exercises/practice/parallel-letter-frequency/.meta/proof.ci.js
@@ -1,7 +1,7 @@
 export const parallelLetterFrequency = (texts) => {
   let result = {}
   let formatedTexts = texts.map(x => x.toLowerCase().match(/\p{Letter}+/gu) ?? []).flat()
-  let _ = Promise.all(formatedTexts.map(t => processSingleText(t, result)))
+  Promise.all(formatedTexts.map(t => processSingleText(t, result)))
   return result
 };
 
@@ -11,5 +11,5 @@ const processSingleText = (text, result) => {
     return acc
   }, result)
 
-  return new Promise((resolve, _) => resolve(res))
+  return new Promise((resolve) => { resolve(res) })
 }
