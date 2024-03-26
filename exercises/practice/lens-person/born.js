2e6846a0397d05f144c703406c399ddf2addc3fc
Cool-Katt <stani_s1@abv.bg>
2024-03-26 14:23:20 +0200

    Adding support classes

---
+++ exercises/practice/lens-person/born.js
@@ -0,0 +1,11 @@
+export class Born {
+  /**
+   *
+   * @param {Address} bornAt
+   * @param {Date} bornOn
+   */
+  constructor(bornAt, bornOn) {
+    this.bornAt = bornAt;
+    this.bornOn = bornOn;
+  }
+}
