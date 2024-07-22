43f1fcdc3abc1f1ce04ad48cff73d2a859ce9546
ciiro <ciirodev335@gmail.com>
2024-07-22 09:43:35 +0100

    New immutability tests

--- exercises/practice/lens-person/lens-person.spec.js
+++ exercises/practice/lens-person/lens-person.spec.js
@@ -31,18 +31,20 @@ describe('nameLens', () => {
     expect(nameLens.get(updatedPerson)).toEqual(updatedPerson.name);
   });
 
-  xtest('should ensure immutability by checking the original person object', () => {
-    expect(person.name).toStrictEqual(new Name('Saravanan', 'Lakshmanan'));
+  xtest('should ensure immutability when setting a new name', () => {
+    const originalName = new Name('Saravanan', 'Lakshmanan');
+    nameLens.set(person, new Name('Subhash', 'Forst'));
+    expect(person.name).toStrictEqual(originalName);
   });
 });
 
 // Test suite for bornAtLens
 describe('bornAtLens', () => {
-  xtest('should get the address where the person was born', () => {
+  xtest('should get the address for where the person was born', () => {
     expect(bornAtLens.get(person)).toEqual(person.born.bornAt);
   });
 
-  xtest('should set a new street for the place where the person was born', () => {
+  xtest('should set a new address for where the person was born', () => {
     const updatedPerson = bornAtLens.set(
       person,
       new Address(2, 'Exercism street', 'Tamil Nadu', 'India'),
@@ -50,10 +52,15 @@ describe('bornAtLens', () => {
     expect(bornAtLens.get(updatedPerson)).toEqual(updatedPerson.born.bornAt);
   });
 
-  xtest('should ensure immutability by checking the original person object', () => {
-    expect(person.born.bornAt).toStrictEqual(
-      new Address(100, 'Hospital street', 'Tamil Nadu', 'India'),
+  xtest('should ensure immutability when setting a new birth address', () => {
+    const originalBirthAddress = new Address(
+      100,
+      'Hospital street',
+      'Tamil Nadu',
+      'India',
     );
+    bornAtLens.set(person, new Address(15, 'Clinic street', 'Kerala', 'India'));
+    expect(person.born.bornAt).toStrictEqual(originalBirthAddress);
   });
 });
 
@@ -68,7 +75,14 @@ describe('streetLens', () => {
     expect(streetLens.get(updatedPerson)).toEqual(updatedPerson.address.street);
   });
 
-  xtest('should ensure immutability by checking the original person object', () => {
-    expect(person.address.street).toStrictEqual('Coder street');
+  xtest('should ensure immutability when setting a new street', () => {
+    const originalAddress = new Address(
+      1,
+      'Coder street',
+      'Tamil Nadu',
+      'India',
+    );
+    streetLens.set(person, 'Mimic street');
+    expect(person.address).toStrictEqual(originalAddress);
   });
 });
