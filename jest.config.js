0bb2149e6b192b9b4dc1ff0cc3de2b22cc24d60f
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2024-10-18 22:30:46 +0200

    Fix test configuration

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,22 @@
 module.exports = {
   verbose: true,
-  modulePathIgnorePatterns: ['package.json'],
+  projects: ['<rootDir>'],
+  testMatch: [
+    '**/__tests__/**/*.[jt]s?(x)',
+    '**/test/**/*.[jt]s?(x)',
+    '**/?(*.)+(spec|test).[jt]s?(x)',
+  ],
+  testPathIgnorePatterns: [
+    '/(?:production_)?node_modules/',
+    '.d.ts$',
+    '<rootDir>/test/fixtures',
+    '<rootDir>/test/helpers',
+    '__mocks__',
+  ],
   transform: {
-    '^.+\\.[t|j]sx?$': 'babel-jest',
+    '^.+\\.[jt]sx?$': 'babel-jest',
   },
-  reporters: [],
+  moduleNameMapper: {
+    '^(\\.\\/.+)\\.js$': '$1',
+  },
 };
