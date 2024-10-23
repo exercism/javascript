811d743f8030933bd02eccfcdc6ab3b9a311fd66
Cool-Katt <stani_s1@abv.bg>
2024-10-23 15:31:57 +0300

    Add scaffolding

---
+++ exercises/practice/killer-sudoku-helper/killer-sudoku-helper.js
@@ -0,0 +1,8 @@
+//
+// This is only a SKELETON file for the 'Killer Sudoku Helper' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const combinations = (cage) => {
+  throw new Error('Remove this statement and implement this function');
+};
