94f73411bb92261137beef218a46bff3f1af098b
Dimitar Apostolovski <8737118+themetar@users.noreply.github.com>
2024-11-06 18:51:18 +0100

    [lens-person] Remove `get` and `set` methods (#2559)

--- exercises/practice/lens-person/lens.js
+++ exercises/practice/lens-person/lens.js
@@ -1,30 +1,11 @@
 export class Lens {
   /**
    *
-   * @param {Function} getter
-   * @param {Function} setter
+   * @param {Function} getter - Function to get the value using a lens
+   * @param {Function} setter - Function to set the value using a lens
    */
   constructor(getter, setter) {
     this.get = getter;
     this.set = setter;
-  }
-
-  /**
-   * Function to get the value from a lens
-   * @param {Person} person
-   * @returns {Person}
-   */
-  get(person) {
-    return this.get(person);
-  }
-
-  /**
-   * Function to set the value using a lens
-   * @param {Person} person
-   * @param {any} value
-   * @returns {Person}
-   */
-  set(person, value) {
-    return this.set(value, person);
   }
 }
