d4b91b3228171e139ec86636192c69dcf7f3d07b
Karan Agrahari <karanagraharee@gmail.com>
2025-01-13 18:10:07 +0530

    add recersive function

--- exercises/concept/pizza-order/pizza-order.js
+++ exercises/concept/pizza-order/pizza-order.js
@@ -1,17 +1,33 @@
 /// <reference path="./global.d.ts" />
+
+import { forEach } from "core-js/core/array"
+
 //
 // @ts-check
 
 /**
  * Determine the price of the pizza given the pizza and optional extras
  *
- * @param {Pizza} pizza name of the pizza to be made
+ * @param {Pizz a} pizza name of the pizza to be made
  * @param {Extra[]} extras list of extras
  *
  * @returns {number} the price of the pizza
  */
 export function pizzaPrice(pizza, ...extras) {
-  throw new Error('Please implement the pizzaPrice function');
+  const PizzaPrice = {
+    Margherita: 7,
+    Caprese: 9,
+    Formaggio: 10,
+  }
+  const extrasPrice = {
+    ExtraSauce: 1,
+    ExtraToppings: 2,
+  }
+  let price = PizzaPrice[pizza]||0
+  for( const extra of extras){
+    price += extrasPrice[extra]||0
+  }
+  return price
 }
 
 /**
@@ -24,5 +40,16 @@ export function pizzaPrice(pizza, ...extras) {
  * @returns {number} the price of the total order
  */
 export function orderPrice(pizzaOrders) {
-  throw new Error('Please implement the orderPrice function');
+  if(pizzaOrders.length === 0){
+    return 0;
+  }
+
+  let totalPrice = 0
+
+  pizzaOrders.forEach(order => {
+    totalPrice += pizzaPrice(order.pizza, ...order.extras) 
+  })
+
+  return totalPrice
+  // throw new Error('Please implement the orderPrice function');
 }
