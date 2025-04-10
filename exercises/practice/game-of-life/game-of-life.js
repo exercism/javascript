c8ae9b1c2f9904fdc898ae153a71dacf8d26ab0b
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2025-04-09 23:01:14 -0700

    Add `game-of-life` (#2620)
    * Add `game-of-life`
    
    * Format exercise config
    
    * Fix copy and paste error with first test
    
    * Add practices

---
+++ exercises/practice/game-of-life/game-of-life.js
@@ -0,0 +1,18 @@
+//
+// This is only a SKELETON file for the 'Conway's Game of Life' exercise. It's been provided
+// as a convenience to get you started writing code faster.
+//
+
+export class GameOfLife {
+  constructor() {
+    throw new Error('Remove this statement and implement this function');
+  }
+
+  tick() {
+    throw new Error('Remove this statement and implement this function');
+  }
+
+  state() {
+    throw new Error('Remove this statement and implement this function');
+  }
+}
