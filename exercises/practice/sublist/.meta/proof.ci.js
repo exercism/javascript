48c714c8ff321ef36ec5cfbece952164f29f57ce
Jagdish Prajapati <jagadishdrp@gmail.com>
2025-06-06 13:47:19 +0530

    running format.mjs

--- exercises/practice/sublist/.meta/proof.ci.js
+++ exercises/practice/sublist/.meta/proof.ci.js
@@ -6,10 +6,14 @@ export class List {
   }
 
   compare(other) {
-    const sublist = this.items.length === 0 ||
+    const sublist =
+      this.items.length === 0 ||
       `,${other.items.join(',')},`.includes(`,${this.items.join(',')},`);
-    const superlist = other.items.length === 0 ||
+    const superlist =
+      other.items.length === 0 ||
       `,${this.items.join(',')},`.includes(`,${other.items.join(',')},`);
-    return ['UNEQUAL', 'SUPERLIST', 'SUBLIST', 'EQUAL'][+superlist + (+sublist << 1)]
+    return ['UNEQUAL', 'SUPERLIST', 'SUBLIST', 'EQUAL'][
+      +superlist + (+sublist << 1)
+    ];
   }
 }
