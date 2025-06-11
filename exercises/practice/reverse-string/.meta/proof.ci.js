e4620261658ade56875028e4e10e15e37cdf94c8
Jagdish Prajapati <jagadishdrp@gmail.com>
2025-06-11 13:30:34 +0530

    Running prettier

--- exercises/practice/reverse-string/.meta/proof.ci.js
+++ exercises/practice/reverse-string/.meta/proof.ci.js
@@ -1,6 +1,9 @@
 export const reverseString = (string) => {
   let revString = '';
-  let characters = Array.from(new Intl.Segmenter().segment(String(string)), (x) => x.segment);
+  let characters = Array.from(
+    new Intl.Segmenter().segment(String(string)),
+    (x) => x.segment,
+  );
   for (let i = characters.length - 1; i >= 0; i--) {
     revString += characters[i];
   }
