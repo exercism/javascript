c334b74a0037fb477a203440a83015eb32126423
J R M <jwrmmine@protonmail.com>
2025-06-18 15:03:14 +0200

    Update captains-log.spec.js

--- exercises/concept/captains-log/captains-log.spec.js
+++ exercises/concept/captains-log/captains-log.spec.js
@@ -10,21 +10,42 @@ describe('randomShipRegistryNumber',() => {
     expect(randomShipRegistryNumber()).not.toEqual(randomShipRegistryNumber()) 
   });
 });
-describe('randomStardate',() => {
-  test('stardates are valid',() => {
-    for (let i=0;i<4; i++){
-      expect(randomStardate()>=41000 && randomStardate()<42000).toBe(true);
+function loadDie(...values) {
+  const originalRandom = Math.random()
+  
+  Math.random = function loadedDie { 
+    if (values.length === 0) {
+      return originalRandom();
     }
-  });
-  test('returns a random stardate',() => {
-    expect(randomStardate()).not.toEqual(randomStardate()) 
-  });
-});
+    return values.shift();
+  }
+  return () => {
+    Math.random = originalRandom;
+  }
+}
+describe('randomStardate',() => {
+  test('stardate is between 41000 and 42000',() => {
+    const restore = loadDie(
+      0, 0, 0, 0, 0, 0,
+      1, 1, 1, 1, 1, 1,
+      0.5, 0.5, 0.5, 0.5, 0.5, 0.5
+    );
+    
+    // 6 * 0, 6 * 1, 6 * 0.5 and everything else random
+    
+    for (let i = 0; i < 10_000; i++) {
+      const starDate = randomStardate()
+      expect(starDate).toBeGreaterThanOrEqual(41_000);
+      expect(starDate).toBeLessThan(42_000);
+    }
+    restore();
+  }
+}
 describe('randomPlanetClass', () => {
   test('planet classes are valid',  () => {
     const planetClasses = ['D', 'H', 'J', 'K', 'L', 'M', 'N', 'R', 'T', 'Y'];
     for (let i=0;i<4; i++){
-      expect(planetClasses.includes(randomPlanetClass())).toBe(true)
+      expect(planetClasses).toContain(randomPlanetClass())
     }
   });
   test('returns a random planet class',() => {
