1de010d8e01e3d517584340c0deef68bece1e828
J R M <jwrmmine@protonmail.com>
2025-06-18 14:33:35 +0200

    Modified tests in captains-log.spec.js

--- exercises/concept/captains-log/captains-log.spec.js
+++ exercises/concept/captains-log/captains-log.spec.js
@@ -2,9 +2,9 @@ import { describe, expect, test } from '@jest/globals';
 import { randomShipRegistryNumber, randomStardate, randomPlanetClass } from './captains-log';
 describe('randomShipRegistryNumber',() => {
   test('registry numbers are valid',() => {
-    expect(randomShipRegistryNumber().test(/NCC-[1-9][0-9]{3}/)).toBe(true);
-    expect(randomShipRegistryNumber().test(/NCC-[1-9][0-9]{3}/)).toBe(true);
-    expect(randomShipRegistryNumber().test(/NCC-[1-9][0-9]{3}/)).toBe(true);
+    for (let i=0;i<4; i++){
+      expect(randomShipRegistryNumber()).toMatch(/NCC-[1-9][0-9]{3}/))
+    }
   });
   test('returns a random registry number',() => {
     expect(randomShipRegistryNumber()).not.toEqual(randomShipRegistryNumber()) 
@@ -12,9 +12,9 @@ describe('randomShipRegistryNumber',() => {
 });
 describe('randomStardate',() => {
   test('stardates are valid',() => {
-    expect(41000<=randomStardate()<42000).toBe(true);
-    expect(41000<=randomStardate()<42000).toBe(true);
-    expect(41000<=randomStardate()<42000).toBe(true);
+    for (let i=0;i<4; i++){
+      expect(randomStardate()>=41000 && randomStardate()<42000).toBe(true);
+    }
   });
   test('returns a random stardate',() => {
     expect(randomStardate()).not.toEqual(randomStardate()) 
@@ -23,9 +23,9 @@ describe('randomStardate',() => {
 describe('randomPlanetClass', () => {
   test('planet classes are valid',  () => {
     const planetClasses = ['D', 'H', 'J', 'K', 'L', 'M', 'N', 'R', 'T', 'Y'];
-    expect(planetClasses.includes(randomPlanetClass())).toBe(true)
-    expect(planetClasses.includes(randomPlanetClass())).toBe(true)
-    expect(planetClasses.includes(randomPlanetClass())).toBe(true)
+    for (let i=0;i<4; i++){
+      expect(planetClasses.includes(randomPlanetClass())).toBe(true)
+    }
   });
   test('returns a random planet class',() => {
     expect(randomPlanetClass()).not.toEqual(randomPlanetClass())
