90b0cd3030947266e173f3200d77832cc6c1b980
J R M <jwrmmine@protonmail.com>
2025-06-27 18:41:33 +0200

    Update assembly-line.js
    Create all the functions using typeof

---
+++ exercises/concept/assembly-line/assembly-line.js
@@ -0,0 +1,55 @@
+// @ts-check
+//
+// The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion when
+// implementing this exercise.
+
+/**
+ * Checks if input is a boolean.
+ *
+ * @param {number|boolean|string|null|undefined|symbol|bigint|object} value
+ * @returns {boolean} whether the input is a boolean
+ */
+export function isBoolean(value) {
+  throw new Error("Remove this line and implement the isBoolean function")
+}
+
+/**
+ * Checks if input is a finite number or bigint.
+ *
+ * @param {number|boolean|string|null|undefined|symbol|bigint|object} value
+ * @returns {boolean} whether the input is a finite number or bigint
+ */
+export function isNumber(value) {
+  throw new Error("Remove this line and implement the isNumber function")
+}
+
+/**
+ * Checks if a value is an object.
+ *
+ * @param {number|boolean|string|null|undefined|symbol|bigint|object} value
+ * @returns {boolean} whether the input is an object.
+ */
+export function isObject(value) {
+  throw new Error("Remove this line and implement the isObject function")
+}
+
+/**
+ * Checks if a value is a numeric string.
+ *
+ * @param {number|boolean|string|null|undefined|symbol|bigint|object} value
+ * @returns {boolean} whether the input is a numeric string.
+ */
+export function isNumericString(value) {
+  throw new Error("Remove this line and implement the isNumericString function")
+}
+
+/**
+ * Checks if a value is an object.
+ *
+ * @param {number|boolean|string|null|undefined|symbol|bigint|object} value
+ * @returns {boolean} whether the input is an object.
+ */
+export function isElectronic(value) {
+  throw new Error("Remove this line and implement the isElectronic function")
+}
