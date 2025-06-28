4057c80baf8fa46e79d30f0b80adfe572479e5ca
J R M <jwrmmine@protonmail.com>
2025-06-28 17:18:38 +0200

    Update assembly-line.spec.js

--- exercises/concept/assembly-line/assembly-line.spec.js
+++ exercises/concept/assembly-line/assembly-line.spec.js
@@ -1,5 +1,5 @@
 import { describe, expect, test } from '@jest/globals';
-import { isBoolean, isNumber/*, isObject, isNumericString, isElectronic, isNonEmptyArray, isEmptyArray ,  assertHasId, hasType, hasConstructorProperty, hasDefinedType*/} from './assembly-line'
+import { isBoolean, isNumber, isObject/*, isNumericString, isElectronic, isNonEmptyArray, isEmptyArray ,  assertHasId, hasType, hasConstructorProperty, hasDefinedType*/} from './assembly-line'
 describe("isBoolean",() => {
   test("isBoolean works on booleans",() => {
     expect(isBoolean(true)).toBe(true)
@@ -34,5 +34,32 @@ describe("isNumber",() => {
   test("isNumber works on NaN and Infinity",() => {
     expect(isNumber(NaN)).toBe(false)
     expect(isNumber(Infinity)).toBe(false)
+  })
+});
+class ClassForTestingisObject {
+  constructor(word){
+    this.number=word
+  }
+}
+describe("isObject",() => {
+  test("isObject works on objects",() => {
+    expect(isObject({})).toBe(true);
+    expect(isObject({greeting:hello})).toBe(true);
+  });
+  test("isObject works on classes",() => {
+    expect(isObject(new ClassForTestingisObject())).toBe(true);
+    expect(isObject(new ClassForTestingisObject())).toBe(true);
+    expect(isObject(new ClassForTestingisObject())).toBe(true)
+  });
+  test("isObject works on non-Objects",() => {
+    expect(isObject(true)).toBe(false)
+    expect(isObject("Hello, World!")).toBe(false)
+    expect(isObject(null)).toBe(false)
+    expect(isObject("")).toBe(false)
+    expect(isObject(Symbol("1"))).toBe(false)
+  });
+  test("isObject works on NaN and Infinity",() => {
+    expect(isObject(NaN)).toBe(false)
+    expect(isObject(Infinity)).toBe(false)
   })
 });
