8c070a599233a1fc8ab1695afb0975c7160d26c2
Obinna Obi-Akwari <111562983+A-O-Emmanuel@users.noreply.github.com>
2025-08-26 21:08:48 +0100

    Update exercises/practice/error-handling/error-handling.js
    Co-authored-by: Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>

--- exercises/practice/error-handling/error-handling.js
+++ exercises/practice/error-handling/error-handling.js
@@ -1,6 +1,3 @@
 export const processString = (input) => {
   //TODO: implement this
-  //should throw TypeError if input is not a string
-  //should throw a general Error if input is an empty string
-  //should return the uppercase version of the string 'hello'
 };
