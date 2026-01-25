ffcc478ece72fa559a41dc2489044b4300eefdfc
Ryan Hartlage <2488333+ryanplusplus@users.noreply.github.com>
2026-01-25 00:19:09 -0500

    Use correct exercise name in the skeleton comment for Camicia (#2812)

--- exercises/practice/camicia/camicia.js
+++ exercises/practice/camicia/camicia.js
@@ -1,5 +1,5 @@
 //
-// This is only a SKELETON file for the 'BookStore' exercise. It's been provided as a
+// This is only a SKELETON file for the 'Camicia' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
 
