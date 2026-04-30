5e92916a3a72e075c5d79a8017afcf5cd3d2f301
Joyal George K J <144530321+joyalgeorgekj@users.noreply.github.com>
2026-04-30 20:22:09 +0530

    Update JSDoc to use CallbackType for return types

--- exercises/concept/coordinate-transformation/coordinate-transformation.js
+++ exercises/concept/coordinate-transformation/coordinate-transformation.js
@@ -5,13 +5,17 @@
 // implementing this exercise.
 
 /**
+ * @typedef {function (number, number): [number, number]} CallbackType
+ */
+
+/**
  * Create a function that returns a function making use of a closure to
  * perform a repeatable 2d translation of a coordinate pair.
  *
  * @param {number} dx the translate x component
  * @param {number} dy the translate y component
  *
- * @returns {function} a function which takes an x, y parameter, returns the
+ * @returns {CallbackType} a function which takes an x, y parameter, returns the
  *  translated coordinate pair in the form [x, y]
  */
 export function translate2d(dx, dy) {
@@ -25,7 +29,7 @@ export function translate2d(dx, dy) {
  * @param {number} sx the amount to scale the x component
  * @param {number} sy the amount to scale the y component
  *
- * @returns {function} a function which takes an x, y parameter, returns the
+ * @returns {CallbackType} a function which takes an x, y parameter, returns the
  *  scaled coordinate pair in the form [x, y]
  */
 export function scale2d(sx, sy) {
@@ -39,7 +43,7 @@ export function scale2d(sx, sy) {
  * @param {function} f the first function to apply
  * @param {function} g the second function to apply
  *
- * @returns {function} a function which takes an x, y parameter, returns the
+ * @returns {CallbackType} a function which takes an x, y parameter, returns the
  *  transformed coordinate pair in the form [x, y]
  */
 export function composeTransform(f, g) {
@@ -52,7 +56,7 @@ export function composeTransform(f, g) {
  *
  * @param {function} f the transformation function to memoize, assumes takes two arguments 'x' and 'y'
  *
- * @returns {function} a function which takes x and y arguments, and will either return the saved result
+ * @returns {CallbackType} a function which takes x and y arguments, and will either return the saved result
  *  if the arguments are the same on subsequent calls, or compute a new result if they are different.
  */
 export function memoizeTransform(f) {
